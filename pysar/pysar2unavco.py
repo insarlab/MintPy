18c96ca316eedc5bfbd37b02319b565fd7ce7862
stackTom <rtiky23@gmail.com>
2016-10-14 22:57:59 -0400

    added pysar2unavco and unavco2website

---
+++ pysar/pysar2unavco.py
@@ -0,0 +1,373 @@
+#! /usr/bin/env python
+
+import h5py
+import numpy as np
+import datetime
+from datetime import date
+import sys
+import os
+from osgeo import ogr
+import getopt
+import re
+
+# COMMAND I USE TO RUN SCRIPT: 
+# python ~/code/insar_map_mvc/storage/json/pysar2unavco_back.py -t timeseries.h5 -i incidence_angle.h5 -d DEM_error.h5 -c temporal_coherence.h5 -m mask.h5 
+# ---------------------------------------------------------------------------------------
+# FUNCTIONS
+# ---------------------------------------------------------------------------------------
+# takes string formatted (YYYY-MM-DD) and returns date object
+def get_date(date_string): 
+	year = int(date_string[0:4])
+	month = int(date_string[4:6])
+	day = int(date_string[6:8])
+	return date(year, month, day)
+
+def usage():
+	print 'Correct format: python pysar2unavco.py -t timeseries.h5 -i incidence_angle.h5 -d DEM_error.h5 -c temporal_coherence.h5 -m mask.h5'
+	print 'Optional: --add_options OPTION_NAME=OPTION_VALUE'
+
+# ---------------------------------------------------------------------------------------
+#  BEGIN EXECUTABLE
+# ---------------------------------------------------------------------------------------
+# if len(sys.argv) != 10:
+try:
+	opts, extraArgs = getopt.getopt(sys.argv[1:],'t:i:d:c:m:', ['add_option=']) 
+except getopt.GetoptError:
+	print 'Error while retrieving operations - exit'
+	usage()
+	sys.exit()
+
+# read operations and arguments(file names):
+operations = {}
+added_options = {}
+
+for o, a in opts:
+	if o == "-t" or o == "-i" or o == "-d" or o == "-c" or o == "-m":
+		operations[o] = a
+	if o == '--add_option':
+		option_and_value = a.split('=')
+		added_options[option_and_value[0].upper()] = option_and_value[1]
+	elif o == "-t":
+		timeseries = a
+	elif o == "-i":
+		incidence_angle = a
+	elif o == "-d":
+		dem = a
+	elif o == "-c":
+		temporal_coherence = a
+	elif o == "-m":
+		mask = a
+	else:
+		assert False, "unhandled option - exit"
+		sys.exit()
+
+# ---------------------------------------------------------------------------------------
+#  GET TIMESERIES FILE INTO UNAVCO and encode attributes to timeseries group in unavco file
+# ---------------------------------------------------------------------------------------
+print 'trying to open ' + timeseries
+timeseries_file = h5py.File(timeseries, "r")
+try: 
+	key = 'timeseries'
+	group = timeseries_file[key]
+except: 
+	print "failed using key '" + key + "' on " + dem
+	sys.exit()
+
+# get attributes and dates from timeseries file, sort dates to make life easier
+attributes = group.attrs
+dates = group.keys()
+dates.sort()
+
+# create unavco timeseries file - will change name of file to proper format later in script
+unavco = "unavco_test.h5"
+unavco_file = h5py.File(unavco, "w")
+print "created test file"
+
+# copy datasets from timeseries file into unavco file - main group key is 'GEOCODE'
+# similar format key to the interferogram files found in unavco portal
+unavco_file.create_group('/GEOCODE/timeseries')
+for d in dates:
+	old_key = '/timeseries/' + d
+	timeseries_file.copy(old_key, unavco_file['/GEOCODE/timeseries'], d)
+
+# ---------------------------------------------------------------------------------------
+#  ENCODE REQURIED ATTRIBUTES FROM TIMESERIES FILE INTO UNAVCO
+# ---------------------------------------------------------------------------------------
+x_step = float(attributes["X_STEP"])	# longitude = width = x = columns
+y_step = float(attributes["Y_STEP"])	# latitude = length = y = rows
+x_first = float(attributes["X_FIRST"])
+y_first = float(attributes["Y_FIRST"])
+num_columns = int(attributes["WIDTH"])
+num_rows = int(attributes["FILE_LENGTH"])
+
+# extract attributes from project name
+# KyushuT73F2980_2990AlosD - 73 = track number, 2980_2990 = frames, Alos = mission
+try:
+	project_name = attributes["PROJECT_NAME"]
+except Exception, e:
+	print "Project name is not in the h5 file, trying to find supplied name on the command line"
+	key = "project_name"
+
+	try:
+		project_name = added_options[key.upper()]
+	except Exception, e:
+		print "Project name not supplied on the command line... quitting"
+		sys.exit()
+
+print project_name
+track_index = project_name.find('T')
+frame_index = project_name.find('F')
+track_number = project_name[track_index+1:frame_index]
+region_name = project_name[:track_index]
+
+# sometimes there is only one frame number instead of framenumber_framenumber - look for "_"
+multipleFrames = False
+try:
+	underscore = re.search("_", project_name).group(0)
+	multipleFrames = True
+except:
+	pass
+
+if multipleFrames:
+	frames = re.search("\d+_\d+", project_name).group(0)
+	first_frame = frames.split("_")[0]
+	last_frame = frames.split("_")[1]
+else:
+	frames = re.search("\d+", project_name[frame_index+1:]).group(0)
+	first_frame = frames
+	last_frame = frames
+
+mission_index = project_name.find(frames) + len(frames)
+mission = project_name[mission_index:len(project_name)-1]
+
+group = unavco_file['/']
+# 1) mission = last chars of a folder name - SinabungT495F40_50AlosA -> mission = Alos
+group.attrs['mission'] = mission
+
+# 2) beam_mode = Yunjun said SM (stripmap) for now, later read from encoded timeseries.h5
+group.attrs['beam_mode'] = 'SM'
+
+# 3) beam_swath = Yunjun said SM (stripmap) for now
+group.attrs['beam_swath'] = 'SM'
+
+# 4) relative_orbit = number after T -> in case of Sinabung example above, 495
+group.attrs['relative_orbit'] = int(track_number)
+
+# 5 + 6) first_date and last date of timeseries datasets
+group.attrs['first_date'] = get_date(dates[0]).isoformat()
+group.attrs['last_date'] = get_date(dates[len(dates)-1]).isoformat()
+print "type of last_date: " + str(type(get_date(dates[len(dates)-1]).isoformat()))
+
+# 7) scene footprint = wkt polygon
+# Create polygon square with boundaries of datasets
+ring = ogr.Geometry(ogr.wkbLinearRing) # points added as (x,y) or (long,lat)
+ring.AddPoint( x_first, y_first )
+ring.AddPoint( x_first, (y_step * num_rows + y_first) )
+ring.AddPoint( (x_first + x_step * num_columns),  y_first)
+ring.AddPoint( (x_first + x_step * num_columns), (y_step * num_rows + y_first) )
+geom_poly = ogr.Geometry(ogr.wkbPolygon)
+geom_poly.AddGeometry(ring)
+wkt = geom_poly.ExportToWkt()
+group.attrs['scene_footprint'] = wkt
+
+# 8) processing type = LOS_TIMESERIES
+group.attrs['processing_type'] = 'LOS_TIMESERIES'
+
+# 9) processing software = PySAR or StaMPS - PySAR for now
+group.attrs['processing_software'] = 'PySAR'
+
+# 10) history = (not important) the day I processed h5 files into UNAVCO format
+group.attrs['history'] = datetime.datetime.now().date().isoformat()
+
+# ---------------------------------------------------------------------------------------
+#  ENCODE RECOMMENDED ATTRIBUTES FROM TIMESERIES FILE INTO UNAVCO
+# ---------------------------------------------------------------------------------------
+# UNAVCO wants this to be an int but we have multiple frames so we have two frame attributes
+group.attrs['first_frame'] = int(first_frame)
+group.attrs['last_frame'] = int(last_frame)
+
+# flight_direction = A or D (ascending or descending)
+# tried to encode as char but python seems to only know string
+group.attrs['flight_direction'] = chr(ord(attributes['ORBIT_DIRECTION'][0].upper()))
+
+# for now, all falks files are Right (R)
+# tried to encode as char but python seems to only know string
+group.attrs['look_direction'] = chr(ord('R'))
+
+# polarization = naN (Yunjun doesn't care for this attribute)
+# group.attrs['polarization'] = np.nan
+
+# attribute in yunjun/falk's h5 files: prf, wavelength
+group.attrs['prf'] = float(attributes['PRF'])
+group.attrs['wavelength'] = float(attributes['WAVELENGTH'])
+
+# atmos_correct_method = 'ECMWF' harcode for now, later read from timeseries.h5 after Yunjun encodes attribute
+group.attrs['atmos_correct_method'] = 'ECMWF'
+
+# processing_dem = yunjun is adding attribute to geo_timeseries.h5, wait for now
+
+# unwrap method = SNAPHU for all files right now
+group.attrs['unwrap_method'] = 'SNAPHU'
+
+# post_processing_method = PySAR or StaMPS - PySAR for now
+group.attrs['post_processing_method'] = 'PySAR'
+
+# master_platform, percent_atmos, and other interferogram attributes - dont need
+
+# baseline_perp = attribute called P_BASELINE_TIMESERIES
+# UNAVCO format pdf says: This really only applies to interferograms, but for 
+# time series or velocity products this could be the min and max bperp.
+group.attrs['min_baseline_perp'] = float(attributes['P_BASELINE_BOTTOM_HDR'])
+group.attrs['max_baseline_perp'] = float(attributes['P_BASELINE_TOP_HDR'])
+
+# ---------------------------------------------------------------------------------------
+#  ENCODE ATTRIBUTES FROM FALK'S FILES TO CALCULATE LATITUDE AND LONGITUDE - NOT IN UNAVCO DOC
+# ---------------------------------------------------------------------------------------
+group.attrs['X_STEP'] = x_step
+group.attrs['Y_STEP'] = y_step
+group.attrs['X_FIRST'] = x_first
+group.attrs['Y_FIRST'] = y_first
+group.attrs['WIDTH'] = num_columns
+group.attrs['FILE_LENGTH'] = num_rows
+
+timeseries_file.close()
+# ---------------------------------------------------------------------------------------
+#  GET INCIDENCE_ANGLE FILE INTO UNAVCO
+# ---------------------------------------------------------------------------------------
+# Yunjun encoded incidence_angle.h5 with key 'mask' but it should be 'incidence_angle'
+# try both keys
+print 'trying to open ' + incidence_angle
+incidence_angle_file = h5py.File(incidence_angle, "r")
+is_open = True
+try:
+	key = 'incidence_angle'
+	group = incidence_angle_file[key]
+except:
+	print "failed using key '" + key + "' on " + incidence_angle
+	is_open = False
+
+if not is_open:
+	try:
+		key = 'mask'
+		group = incidence_angle_file[key]
+	except:
+		print "failed using key '" + key + "' on " + incidence_angle
+		print incidence_angle + ' could not be read'
+		sys.exit()
+
+# in current format incidence_angle.h5 requires you to use key 'incidence_angle/incidence_angle'
+# in order to get the datasets from the h5 file - perhaps in later version we fix this to make it
+# less verbose
+for k in group.keys():
+	old_key = key + "/" + k
+	incidence_angle_file.copy(old_key, unavco_file['/GEOCODE'], 'incidence_angle')
+
+incidence_angle_file.close()
+
+# ---------------------------------------------------------------------------------------
+#  GET DEM FILE INTO UNAVCO
+# ---------------------------------------------------------------------------------------
+dem_file = h5py.File(dem, "r")
+print 'trying to open ' + dem
+try:
+	key = 'dem'
+	group = dem_file[key]
+except:
+	print "failed using key '" + key + "' on " + dem
+
+for k in group.keys():
+	old_key = key + "/" + k
+	dem_file.copy(old_key, unavco_file['/GEOCODE'], key)
+
+dem_file.close()
+
+# ---------------------------------------------------------------------------------------
+#  GET TEMPORAL COHERENCE FILE INTO UNAVCO
+# ---------------------------------------------------------------------------------------
+temporal_coherence_file = h5py.File(temporal_coherence, "r")
+print 'trying to open ' + temporal_coherence
+try:
+	key = 'temporal_coherence'
+	group = temporal_coherence_file[key]
+except:
+	print "failed using key '" + key + "' on " + temporal_coherence
+
+for k in group.keys():
+	old_key = key + "/" + k
+	temporal_coherence_file.copy(old_key, unavco_file['/GEOCODE'], key)
+
+temporal_coherence_file.close()
+
+# ---------------------------------------------------------------------------------------
+#  GET MASK FILE INTO UNAVCO
+# ---------------------------------------------------------------------------------------
+mask_file = h5py.File(mask, "r")
+print 'trying to open ' + mask
+try:
+	key = 'mask'
+	group = mask_file[key]
+except:
+	print "failed using key '" + key + "' on " + mask
+
+for k in group.keys():
+	old_key = key + "/" + k
+	mask_file.copy(old_key, unavco_file['/GEOCODE'], key)
+
+mask_file.close()
+
+# CLOSE UNAVCO FILE
+unavco_file.close()
+
+# IMPORTANT: RENAME UNAVCO file to proper format based on file attributes
+# example - pysar file is called Kyushu T 80 F 245_246 JersD.h5
+# UNAVCO timeseries file is called JERS_SM_80_245_246_<first date>_<last date>.h5 since we dont need TBASE or BPERP for timeseries
+unavco_name = mission + '_SM_' + track_number + '_' + frames + '_' + dates[0] + '_' + dates[len(dates)-1] + '.h5'
+os.rename(unavco, "./" + unavco_name)
+
+# create a text file to store region (ex: Kyushu) needed for database but not unavco format
+attributes_file_name = unavco_name[:len(unavco_name)-3] + '_region.txt'
+attributes_file = open(attributes_file_name, "w")
+attributes_file.write(region_name)
+attributes_file.close()
+
+# ---------------------------------------------------------------------------------------
+#  TEST THE UNAVCO FILE FOR DATASETS
+# ---------------------------------------------------------------------------------------
+# open unavco h5 file that was created earlier for testing
+f = h5py.File(unavco_name, "r")
+print "opened "  + unavco_name
+
+print 'trying to print the attribute types'
+attributes = f['/'].attrs.items()
+for k, v in attributes:
+	print "key: " + k + ", value: " + str(v) + ", type: " + str(type(v)) + "\n"
+
+print 'trying to print main group GEOCODE'
+geocode = f["GEOCODE"]
+print geocode
+
+print 'trying to print group timeseries'
+group = f['GEOCODE/timeseries']
+print group
+
+print 'trying to print last dataset of timeseries'
+dataset = group[dates[len(dates)-1]]
+print dataset
+
+print 'trying to print dataset incidence_angle'
+dataset = f['GEOCODE/incidence_angle']
+print dataset
+
+print 'trying to print dataset dem'
+dataset = f['GEOCODE/dem']
+print dataset
+
+print 'trying to print dataset temporal_coherence'
+dataset = f['GEOCODE/temporal_coherence']
+print dataset
+
+print 'trying to print dataset mask'
+dataset = f['GEOCODE/mask']
+print dataset
+
+f.close()
---
+++ pysar/unavco2website.py
@@ -0,0 +1,302 @@
+#! /usr/bin/env python
+
+import json
+import h5py
+import numpy as np
+from datetime import date
+import math
+import time
+import os
+import sys
+import psycopg2
+import geocoder
+
+# ex: python Converter_unavco.py Alos_SM_73_2980_2990_20070107_20110420.h5
+
+# This script takes a UNAVCO format timeseries h5 file, converts to mbtiles, 
+# and sends to database which allows website to make queries and display data
+# ---------------------------------------------------------------------------------------
+# FUNCTIONS
+# ---------------------------------------------------------------------------------------
+dbUsername = "INSERT"
+dbPassword = "INSERT"
+dbHost = "INSERT"
+# returns a dictionary of datasets that are stored in memory to speed up h5 read process
+def get_date(date_string): 
+	year = int(date_string[0:4])
+	month = int(date_string[4:6])
+	day = int(date_string[6:8])
+	return date(year, month, day)
+# ---------------------------------------------------------------------------------------
+# takes a date and calculates the number of days elapsed in the year of that date
+# returns year + (days_elapsed / 365), a decimal representation of the date necessary
+# for calculating linear regression of displacement vs time
+def get_decimal_date(d):
+	start = date(d.year, 1, 1)
+   	return abs(d-start).days / 365.0 + d.year
+# ---------------------------------------------------------------------------------------
+def convert_data(): 
+	# create a siu_man array to store json point objects
+	siu_man = []
+	displacement_values = []
+	displacements = '{'
+	# np array of decimal dates, x parameter in linear regression equation
+	x = decimal_dates
+	A = np.vstack([x, np.ones(len(x))]).T
+	y = []
+	chunk_num = 1
+	point_num = 0
+
+	# outer loop increments row = longitude, inner loop increments column = latitude
+	for (row, col), value in np.ndenumerate(timeseries_datasets[dataset_keys[0]]):
+		latitude = x_first + (col * x_step)
+		longitude = y_first + (row * y_step) 
+		displacement = float(value)	
+		# if value is not equal to naN, create a new json point object and append to siu_man array
+		if not math.isnan(displacement):
+			# get displacement values for all the dates into array for json and string for pgsql
+			for key in dataset_keys:
+				displacement = timeseries_datasets[key][row][col]
+				displacements += (str(displacement) + ",")
+				displacement_values.append(float(displacement))
+			displacements = displacements[:len(displacements)-2] + '}'
+
+			# np array of displacement values, y parameter in linear regression equation
+			y = displacement_values
+
+			# y = mx + c -> we want m = slope of the linear regression line 
+			m, c = np.linalg.lstsq(A, y)[0]
+
+			data = {
+   			"type": "Feature",
+   			"geometry": {"type": "Point", "coordinates": [latitude, longitude]},	
+   			"properties": {"d": displacement_values, "m": m, "p": point_num}
+			}	
+			# allocate memory space for siu_man array in beginning 
+			siu_man.append(data)
+
+			# clear displacement array for json and the other string for dictionary, for next point
+			displacement_values = []
+			displacements = '{'
+			point_num += 1
+			# break;	# for testing purposes convert only 1 point
+
+			# if chunk_size limit is reached, write chunk into a json file
+			# then increment chunk number and clear siu_man array
+			if len(siu_man) == chunk_size:
+				make_json_file(chunk_num, siu_man)
+				chunk_num += 1
+				siu_man = []
+
+	# write the last chunk that might be smaller than chunk_size
+	make_json_file(chunk_num, siu_man)
+
+	# calculate mid lat and long of dataset - then use google python lib to get country
+	mid_lat = x_first + ((num_columns/2) * x_step)
+	mid_long = y_first + ((num_rows/2) * y_step)
+	country = None
+	try:
+		g = geocoder.google([mid_long,mid_lat], method='reverse', timeout=60.0)
+ 		country = str(g.country_long)
+	except Exception, e:
+		print "timeout reverse geocoding country name"
+
+ 	area = folder_name
+
+ 	# for some reason pgsql only takes {} not [] - format date arrays and attributes to be inserted to pgsql
+ 	string_dates_sql = '{'
+ 	for k in dataset_keys:
+ 		string_dates_sql += (str(k) + ",")
+ 	string_dates_sql = string_dates_sql[:len(string_dates_sql)-2] + '}'
+
+ 	decimal_dates_sql = '{'
+ 	for d in decimal_dates:
+ 		decimal_dates_sql += (str(d) + ",")
+ 	decimal_dates_sql = decimal_dates_sql[:len(decimal_dates_sql)-2] + '}'
+
+ 	# scene_footprint attribute uses a wkt geometry type with format that confuses postgresql database
+ 	# thus we have to add "Polygon(coordinates, coordinates, coordinates, coordinates)" as string
+ 	attribute_keys = '{'
+ 	attribute_values = '{'
+ 	for k, v in attributes_dictionary:
+ 		attribute_keys += (str(k) + ",")
+ 		if "POLYGON" in str(v):
+ 			arr = v.split(",")
+ 			s = "\,"
+ 			v = s.join(arr)
+ 		attribute_values += (str(v) + ',')
+ 	attribute_keys = attribute_keys[:len(attribute_keys)-1] + '}'
+ 	attribute_values = attribute_values[:len(attribute_values)-1] + '}'
+
+	try:	# connect to databse
+		con = psycopg2.connect("dbname='pgis' user='" + dbUsername + "' host='" + dbHost + "' password='" + dbPassword + "'")
+		cur = con.cursor()
+		# create area table if not exist - limit for number of dates is 200, limt for number of attribute keys/values is 100
+		cur.execute("CREATE TABLE IF NOT EXISTS area ( name varchar, latitude double precision, longitude double precision, country varchar, region varchar, numchunks integer, attributekeys varchar[100], attributevalues varchar[100], stringdates varchar[200], decimaldates double precision[200] );")
+		con.commit()
+		print 'created area table'
+	except Exception, e:
+		print "unable to connect to the database"
+		print e
+		sys.exit()
+
+	# put dataset into area table
+	# area_data = {"latitude": mid_lat, "longitude": mid_long, "country": country, "num_chunks": chunk_num, "dates": dataset_keys}
+	try:
+		con = psycopg2.connect("dbname='pgis' user='" + dbUsername + "' host='" + dbHost + "' password='" + dbPassword + "'")
+		cur = con.cursor()
+		query = 'INSERT INTO area VALUES (' + "'" + area + "','" + str(mid_lat) + "','" + str(mid_long) + "','" + country + "','" + region + "','" + str(chunk_num) + "','" + attribute_keys + "','" + attribute_values + "','" + string_dates_sql + "','" + decimal_dates_sql + "')"
+		cur.execute(query)
+		con.commit()
+		con.close()
+	except Exception, e:
+		print "error inserting into area"
+		print e
+		sys.exit()
+
+	# create index to speed up queries:
+	print "Creating index"
+	try:
+		con = psycopg2.connect("dbname='pgis' user='" + dbUsername + "' host='" + dbHost + "' password='" + dbPassword + "'")
+		cur = con.cursor()
+		query = 'CREATE INDEX ON ' + area + ' (p)'
+		cur.execute(query)
+		con.commit()
+		con.close()
+	except Exception, e:
+		print "error creating index on p"
+		print e
+		#sys.exit() Don't exit, as what if the index already exists
+	print "Done creating index"
+	
+# ---------------------------------------------------------------------------------------
+# create a json file out of siu man array
+# then put json file into directory named after the h5 file
+def make_json_file(chunk_num, points): 
+
+	data = {
+	"type": "FeatureCollection",
+	"dates": dataset_keys, 
+	"features": points
+	}
+
+	chunk = "chunk_" + str(chunk_num) + ".json"
+	json_file = open(json_path + "/" + chunk, "w")
+	string_json = json.dumps(data, indent=4, separators=(',',':'))
+	json_file.write("%s" % string_json)
+	json_file.close()
+
+	# insert json file to pgsql using ogr2ogr - folder_name = area name
+	command = 'ogr2ogr -append -f "PostgreSQL" PG:"dbname=pgis host=' + dbHost + ' user=' + dbUsername + ' password=' + dbPassword + '" --config PG_USE_COPY YES -nln ' + folder_name + " "
+	chunk_path = './mbtiles/' + folder_name + '/' + chunk
+	os.system(command + ' ' + chunk_path)
+	print "inserted chunk " + str(chunk_num) + " to db"
+
+def usage():
+	print "Usage: python Converter_unavco.py Alos_SM_73_2980_2990_20070107_20110420.h5"
+# ---------------------------------------------------------------------------------------
+# START OF EXECUTABLE
+# ---------------------------------------------------------------------------------------
+# get name of h5 file and the groupname of that file's data
+if (len(sys.argv) != 2):
+	print "Incorrect number of arguments - see correct example below:"
+	usage()
+	sys.exit()
+
+file_name = sys.argv[1]
+path_name = file_name[:len(file_name)-3]
+region_file_name = file_name[:len(file_name)-3] + '_region.txt'
+# ---------------------------------------------------------------------------------------
+# start clock to track how long conversion process takes
+start_time = time.clock()
+
+# search for region file - if exist get first line which is region name
+region_file = None
+region = "null"
+try: 
+	region_file = open(region_file_name, "r")
+	region = region_file.readline()
+	region_file.close()
+except:
+	pass
+
+# use h5py to open specified group(s) in the h5 file 
+# then read datasets from h5 file into memory for faster reading of data
+# depending on UNAVCO format, the main key to access groups might be '/GEOCODE'
+file = h5py.File(file_name,  "r")
+group = file['GEOCODE/timeseries']	# assuming there is only one main key called 'GEOCODE'
+
+# get attributes (stored at root) of UNAVCO timeseries file
+attributes = file['/'].attrs
+attributes_dictionary = attributes.items()
+
+# in timeseries group, there are 25 datasets 
+# need to get datasets with dates - strings that can be converted to integers
+dataset_keys = []
+for k in group.keys():
+	if k.isdigit():
+		dataset_keys.append(k)
+dataset_keys.sort()
+
+# get the attributes for calculating latitude and longitude
+x_step = float(attributes["X_STEP"])
+y_step = float(attributes["Y_STEP"])
+x_first = float(attributes["X_FIRST"])
+y_first = float(attributes["Y_FIRST"])
+num_columns = int(attributes["WIDTH"])
+num_rows = int(attributes["FILE_LENGTH"])
+print "columns: %d" % num_columns
+print "rows: %d" % num_rows
+
+# array that stores dates from dataset_keys that have been converted to decimal
+decimal_dates = []
+
+# read datasets in the group into a dictionary of 2d arrays and intialize decimal dates
+timeseries_datasets = {}
+for key in dataset_keys:
+	timeseries_datasets[key] = group[key][:]
+	d = get_date(key)
+	decimal = get_decimal_date(d)
+	decimal_dates.append(decimal)
+
+# set number of points per json chunk - then close h5 file
+chunk_size = 20000
+file.close()	
+
+# connect to postgresql database
+# also create folder named after h5 file to store json files in mbtiles folder
+con = None
+cur = None
+
+path_list = path_name.split("/")
+mbtiles_path = os.getcwd() + "/mbtiles"
+folder_name = path_name.split("/")[len(path_list)-1]
+json_path = mbtiles_path + "/" + folder_name
+
+try: # create path for folder that stores all mbtiles
+	os.mkdir(mbtiles_path)
+except:
+	print mbtiles_path + " already exists"
+
+try: # create path for json
+	os.mkdir(json_path)
+except:
+	print json_path + " already exists"
+
+# read and convert the datasets, then write them into json files and insert into database
+convert_data()
+
+# run tippecanoe command to get mbtiles file and then delete the json files to save space
+os.chdir(os.path.abspath(json_path))
+os.system("tippecanoe *.json -x d -pf -pk -Bg -d9 -D12 -g12 -r0 -o " + folder_name + ".mbtiles")
+os.system("rm -rf *.json")
+
+# move mbtiles file from json folder to mbtiles folder and then delete json folder
+os.system("mv " + folder_name + ".mbtiles " + os.path.abspath(mbtiles_path))
+os.system("rm -rf " + os.path.abspath(json_path))
+
+# ---------------------------------------------------------------------------------------
+# check how long it took to read h5 file data and create json files
+end_time =  time.clock()
+print ("time elapsed: " + str(end_time - start_time))
+# ---------------------------------------------------------------------------------------
