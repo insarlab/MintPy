3a78d349f3bf82f8cbe05b61b0009684f9a36058
Yunjun Zhang <yunjunzgeo@gmail.com>
2017-04-10 18:19:42 -0400

    add script range_distance.py to calculate range distance and write to file, similar to incidence_angle.h5

--- pysar/range_distance.py
+++ pysar/range_distance.py
@@ -1,12 +1,9 @@
 #! /usr/bin/env python
 ############################################################
-# Program is part of PySAR v1.0                            #
-# Copyright(c) 2013, Heresh Fattahi                        #
-# Author:  Heresh Fattahi                                  #
+# Program is part of PySAR v1.2                            #
+# Copyright(c) 2017, Zhang Yunjun                          #
+# Author:  Zhang Yunjun                                    #
 ############################################################
-# Yunjun, Jun 2016: use read() and write() for IO
-#                   add incidence_angle()
-# Yunjun, Feb 2017: move incidence_angle() to _pysar_utilities
 
 
 import sys
@@ -22,9 +19,9 @@ import pysar._pysar_utilities as ut
 
 def usage():
     print '''
-usage:  incidence_angle.py  file  [outfile]
+usage:  range_distance.py  file  [outfile]
 
-Generates incidence angles (in Radar Coordinate) for each pixel
+Generates range distance (in Radar Coordinate) for each pixel
   with required attributes read from the h5 file
 
 input arguments:
@@ -33,9 +30,9 @@ input arguments:
             calculated from file in radar coord
 
 example:
-  incidence_angle.py  velocity.h5
-  incidence_angle.py  timeseries.h5
-  incidence_angle.py  temporal_coherence.h5
+  range_distance.py  velocity.h5
+  range_distance.py  timeseries.h5
+  range_distance.py  temporal_coherence.h5
     '''
     return
 
@@ -47,23 +44,23 @@ def main(argv):
         usage();  sys.exit(1)
     
     try:    outFile = argv[1]
-    except: outFile = 'incidenceAngle.h5'
+    except: outFile = 'rangeDistance.h5'
     
     # Calculate look angle
-    angle = ut.incidence_angle(atr, dimension=2)
+    range_dis = ut.range_distance(atr, dimension=2)
     
     # Geo coord
     if 'Y_FIRST' in atr.keys():
-        print 'Input file is geocoded, only center incident angle is calculated: '
-        print angle
-        return angle
+        print 'Input file is geocoded, only center range distance is calculated: '
+        print range_dis
+        return range_dis
 
     # Radar coord
     else:
         print 'writing >>> '+outFile
         atr['FILE_TYPE'] = 'mask'
-        atr['UNIT'] = 'degree'
-        writefile.write(angle, atr, outFile)
+        atr['UNIT'] = 'm'
+        writefile.write(range_dis, atr, outFile)
         return outFile
 
 ############################################################
