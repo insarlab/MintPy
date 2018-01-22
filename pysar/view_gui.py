e396d52e1c1c97f98857c4616c55db133ff1248c
Joshua Zahner <joshuazahner@gmail.com>
2018-01-22 13:49:13 -0500

    Adjusted placement of option menu labels

--- pysar/view_gui.py
+++ pysar/view_gui.py
@@ -25,27 +25,20 @@ scalebar_distance, scalebar_lat, scalebar_lon, show_scalebar, save, output_file 
       None, None, None, None, None, None, None, None, None, None, None, None, None
 
 colormaps = ['Accent', 'Accent_r', 'Blues', 'Blues_r', 'BrBG', 'BrBG_r', 'BuGn', 'BuGn_r', 'BuPu', 'BuPu_r', 'CMRmap',
-             'CMRmap_r', 'Dark2', 'Dark2_r', 'GnBu', 'GnBu_r', 'Greens', 'Greens_r', 'Greys', 'Greys_r', 'OrRd',
-             'OrRd_r',
+             'CMRmap_r', 'Dark2', 'Dark2_r', 'GnBu', 'GnBu_r', 'Greens', 'Greens_r', 'Greys', 'Greys_r', 'OrRd', 'OrRd_r',
              'Oranges', 'Oranges_r', 'PRGn', 'PRGn_r', 'Paired', 'Paired_r', 'Pastel1', 'Pastel1_r', 'Pastel2',
              'Pastel2_r', 'PiYG', 'PiYG_r', 'PuBu', 'PuBuGn', 'PuBuGn_r', 'PuBu_r', 'PuOr', 'PuOr_r', 'PuRd', 'PuRd_r',
-             'Purples', 'Purples_r', 'RdBu', 'RdBu_r', 'RdGy', 'RdGy_r', 'RdPu', 'RdPu_r', 'RdYlBu', 'RdYlBu_r',
-             'RdYlGn',
+             'Purples', 'Purples_r', 'RdBu', 'RdBu_r', 'RdGy', 'RdGy_r', 'RdPu', 'RdPu_r', 'RdYlBu', 'RdYlBu_r', 'RdYlGn',
              'RdYlGn_r', 'Reds', 'Reds_r', 'Set1', 'Set1_r', 'Set2', 'Set2_r', 'Set3', 'Set3_r', 'Spectral',
-             'Spectral_r',
-             'Vega10', 'Vega10_r', 'Vega20', 'Vega20_r', 'Vega20b', 'Vega20b_r', 'Vega20c', 'Vega20c_r', 'Wistia',
+             'Spectral_r', 'Vega10', 'Vega10_r', 'Vega20', 'Vega20_r', 'Vega20b', 'Vega20b_r', 'Vega20c', 'Vega20c_r', 'Wistia',
              'Wistia_r', 'YlGn', 'YlGnBu', 'YlGnBu_r', 'YlGn_r', 'YlOrBr', 'YlOrBr_r', 'YlOrRd', 'YlOrRd_r', 'afmhot',
              'afmhot_r', 'autumn', 'autumn_r', 'binary', 'binary_r', 'bone', 'bone_r', 'brg', 'brg_r', 'bwr', 'bwr_r',
-             'cool', 'cool_r', 'coolwarm', 'coolwarm_r', 'copper', 'copper_r', 'cubehelix', 'cubehelix_r', 'flag',
-             'flag_r',
+             'cool', 'cool_r', 'coolwarm', 'coolwarm_r', 'copper', 'copper_r', 'cubehelix', 'cubehelix_r', 'flag', 'flag_r',
              'gist_earth', 'gist_earth_r', 'gist_gray', 'gist_gray_r', 'gist_heat', 'gist_heat_r', 'gist_ncar',
-             'gist_ncar_r',
-             'gist_rainbow', 'gist_rainbow_r', 'gist_stern', 'gist_stern_r', 'gist_yarg', 'gist_yarg_r', 'gnuplot',
-             'gnuplot2', 'gnuplot2_r', 'gnuplot_r', 'gray', 'gray_r', 'hot', 'hot_r', 'hsv', 'hsv_r', 'inferno',
-             'inferno_r',
+             'gist_ncar_r', 'gist_rainbow', 'gist_rainbow_r', 'gist_stern', 'gist_stern_r', 'gist_yarg', 'gist_yarg_r', 'gnuplot',
+             'gnuplot2', 'gnuplot2_r', 'gnuplot_r', 'gray', 'gray_r', 'hot', 'hot_r', 'hsv', 'hsv_r', 'inferno', 'inferno_r',
              'jet', 'jet_r', 'magma', 'magma_r', 'nipy_spectral', 'nipy_spectral_r', 'ocean', 'ocean_r', 'pink',
-             'pink_r',
-             'plasma', 'plasma_r', 'prism', 'prism_r', 'rainbow', 'rainbow_r', 'seismic', 'seismic_r', 'spectral',
+             'pink_r', 'plasma', 'plasma_r', 'prism', 'prism_r', 'rainbow', 'rainbow_r', 'seismic', 'seismic_r', 'spectral',
              'spectral_r', 'spring', 'spring_r', 'summer', 'summer_r', 'tab10', 'tab10_r', 'tab20', 'tab20_r', 'tab20b',
              'tab20b_r', 'tab20c', 'tab20c_r', 'terrain', 'terrain_r', 'viridis', 'viridis_r', 'winter', 'winter_r']
 
@@ -157,6 +150,7 @@ def show_file_info(file_info):
 
 
 def show_plot():
+
     options = [h5_file.get(), "-m", str(y_lim_lower.get()), "-M", str(y_lim_upper.get()), "--alpha",
                str(transparency.get()), "--figext", fig_ext.get(), "--fignum", fig_num.get(), "--coord", coords.get()]
 
@@ -306,6 +300,7 @@ def show_plot():
 
 
 def set_variables_from_attributes():
+
     subset_x_from.set(attributes['XMIN'])
     subset_y_from.set(attributes['YMIN'])
     subset_x_to.set(attributes['XMAX'])
@@ -1019,8 +1014,8 @@ def main():
     y_lim_lower_entry.pack(side=LEFT)
 
     unit_cmap_projection_labels_frame.pack(anchor='w', fill=X, pady=(10, 0), padx=10)
-    unit_label.pack(side=LEFT, padx=(0, 10))
-    colormap_label.pack(side=LEFT, padx=(0, 10))
+    unit_label.pack(side=LEFT, padx=(0, 20))
+    colormap_label.pack(side=LEFT, padx=(0, 20))
     projection_label.pack(side=LEFT)
 
     unit_cmap_projection_frame.pack(anchor='w', fill=X, pady=(10, 5), padx=10)
@@ -1111,8 +1106,8 @@ def main():
     show_ref_checkbutton.pack(side=LEFT, pady=10)
 
     reference_options_labels_frame.pack(anchor='w', fill=X, pady=(0, 0), padx=10)
-    ref_color_label.pack(side=LEFT, padx=(0, 10))
-    ref_symbol_label.pack(side=LEFT, padx=(0, 10))
+    ref_color_label.pack(side=LEFT, padx=(0, 20))
+    ref_symbol_label.pack(side=LEFT, padx=(0, 20))
     ref_date_label.pack(side=LEFT)
 
     reference_options_frame.pack(anchor='w', fill=X, pady=(5, 10), padx=10)
@@ -1155,8 +1150,8 @@ def main():
     fig_size_height_entry.pack(side=LEFT, padx=(0, 10))
 
     fig_ext_num_label_frame.pack(anchor='w', fill=X, padx=10, pady=(10, 0))
-    fig_ext_label.pack(side=LEFT, padx=(0, 10))
-    fig_num_label.pack(side=LEFT, padx=(0, 10))
+    fig_ext_label.pack(side=LEFT, padx=(0, 20))
+    fig_num_label.pack(side=LEFT)
 
     fig_ext_num_frame.pack(anchor='w', fill=X, padx=10, pady=(5, 10))
     fig_ext_option_menu.pack(side=LEFT, padx=(0, 10))
