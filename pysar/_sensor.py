4481aa1aebf37dbfab2f7fc8672dfb8283dbc155
Zhang Yunjun <yunjunz@users.noreply.github.com>
2018-03-14 20:22:39 -0400

    submit unfinished python2 scripts

---
+++ pysar/_sensor.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+############################################################
+# Program is part of PySAR v1.0                            #
+# Copyright(c) 2016, Yunjun Zhang                          #
+# Author:  Yunjun Zhang                                    #
+############################################################
+# Recommended Usage:
+#   import pysar._sensor as sensor
+#
+
+
+class JERS(object):
+
+    def __init__(self, file=None):
+        self.file = file
+        self.center_frequency = 1.275e9  # Hz
+        self.bandwidth = 15e6            # Hz
+        self.swath_width = 75e3          # meter
+        self.prf         = 1505.8
+        
+    
--- pysar/rewrap.py
+++ pysar/rewrap.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python2
 ############################################################
-# Program is part of PySAR v1.0                            #
+# Program is part of PySAR v1.2                            #
 # Copyright(c) 2013, Heresh Fattahi                        #
 # Author:  Heresh Fattahi                                  #
 ############################################################
@@ -12,20 +12,24 @@ import sys
 import h5py
 import numpy as np
 
+import pysar._datetime as ptime
+import pysar._readfile as readfile
+
 
 def usage():
-    print '''usage: rewrap.py  ifgram_file   [output_name]
+    print '''usage: rewrap.py  ifgram_file   [cycle_unit]
 
 Re-wrap unwraped interferograms to wrapped interferograms.
 
 example:
   rewrap.py  interferograms.h5
+  rewrap.py  timeseries_ECMWF_demErr_refDate.h5
     '''
     return
 
 
-def rewrap(unw):
-    rewrapped = unw - np.round(unw/(2*np.pi)) * 2*np.pi
+def rewrap(unw, cycle=2*np.pi):
+    rewrapped = unw - np.round(unw/cycle) * cycle
     return rewrapped
 
 
@@ -35,26 +39,55 @@ def main(argv):
     except:
         usage(); sys.exit(1)
 
-    try:    outfile = argv[1]
-    except: outfile = 'rewrapped_'+file
+    outfile = os.path.splitext(file)[0]+'_wrap'+os.path.splitext(file)[1]
+    one_cycle = 2*np.pi
+    one_cycle = 0.05
 
-    print 'writing >>> '+outfile
-    h5 = h5py.File(file, 'r')
-    h5out = h5py.File(outfile,'w')
-    gg = h5out.create_group('interferograms')
-    ifgramList = h5['interferograms'].keys()
-    print 'number of interferograms: '+str(len(ifgramList))
-    for ifgram in ifgramList:
-        print ifgram
-        unw = h5['interferograms'][ifgram].get(ifgram)[:]
-        rewrapped = rewrap(unw)
-        group = gg.create_group(ifgram)
-        dset = group.create_dataset(ifgram, data=rewrapped, compression='gzip')
-        for key, value in h5['interferograms'][ifgram].attrs.iteritems():
-            group.attrs[key] = value
+    atr = readfile.read_attribute(file)
+    k = atr['FILE_TYPE']
 
-    h5.close()
-    h5out.close()
+    if k in ['interferograms','coherence','wrapped','timeseries']:
+        h5 = h5py.File(file,'r')
+        epochList = sorted(h5[k].keys())
+        epoch_num = len(epochList)
+        prog_bar = ptime.progress_bar(maxValue=epoch_num)
+
+        print 'writing >>> '+outfile
+        h5out = h5py.File(outfile,'w')
+        group = h5out.create_group(k)
+
+        if k in ['interferograms','coherence','wrapped']:
+            date12_list = ptime.list_ifgram2date12(epochList)
+            print 'number of interferograms: '+str(len(epochList))
+            for i in range(epoch_num):
+                epoch = epochList[i]
+                data = h5[k][epoch].get(epoch)[:]
+
+                data_wrap = rewrap(data)
+
+                gg = group.create_group(epoch)
+                dset = gg.create_dataset(epoch, data=data_wrap, compression='gzip')
+                for key, value in h5[k][epoch].attrs.iteritems():
+                    gg.attrs[key] = value
+                prog_bar.update(i+1, suffix=date12_list[i])
+
+        elif k == 'timeseries':
+            print 'number of acquisitions: '+str(len(epochList))
+            for i in range(epoch_num):
+                epoch = epochList[i]
+                data = h5[k].get(epoch)[:]
+
+                data_wrap = rewrap(data, one_cycle)
+                
+                dset = group.create_dataset(epoch, data=data_wrap, compression='gzip')
+                prog_bar.update(i+1, suffix=epoch)
+            for key, value in h5[k].attrs.iteritems():
+                group.attrs[key] = value
+
+        h5.close()
+        h5out.close()
+        prog_bar.close()
+
     print 'Done.'
     return outfile
 
