e17661b684ae4defb30baad51ff0b11553306093
Joshua Zahner <joshuazahner@gmail.com>
2018-04-23 14:52:07 -0400

    Added ability to plot from file_info_viewer

--- pysar/file_info_viewer.py
+++ pysar/file_info_viewer.py
@@ -1,12 +1,20 @@
 #! /usr/bin/env python3
 
+
 from tkinter import *
 from tkinter import ttk
+from tkinter import filedialog
+import matplotlib
+matplotlib.use('TkAgg')
 import h5py
+import pysar.view as view
+import pysar.utils.readfile as readfile
 
-index = 1
-
-file = "/Users/joshua/Desktop/pysar/test_data/geometryGeo.h5"
+index = 2
+root = None
+file = None
+pick_h5_file_button = None
+tree = None
 
 
 def add_to_tree(name, obj):
@@ -18,41 +26,88 @@ def add_to_tree(name, obj):
         subindex += 1
     index += 1
 
-root = Tk()
 
-tree = ttk.Treeview(root)
-tree.configure(height=850)
+def add_attrs_to_tree(attrs):
+    group = tree.insert("", 1, text="Attributes", values=())
+    subindex = 0
+    for key in sorted(attrs.keys()):
+        tree.insert(group, subindex, text=key, values=attrs.get(key))
+        subindex += 1
 
-'''tree["columns"]=("one","two")
-tree.column("one", width=100 )
-tree.column("two", width=100)
-tree.heading("one", text="coulmn A")
-tree.heading("two", text="column B")
 
-tree.insert("" , 0,    text="Line 1", values=("1A","1b"))
+def plot_data():
 
-id2 = tree.insert("", 1, "dir2", text="Dir 2")
-tree.insert(id2, "end", "dir 2", text="sub dir 2", values=("2A","2B"))
+    atr = readfile.read_attribute(file.get())
+    file_type = atr['FILE_TYPE']
 
-##alternatively:
-tree.insert("", 3, "dir3", text="Dir 3")
-tree.insert("dir3", 3, text=" sub dir 3",values=("3A"," 3B"))
+    datasets = view.get_file_dataset_list(file.get(), file_type)
 
-tree.pack()'''
+    item = tree.focus()
+    the_item = tree.item(item)
+    epoch_num = the_item['text']
+    if epoch_num in datasets:
+        view.main([file.get(), epoch_num])
 
-tree.column("#0", width=285)
-tree["columns"] = ("Value")
-tree.column("Value", width=500)
-tree.heading("Value", text="Value")
-tree.insert("", 0, text="File", values=(file))
 
-h5file = h5py.File(file, 'r')
-h5file.visititems(add_to_tree)
-h5file.close()
+def pick_file():
+    global tree
 
-#tree.insert("", 0, text="FILE NAME", values=("Some File Name"))
-#tree.insert("", 1, text="FILE NAME 2", values=("Some File Name 2"))
+    if file.get() == "":
+        filename = filedialog.askopenfilename(initialdir="/", title="Select file",
+                                              filetypes=(("jpeg files", "*.h5"), ("all files", "*.*"), ("more files", "*.he5")))
+        if tree is not None:
+            tree.destroy()
 
-tree.pack()
+        file.set(filename)
+        setup_tree(file.get())
 
-root.mainloop()
+        pick_h5_file_button.config(text="Clear")
+
+    else:
+        file.set("")
+        pick_h5_file_button.config(text="Select .h5 File")
+
+
+def main():
+    global tree, file, pick_h5_file_button, root
+
+    root = Tk()
+    root.minsize(width=365, height=750)
+
+    reset_settings_file_frame = Frame(root)
+    reset_settings_file_frame.pack(side=TOP, fill="none", expand=True)
+
+    show_button = Button(reset_settings_file_frame, text="Show", width=10, command=plot_data)
+    show_button.pack(side=LEFT, pady=(10, 5))
+
+    file = StringVar()
+    h5_file_short = StringVar()
+    h5_file_short.set("No File Selected")
+
+    pick_h5_file_button = Button(reset_settings_file_frame, text='Select .h5 File', width=15,
+                                 command=pick_file)
+
+    pick_h5_file_button.pack(side=LEFT, pady=(10, 5))
+
+    root.mainloop()
+
+
+def setup_tree(file):
+    global tree, root
+
+    tree = ttk.Treeview(root)
+    tree.configure(height=40)
+    tree.column("#0", width=150)
+    tree["columns"] = "Value"
+    tree.column("Value", width=350)
+    tree.heading("Value", text="Value")
+    tree.insert("", 0, text="File", values=file)
+    h5file = h5py.File(file, 'r')
+    add_attrs_to_tree(h5file.attrs)
+    h5file.visititems(add_to_tree)
+    h5file.close()
+    tree.pack(side=TOP)
+
+
+if __name__ == '__main__':
+    main()
