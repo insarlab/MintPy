44f1902115f3c2a08ae85494726364a0a46910ca
Zhang Yunjun <yunjunz@users.noreply.github.com>
2018-06-03 14:02:27 -0400

    get rid of mit license in deramp.py

--- pysar/utils/deramp.py
+++ pysar/utils/deramp.py
@@ -1,34 +1,10 @@
-###############################################################################
-# Program is part of PySAR
-# Copyright(c) 2013-2018, Heresh Fattahi, Zhang Yunjun
-# Author:  Heresh Fattahi, Zhang Yunjun
-###############################################################################
-#  deramp are modified from a software originally
-#  written by Scott Baker with the following licence:
-#
-#  Copyright (c) 2011, Scott Baker
-#
-#  Permission is hereby granted, free of charge, to any person obtaining a
-#  copy of this software and associated documentation files (the "Software"),
-#  to deal in the Software without restriction, including without limitation
-#  the rights to use, copy, modify, merge, publish, distribute, sublicense,
-#  and/or sell copies of the Software, and to permit persons to whom the
-#  Software is furnished to do so, subject to the following conditions:
-#
-#  The above copyright notice and this permission notice shall be included
-#  in all copies or substantial portions of the Software.
-#
-#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-#  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-#  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-#  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-#  DEALINGS IN THE SOFTWARE.
-###############################################################################
+#####################################################################
+# Program is part of PySAR                                          #
+# Copyright(c) 2011-2018, Heresh Fattahi, Zhang Yunjun, Scott Baker #
+# Author:  Heresh Fattahi, Zhang Yunjun, Scott Baker                #
+#####################################################################
 # Recommend import:
 #     from pysar.utils import deramp
-
 
 
 import os
