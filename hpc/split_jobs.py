58f23e5b4c6d19bf28e6519da41763767274ccbe
Zhang Yunjun <yunjunz@users.noreply.github.com>
2019-03-10 01:47:48 -0500

    hpc: add scripts for job submission for HPC

---
+++ hpc/split_jobs.py
@@ -0,0 +1,229 @@
+#! /usr/bin/env python3
+############################################################
+# Program is part of PySAR                                 #
+# Copyright(c) 2016-2019, Zhang Yunjun, Heresh Fattahi     #
+# Author:  Zhang Yunjun, Heresh Fattahi                    #
+############################################################
+
+
+import sys
+import os
+import glob
+import time
+import argparse
+import datetime
+
+
+#####################################################################################
+EXAMPLE = """example:
+  split_jobs.py run_1_master -w 1:00 -r 2000 -e $NOTIFICATIONEMAIL
+"""
+
+CCS = """Pegasus Job Queues on UM/CCS
+   name       processors        memory         walltime             description
+                (cores)                      default / max
+  general        15-          25 GB max      1 day   / 7 days    -R "span[ptime=16]"
+  parallel       16+          25 GB max      1 day   / 7 days
+  bigmem         64 max      250 GB max      4 hours / 5 days
+  interactive    15-         250 GB max      6 hours / 1 day     max 1 job per user
+"""
+
+def create_parser():
+    parser = argparse.ArgumentParser(description='Split and submit batch command lines into job files',
+                                     formatter_class=argparse.RawTextHelpFormatter,
+                                     epilog=CCS+'\n'+EXAMPLE)
+
+    parser.add_argument('run_file', help='run file with >1 command line')
+    parser.add_argument('-w','--walltime', type=str, default='3:00',
+                        help='maximum time per job. Default: 3:00')
+    parser.add_argument('-r','--memory', type=str, default='3700',
+                        help='maximum RAM memoery in MB per command line. Default: 3700\n'
+                             'if N lines of commands in one job, still put memory for one command line,\n'
+                             'this script will calculate and request N*memory to CCS.')
+    parser.add_argument('-e','--email', dest='email', help='email address to send notification when job finished.')
+
+    parser.add_argument('-q','--queue', dest='queue_name', default='general', choices={'general','parallel','bigmem'}
+                        help='job queue to submit. Default: general')
+    parser.add_argument('-p','--project', dest='project_name', default='insarlab',
+                        help='project name for the job. Default: insarlab')
+
+    parser.add_argument('-n','--number', dest='num_processor', type=int, default=1,
+                        help='number of processors per job. Default: 1')
+    parser.add_argument('-l','--line', dest='num_cmd', type=int,
+                        help='number of command per job. Default: same as -n')
+    return parser
+
+
+def cmd_line_parse(iargs=None):
+    parser = create_parser()
+    inps = parser.parse_args(args=iargs)
+
+    inps.run_file = os.path.realpath(inps.run_file)
+
+    if not inps.num_cmd:
+        inps.num_cmd = inps.num_processor
+
+    inps.memory *= inps.num_cmd
+
+    return inps
+
+
+#####################################################################################
+def split_run_file(run_file, num_line_per_job):
+    # get number of lines in the input file
+    with open(run_file, 'r') as f:
+        for i, l in enumerate(f):
+            pass
+        num_line = i + 1
+    print('number of lines: {}'.format(num_line))
+    if num_line == 0:
+        raise ValueError('input run file is empty.')
+
+    # split run_file according to the num_processor
+    num_job = int((num_line - 0.5) / num_line_per_job) + 1
+    num_digit = len(str(num_job))  #length of suffixes
+
+    # Usage: split [OPTION]... [INPUT [PREFIX]]
+    prefix = 'z_input_{}.'.format(os.path.basename(inps.run_file))
+    cmd = 'split -a {a} -l {l} -d {i} {p}'.format(a=num_digit, l=num_line_per_job, i=run_file, p=prefix)
+    print(cmd)
+    os.system(cmd)
+
+    # search all split files
+    zfiles = glob.glob(prefix+'*')
+    return zfiles
+
+
+def wait4jobs2finish(run_file, num_job):
+    job_name = os.path.basename(run_file)
+    file_pattern = 'z_output_{}.*.o'.format(job_name)
+
+    print('-'*50)
+    print('sleeping until {} jobs are done for {}'.format(num_job, job_name))
+    t_sec = 0
+    num_file = len(glob.glob(file_pattern))
+    while num_file < num_job:
+        time.sleep(1)           #wait one second
+        if t_sec%60 == 0:       #every minute
+            print('# of {} files: <{}> out of <{}> after <{}> minutes'.format(
+                file_pattern, num_file, num_job, t_sec/60))
+        num_file = len(glob.glob(file_pattern))
+        t_sec += 1
+
+    print('-'*50)
+    print('ALL {} jobs are done for {}'.format(num_job, job_name))
+
+    m, s = divmod(t_sec, 60); h, m = divmod(m, 60)
+    print('Total used time: {:02d} hours {:02d} mins {:02d} secs'.format(h,m,s))
+    return
+
+
+def move_zfiles2job_folder(run_file):
+    """move z_* files into dedicated directory"""
+    # prepare job folder
+    job_folder = run_file+'_jobs'
+    print('moving z_* files into '+job_folder)
+
+    cmd = 'rm -rf {}'.format(job_folder)
+    os.system(cmd)
+
+    cmd = 'mkdir {}'.format(job_folder)
+    os.system(cmd)
+
+    # move z_input/output_* files
+    job_name = os.path.basename(run_file)
+    cmd = 'mv z_input_{j}.* z_output_{j}.* {d}'.format(j=job_name, d=job_folder)
+    os.system(cmd)
+    return
+
+
+def remove_zfiles(run_file):
+    job_name = os.path.basename(inps.run_file)
+    file_pattern1 = 'z_input_{}.*'.format(job_name)
+    file_pattern2 = 'z_output_{}.*'.format(job_name)
+    zfiles = glob.glob((file_pattern1, file_pattern2))
+    if len(zfiles) > 0:
+        cmd = 'rm {} {}'.format(file_pattern1, file_pattern2)
+        print(cmd)
+        os.system(cmd)
+    return
+
+
+#####################################################################################
+def main(iargs=None):
+    inps = cmd_line_parse(iargs)
+
+    job_dir = os.path.dirname(inps.run_file)
+    os.chdir(job_dir)
+    print('Go to job directory', job_dir)
+
+    job_name = os.path.basename(inps.run_file)
+    print('input run file: {}'.format(inps.run_file))
+
+    # Remove existing z_input/output_* files
+    remove_zfiles(inps.run_file)
+
+    # split run_file according to the num_processor
+    zfiles = split_run_file(run_file, num_line_per_job=inps.num_cmd)
+    num_file = len(zfiles)
+
+    # job setting
+    print('number of jobs to submit: {}'.format(num_file))
+    print('job queue: {}'.format(inps.queue_name))
+    print('walltime: {}'.format(inps.walltime))
+    print('memory: {}'.format(inps.memory))
+    print('number of processors: {}'.format(inps.num_processor))
+    print('email: {}'.format(inps.email))
+
+    # write and submit job files
+    for zfile in zfiles:
+        # write job setting
+        job_num = zfile.split('.')[-1]
+        job_file = zfile+'.sh'
+        f = open(job_file,'w')
+        f.write('#! /bin/bash')
+        f.write('\n#BSUB -J {}.{}'.format(job_name, job_num))
+        f.write('\n#BSUB -P {}'.format(inps.project_name))
+        f.write('\n#BSUB -o z_output_{}.{}.%J.o'.format(job_name, job_num))
+        f.write('\n#BSUB -e z_output_{}.{}.%J.e'.format(job_name, job_num))
+        f.write('\n#BSUB -W {}'.format(inps.walltime))
+        f.write('\n#BSUB -q {}'.format(inps.queue_name))
+        f.write('\n#BSUB -n {}'.format(inps.num_processor))
+        f.write('\n#BSUB -R "rusage[mem={}]"'.format(inps.memory))
+
+        if inps.queue_name == 'parallel':
+            f.write('\n#BSUB -R "span[ptile=16]"')
+
+        if inps.email:
+            f.write('\n#BSUB -u {}'.format(inps.email))
+            f.write('\n#BSUB -N')
+
+        # write cd work directory
+        f.write('\n')
+        f.write('\ncd {}'.format(job_dir))
+
+        # write job excutable commands
+        with open(zfile, 'r') as fz:
+            for line in fz:
+                f.write(line)
+        f.close()
+
+        # submit job file
+        cmd = 'bsub < '+job_file
+        print(cmd)
+        os.system(cmd)
+
+        cmd  = 'rm {}'.format(zfile)
+        os.system(cmd)
+
+    wait4jobs2finish(inps.run_file, num_job=num_file)
+
+    move_zfiles2job_folder(inps.run_file)
+
+    print('finished at {}'.format(datetime.datetime.now()))
+    return
+
+
+#####################################################################################
+if __name__ == '__main__':
+    main()
---
+++ hpc/submit_stripmap_stack.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+
+
+import os
+import time
+import argparse
+import subprocess
+
+
+#####################################################################################
+cDict = {}
+config = {};  config['walltime'] = '1:00';  config['memory'] = '2000';  cDict['run_1_master']               = config
+config = {};  config['walltime'] = '0:30';  config['memory'] = '2000';  cDict['run_2_focus_split']          = config
+config = {};  config['walltime'] = '1:00';  config['memory'] = '5500';  cDict['run_3_geo2rdr_coarseResamp'] = config
+config = {};  config['walltime'] = '1:00';  config['memory'] = '2000';  cDict['run_4_refineSlaveTiming']    = config
+config = {};  config['walltime'] = '0:30';  config['memory'] = '2000';  cDict['run_5_invertMisreg']         = config
+config = {};  config['walltime'] = '0:30';  config['memory'] = '2000';  cDict['run_6_fineResamp']           = config
+config = {};  config['walltime'] = '0:30';  config['memory'] = '2000';  cDict['run_7_grid_baseline']        = config
+config = {};  config['walltime'] = '1:00';  config['memory'] = '6000';  cDict['run_8_igram']                = config
+
+
+#####################################################################################
+EXAMPLE = """example:
+  bsub_stripmap_stack.py ./run_files -e $NOTIFICATION
+"""
+
+def create_parser():
+    parser = argparse.ArgumentParser(description='Submit the run files to jobs for ISCE/stripmapStack',
+                                     formatter_class=argparse.RawTextHelpFormatter,
+                                     epilog=EXAMPLE)
+
+    parser.add_argument('run_file_dir', help='directory of run_files')
+
+    bsub.add_argument_group('bsub option')
+    bsub.add_argument('--bsub', action='store_true', help='submit this script as a job to generic queue.')
+    bsub.add_argument('-r','--memory', type=int, default=1000, help='memory for bsub. Default: 1000')
+    bsub.add_argument('-w','--walltime', type=str, default='5:00', help='wall time for bsub. Default: 5:00')
+    bsub.add_argument('-e','--email', dest='email', help='email address to send notification when all jobs finished.')
+    return parser
+
+
+def cmd_line_parse(iargs=None):
+    parser = create_parser()
+    inps = parser.parse_args(args=iargs)
+    return inps
+
+
+#####################################################################################
+def run_job_submission4run_files():
+    for run_file in sorted(cDict.keys()):
+        config = cDict[run_file]
+        cmd = 'split_jobs.py -f {f} -r {r} -w {w} -e {e}'.format(f=run_file,
+                                                                 r=config['memory'],
+                                                                 w=config['walltime'],
+                                                                 e=email)
+        print(cmd)
+        status = subprocess.Popen(cmd, shell=True).wait()
+        if status is not 0:
+            raise RuntimeError("Error in step {}".format(run_file))
+    return status
+
+
+
+#####################################################################################
+def main(iargs=None):
+    inps = cmd_line_parse()
+    start_time = time.time()
+
+    if not inps.bsub:
+        run_job_submission4run_files()
+    else:
+        print('bsub option is ON')
+        # write run_stripmap_stack
+        run_file = 'run_stripmap_stack'
+        with open(run_file, 'w') as f:
+            f.write('submit_stripmap_stack.py {}\n'.format(inps.run_file_dir))
+        # submit job
+        cmd = 'split_jobs.py {} -r {} -w {}'.format(run_file, inps.memory, inps.walltime)
+        if inps.email:
+            cmd += ' -e {}'
+        print(cmd)
+        os.system(cmd)
+
+    # Timing
+    m, s = divmod(time.time()-start_time, 60)
+    print('\nTotal time: {:02.0f} mins {:02.1f} secs'.format(m, s))
+    return 
+
+#####################################################################################
+if __name__ == '__main__':
+    main()
