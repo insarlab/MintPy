b21c850739a16a847e3705984dae44f331ef9cdc
Zhang Yunjun <yunjunz@users.noreply.github.com>
2019-03-16 18:27:13 -0400

    bug fix in __init__

--- pysar/__init__.py
+++ pysar/__init__.py
@@ -24,7 +24,7 @@ __all__ = ['dem_error',
            'diff',
            'generate_mask',
            'ifgram_inversion',
-           'geocode'
+           'geocode',
            'load_data',
            'local_oscilator_drift',
            'modify_network',
