38d7e1cd50f5987133ed69c2c0d13583933bf5fd
Zhang Yunjun <yunjunz@users.noreply.github.com>
2019-04-27 23:32:49 -0400

    bugs fix for unwrap error correction
    docs/examples/input_files/GalapagosSenDT128.template: add pysar.load.waterMaskFile generated by ISCE
    
    pysar.unwrapError.waterMaskFile: change default value to waterMask.h5 or no if waterMask.h5 is not found; to be the same as pysar.network.waterMaskFile and pysar.networkInversion.waterMaskFile
    
    dem_error: bug fixed for 3D bperp reading when it has different number of acquisitions from the time-series file
    
    get rid of string yunjunz from the code in objects/sensor.py and version.py
    
    unwrap_error_phase_closure: fix bugs when there are interferograms excluded in ifgramStack.h5 file
    
    utils/writefile: fix bug in remove_hdf5_dataset() when input fname is not just basename.
    
    version: move code into get_release_info()

--- pysar/version.py
+++ pysar/version.py
@@ -1,15 +1,30 @@
 # grab version / date of the latest commit
 import os
 import subprocess
-_dir_orig = os.getcwd()
-os.chdir(os.path.dirname(os.path.dirname(__file__)))
-try:
-    release_version = subprocess.check_output(["git", "describe", "--tags"]).decode('utf-8').strip()
-    release_date = subprocess.check_output(["git", "log", "-1", "--date=short", "--format=%cd"]).decode('utf-8').strip()
-except:
-    release_version = 'v1.1.0-dev'
-    release_date = '2019-04-26'
-os.chdir(_dir_orig)
+
+
+def get_release_info(version='v1.1.0-dev', date='2019-04-26'):
+    """Grab version and date of the latest commit from a git repository"""
+    # go to the repository directory
+    dir_orig = os.getcwd()
+    os.chdir(os.path.dirname(os.path.dirname(__file__)))
+
+    # grab git info into string
+    try:
+        version_str = subprocess.check_output(["git", "describe", "--tags"])
+        version, num_commit = version_str.decode('utf-8').split('-')[:2]
+        version += '-{}'.format(num_commit)
+
+        date_str = subprocess.check_output(["git", "log", "-1", "--date=short", "--format=%cd"])
+        date = date_str.decode('utf-8').strip()
+    except:
+        pass
+
+    # go back to the original directory
+    os.chdir(dir_orig)
+    return version, date
+
+release_version, release_date = get_release_info()
 
 # generate_from: http://patorjk.com/software/taag/
 logo = """
@@ -23,11 +38,11 @@ ____/________(___/_(____/___/____|_/_____|_______
             (_ /                            
 
  A Python package for InSAR time series analysis.
-           PySAR v{v}, {d}
+           PySAR {v}, {d}
 _________________________________________________
 """.format(v=release_version, d=release_date)
 
-website = 'https://yunjunz.github.io/PySAR/'
+website = 'https://github.com/insarlab/PySAR'
 
-description = """PySAR version {v}, release date {d}""".format(v=release_version,
-                                                               d=release_date)
+description = """PySAR release version {v}, release date {d}""".format(v=release_version,
+                                                                       d=release_date)
