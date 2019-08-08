4e85a8e5efb51c02920cd5871aa42d3e4b79f132
ranneylxr <lvxr@radi.ac.cn>
2019-08-08 11:14:36 +0800

    version2 of save_geodmod.py:add -outdir and modified some bugs

--- mintpy/save_geodmod.py
+++ mintpy/save_geodmod.py
@@ -22,10 +22,10 @@ from mintpy.defaults.auto_path import autoPath
 
 ######################################################################################
 EXAMPLE = """example:
-  save_geodmod.py $SCRATCHDIR/DarbandikhanSenAT72/PYSAR/timeseries_ECMWF_demErr.h5 20171117_20180603 -b 34.2 35.2 45.0 46.3 -y 0.001 -x 0.001 -t unw -o AT20171117_20180603.unw 
-  save_geodmod.py $SCRATCHDIR/DarbandikhanSenAT72/PYSAR/INPUTS/ifgramStack.h5 20180603_20180615 -b 34.2 35.2 45.0 46.3 -y 0.001 -x 0.001 -t cor -o AT20180603_20180615.cor
-  save_geodmod.py $SCRATCHDIR/DarbandikhanSenAT72/PYSAR/temporalCoherence.h5 20171117_20180603 -b 34.2 35.2 45.0 46.3 -y 0.001 -x 0.001 -t cor -o AT20171117_20180603.cor
-  save_geodmod.py $SCRATCHDIR/DarbandikhanSenAT72/PYSAR/INPUTS/geometryRadar.h5 20171117_20180603 -b 34.2 35.2 45.0 46.3 -y 0.001 -x 0.001 -t dem -o AT.dem
+  save_geodmod.py $SCRATCHDIR/DarbandikhanSenAT72/PYSAR/timeseries_ECMWF_demErr.h5 20171117_20180603 -b 34.2 35.2 45.0 46.3 -y 0.001 -x 0.001 -t unw -o geo_20171117_20180603.unw -outdir $SCRATCHDIR/preGeodmod/ 
+  save_geodmod.py $SCRATCHDIR/DarbandikhanSenAT72/PYSAR/INPUTS/ifgramStack.h5 20180603_20180615 -b 34.2 35.2 45.0 46.3 -y 0.001 -x 0.001 -t cor -o geo_20180603_20180615.cor -outdir $SCRATCHDIR/preGeodmod/
+  save_geodmod.py $SCRATCHDIR/DarbandikhanSenAT72/PYSAR/temporalCoherence.h5 20171117_20180603 -b 34.2 35.2 45.0 46.3 -y 0.001 -x 0.001 -t cor -o geo_20171117_20180603.cor -outdir $SCRATCHDIR/preGeodmod/
+  save_geodmod.py $SCRATCHDIR/DarbandikhanSenAT72/PYSAR/INPUTS/geometryRadar.h5 20171117_20180603 -b 34.2 35.2 45.0 46.3 -y 0.001 -x 0.001 -t dem -o srtm.dem -outdir $SCRATCHDIR/preGeodmod/
 
 """
 
@@ -47,7 +47,9 @@ def create_parser():
     parser.add_argument('-t','--type',dest='Type',type=str,
                         help='The type of data with choice [unw, cor, dem]')
     parser.add_argument('-o', '--output', dest='outfile', nargs=1,
-                        help="output file name.")
+                        help='output file name')
+    parser.add_argument('-outdir','--outdir',dest='outdir',nargs=1,
+                        help='output directory')
     return parser
 
 
@@ -71,13 +73,13 @@ def get_path_com(path):
     (filename, extension) = os.path.splitext(tempfilename)
     return filepath, filename, extension
 
-def write_rsc_file(in_file,out_file):
+def write_rsc_file(inps,in_file,out_file):
     """ write rsc file for Geodmod just estract several properities from rsc file"""
     # read file
     meta = readfile.read_roipac_rsc(in_file)
     # initiate dict
     rsc = dict()
-    rsc['FILE_DIR'] = 'lxr/Iraq/afterslip_example'
+    rsc['FILE_DIR'] = "".join(inps.outdir)
     rsc['FILE_LENGTH'] = meta["FILE_LENGTH"]
     rsc['WIDTH'] = meta["WIDTH"]
     rsc['XMIN'] = 0
@@ -100,6 +102,32 @@ def write_rsc_file(in_file,out_file):
     writefile.write_roipac_rsc(rsc, out_file, print_msg=True)
     return out_file 
 
+def processdata(inps):
+    #use geocode.py and save_roipac.py to process data"
+    atr_asc = inps.file[0]
+    os.chdir(os.path.dirname(atr_asc))
+    
+    if os.path.exists("".join(inps.outdir))=='False':
+        os.mkdir("".join(inps.outdir))
+    asc_args = [atr_asc, '-b',inps.SNWE, '-y',inps.latStep, '-x',inps.lonStep, '--outdir',"".join(inps.outdir)]
+    print("geocode.py", asc_args)
+    args_str = format_args(asc_args)
+    mintpy.geocode.main(args_str.split())
+
+    os.chdir("".join(inps.outdir))
+    filename, extension = get_path_com(atr_asc)[1:3]
+    if filename=='ifgramStack':
+        asct_args = ['geo_'+filename+extension, 'coherence-'+inps.dset[0], '-o', inps.outfile]
+    elif filename=='temporalCoherence':     
+        asct_args = ['geo_'+filename+extension, '-o', inps.outfile]    
+    elif filename=='geometryRadar':
+        asct_args = ['geo_'+filename+extension, 'height', '-o', inps.outfile]        
+    else:
+        asct_args = ['geo_'+filename+extension, inps.dset[0], '-o', inps.outfile]
+    print("save_roipac.py", asct_args)
+    asct_str = format_args(asct_args)
+    mintpy.save_roipac.main(asct_str.split())
+
 ######################################################################################
 def main(iargs=None):
     inps = cmd_line_parse(iargs)
@@ -107,58 +135,24 @@ def main(iargs=None):
    
     if inps.Type=='unw':
         #  geocode timeseries**.h5 file and get the deformation field of two time periods
-        atr_asc = inps.file[0]
-        os.chdir(os.path.dirname(atr_asc))
-    
-        asc_args = [atr_asc, '-b',inps.SNWE, '-y',inps.latStep, '-x',inps.lonStep]
-        print("geocode.py", asc_args)
-        args_str = format_args(asc_args)
-        mintpy.geocode.main(args_str.split())
-    
-        _, filename, extension = get_path_com(atr_asc)
-        asct_args = ['geo_'+filename+extension, inps.dset[0], '-o', inps.outfile]
-        print("save_roipac.py", asct_args)
-        asct_str = format_args(asct_args)
-        mintpy.save_roipac.main(asct_str.split())
+        processdata(inps)
+
     elif inps.Type=='cor':
         #  geocode ifgramStack.h5 file and get the coherence of two time periods
-        atr_asc = inps.file[0]
-        os.chdir(os.path.dirname(atr_asc))
-        
-        asc_args = [atr_asc, '-b',inps.SNWE, '-y',inps.latStep, '-x',inps.lonStep]
-        print("geocode.py", asc_args)
-        args_str = format_args(asc_args)
-        mintpy.geocode.main(args_str.split())
-    
-        _, filename, extension = get_path_com(atr_asc)
-        if filename=='ifgramStack':
-            asct_args = ['geo_'+filename+extension, 'coherence-'+inps.dset[0], '-o', inps.outfile]
-        else:        
-            asct_args = ['geo_'+filename+extension, '-o', inps.outfile]
-        print("save_roipac.py", asct_args)
-        asct_str = format_args(asct_args)
-        mintpy.save_roipac.main(asct_str.split())
+        processdata(inps)
     else:
         #  geocode geometryRadar.h5 file and get the dem
-        atr_asc = inps.file[0]
-        os.chdir(os.path.dirname(atr_asc))
         
-        asc_args = [atr_asc, '-b',inps.SNWE, '-y',inps.latStep, '-x',inps.lonStep]
-        print("geocode.py", asc_args)
-        args_str = format_args(asc_args)
-        mintpy.geocode.main(args_str.split())
-    
-        _, filename, extension = get_path_com(atr_asc)
-        asct_args = ['geo_'+filename+extension, 'height', '-o', inps.outfile]
-        print("save_roipac.py", asct_args)
-        asct_str = format_args(asct_args)
-        mintpy.save_roipac.main(asct_str.split())
+        processdata(inps)
         outfile = format_args([format_args(inps.outfile) + '.rsc'])
-        rscdir = os.path.dirname(os.path.abspath(outfile))
-        print(rscdir)        
-        os.chdir(rscdir)
-        write_rsc_file(outfile,format_args([format_args(inps.outfile) +'.rsc1']))
-        # the extract properities was saved into rsc1 file 
+        #rscdir = os.path.dirname(os.path.abspath(outfile))
+        #print(rscdir)        
+        #os.chdir(rscdir)
+        write_rsc_file(inps,outfile,format_args([format_args(inps.outfile) +'.rsc1']))
+        os.remove(outfile)
+        print('rename *.rsc1 to *.rsc')
+        os.rename(format_args([format_args(inps.outfile) +'.rsc1']),outfile)
+        
         
 
 ######################################################################################
