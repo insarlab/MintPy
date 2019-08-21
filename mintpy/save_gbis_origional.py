4bf65e76267ca17623eaec1cd23af2797a194efa
ranneylxr <lvxr@radi.ac.cn>
2019-08-21 17:02:32 +0800

    initial version of save_gbis.py used in mimtpy

---
+++ mintpy/save_gbis_origional.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python3
+############################################################
+# Program is part of MintPy                                #
+# Copyright(c) 2019, Zhang Yunjun                          #
+# Author:  Zhang Yunjun                                    #
+############################################################
+
+
+
+import os
+import argparse
+import numpy as np
+import scipy.io as sio
+import matplotlib.pyplot as plt
+from mintpy.objects import sensor
+from mintpy.utils import ptime, readfile, utils as ut
+
+
+EXAMPLE = """example:
+  save_gbis.py velocity.h5 -g inputs/geometryGeo.h5 -o AlosDT73_20081012_20100302.mat
+  save_gbis.py 20150223_20161031_msk.unw -g inputs/geometryGeo.h5 -o Alos2DT23_20150223_20161031.mat
+"""
+
+def create_parser():
+    parser = argparse.ArgumentParser(description='Convert MintPy product to GBIS .mat format.',
+                                     formatter_class=argparse.RawTextHelpFormatter,
+                                     epilog=EXAMPLE)
+
+    parser.add_argument('file', help='deformation file.')
+    parser.add_argument('dset', nargs='?',
+                        help='date/date12 of timeseries, or date12 of interferograms to be converted')
+    parser.add_argument('-g','--geometry', dest='geom_file', required=True, help='geometry file')
+    parser.add_argument('-m', '--mask', dest='mask_file', help='mask file.')
+
+    parser.add_argument('--ref-lalo', dest='ref_lalo', type=float, nargs=2,
+                        help='custom reference pixel in lat/lon')
+    parser.add_argument('--nodisplay', dest='disp_fig', action='store_false',
+                        help='do not display the figure')
+    parser.add_argument('-o', '--output', dest='outfile', help='output file name.')
+    parser.add_argument('--out-dir', dest='outdir',
+                        help='custom output directory, ONLY IF --output is not specified.')
+    return parser
+
+
+def cmd_line_parse(iargs=None):
+    parser = create_parser()
+    inps = parser.parse_args(args=iargs)
+    inps.file = os.path.abspath(inps.file)
+    return inps
+
+
+def read_data(inps):
+    """
+    Returns: defo: 2D np.array with in-valid/masked-out pixel in NaN
+    """
+    # metadata
+    inps.metadata = readfile.read_attribute(inps.file)
+    k = inps.metadata['FILE_TYPE']
+    inps.range2phase =  -4. * np.pi / float(inps.metadata['WAVELENGTH'])
+    ext = os.path.splitext(inps.file)[1]
+
+    # mask
+    if inps.mask_file:
+        inps.mask = readfile.read(inps.mask_file)[0]
+    else:
+        inps.mask = np.ones((int(inps.metadata['LENGTH']),
+                             int(inps.metadata['WIDTH'])), dtype=np.bool_)
+
+    # data
+    if k in ['.unw','velocity']:
+        inps.phase = readfile.read(inps.file)[0]
+        if k == 'velocity':
+            # velocity to displacement
+            date1, date2 = inps.metadata['DATE12'].split('_')
+            dt1, dt2 = ptime.date_list2vector([date1, date2])[0]
+            inps.phase *= (dt2 - dt1).days / 365.25
+            # displacement to phase
+            inps.phase *= inps.range2phase
+
+        # update mask to exclude pixel with NaN value
+        inps.mask *= ~np.isnan(inps.phase)
+        # set all masked out pixel to NaN
+        inps.phase[inps.mask==0] = np.nan
+    else:
+        raise ValueError("input file not support yet: {}".format(k))
+    print('number of pixels: {}'.format(np.sum(inps.mask)))
+
+    # change reference point
+    if inps.ref_lalo:
+        coord = ut.coordinate(inps.metadata)
+        ref_lat, ref_lon = inps.ref_lalo
+        ref_y, ref_x = coord.geo2radar(ref_lat, ref_lon)[0:2]
+        # update data
+        inps.phase -= inps.phase[ref_y, ref_x]
+        # update metadata
+        inps.metadata['REF_LAT'] = ref_lat
+        inps.metadata['REF_LON'] = ref_lon
+        inps.metadata['REF_Y'] = ref_y
+        inps.metadata['REF_X'] = ref_x
+
+    # read geometry
+    inps.lat, inps.lon = ut.get_lat_lon(inps.metadata)
+    inps.inc_angle = readfile.read(inps.geom_file, datasetName='incidenceAngle')[0]
+    inps.head_angle = np.ones(inps.inc_angle.shape, dtype=np.float32) * float(inps.metadata['HEADING'])
+    inps.height = readfile.read(inps.geom_file, datasetName='height')[0]
+    inps.lat[inps.mask==0] = np.nan
+    inps.lon[inps.mask==0] = np.nan
+    inps.inc_angle[inps.mask==0] = np.nan
+    inps.head_angle[inps.mask==0] = np.nan
+    inps.height[inps.mask==0] = np.nan
+
+    # output filename
+    if not inps.outfile:
+        proj_name = sensor.project_name2sensor_name(inps.file)[1]
+        if not proj_name:
+            raise ValueError('No custom/auto output filename found.')
+        inps.outfile = '{}_{}.mat'.format(proj_name, inps.metadata['DATE12'])
+
+        if not inps.outdir:
+            inps.outdir = os.path.dirname(inps.file)
+        inps.outfile = os.path.join(inps.outdir, inps.outfile)
+    inps.outfile = os.path.abspath(inps.outfile)
+    return
+
+
+def plot_data(inps):
+    fig, axs = plt.subplots(nrows=2, ncols=3, figsize=[14, 7])
+    axs = axs.flatten()
+
+    # plot deformation
+    defo = inps.phase / inps.range2phase * 100. #convert to deformation in cm
+    dmin, dmax = np.nanmin(defo), np.nanmax(defo)
+    dlim = max(abs(dmin), abs(dmax))
+    im = axs[0].imshow(defo, vmin=-dlim, vmax=dlim, cmap='jet');
+    # reference point
+    axs[0].plot(int(inps.metadata['REF_X']),
+                int(inps.metadata['REF_Y']), 'ks', ms=6)
+    axs[0].set_title('Phase [{:.1f}, {:.1f}] um'.format(dmin, dmax));
+    # colorbar
+    cbar = fig.colorbar(im, ax=axs[0]);
+    cbar.set_label('cm')
+
+    # plot geometry
+    for ax, data, title in zip(axs[1:],
+                               [inps.lat, inps.lon, inps.inc_angle, inps.head_angle, inps.height],
+                               ['Latitude', 'Longitude', 'Incidence Angle', 'Head Angle', 'Height']):
+        im = ax.imshow(data, cmap='jet')
+        ax.set_title(title)
+        cbar = fig.colorbar(im, ax=ax)
+        if title == 'Height':
+            cbar.set_label('m')
+        else:
+            cbar.set_label('degree')
+
+    # save figure to file
+    out_fig = '{}.png'.format(os.path.splitext(inps.outfile)[0])
+    plt.savefig(out_fig, bbox_inches='tight', transparent=True, dpi=300)
+    print('saved figure to {}'.format(out_fig))
+    return
+
+
+def save2mat(inps):
+    """write mat file"""
+    mdict = {}
+    # required by GBIS
+    mdict['Heading'] = inps.head_angle[inps.mask].reshape(-1,1)
+    mdict['Inc'] = inps.inc_angle[inps.mask].reshape(-1,1)
+    mdict['Lat'] = inps.lat[inps.mask].reshape(-1,1)
+    mdict['Lon'] = inps.lon[inps.mask].reshape(-1,1)
+    mdict['Phase'] = inps.phase[inps.mask].reshape(-1,1)
+    # optional
+    mdict['Height'] = inps.height[inps.mask].reshape(-1,1)
+    mdict['Mask'] = inps.mask
+    mdict['Metadata'] = inps.metadata
+    # save to mat file
+    sio.savemat(inps.outfile, mdict, long_field_names=True)
+    print('save to file: {}.mat'.format(os.path.abspath(inps.outfile)))
+    return
+
+
+##############################################################################
+def main(iargs=None):
+    inps = cmd_line_parse(iargs)
+
+    read_data(inps)
+
+    plot_data(inps)
+
+    save2mat(inps)
+
+    if inps.disp_fig:
+        print('showing...')
+        plt.show()
+    return inps.outfile
+
+
+##########################################################################
+if __name__ == '__main__':
+    main()
--- mintpy/save_geodmod.py
+++ mintpy/save_geodmod.py
@@ -12,10 +12,11 @@ import mintpy
 import mintpy.workflow  #dynamic import for modules used by pysarApp workflow
 from mintpy.objects import sensor
 from mintpy.utils import readfile, writefile
+from mintpy.objects import timeseries
 
 ######################################################################################
 EXAMPLE = """example:
-  save_geodmod.py $SCRATCHDIR/DarbandikhanSenAT72/PYSAR/timeseries_ECMWF_demErr.h5 20171117_20180603 -b 34.2 35.2 45.0 46.3 -y 0.001 -x 0.001 -t unw -o geo_20171117_20180603.unw -outdir $SCRATCHDIR/preGeodmod/ 
+  save_geodmod.py timeseries_ECMWF_demErr.h5 -b 34.2 35.2 45.0 46.3 -y 0.001 -x 0.001 -startDate 20171117 -endDate 20180603 -outdir $MODELDIR 
   save_geodmod.py $SCRATCHDIR/DarbandikhanSenAT72/PYSAR/INPUTS/ifgramStack.h5 20180603_20180615 -b 34.2 35.2 45.0 46.3 -y 0.001 -x 0.001 -t cor -o geo_20180603_20180615.cor -outdir $SCRATCHDIR/preGeodmod/
   save_geodmod.py $SCRATCHDIR/DarbandikhanSenAT72/PYSAR/temporalCoherence.h5 20171117_20180603 -b 34.2 35.2 45.0 46.3 -y 0.001 -x 0.001 -t cor -o geo_20171117_20180603.cor -outdir $SCRATCHDIR/preGeodmod/
   save_geodmod.py $SCRATCHDIR/DarbandikhanSenAT72/PYSAR/INPUTS/geometryRadar.h5 20171117_20180603 -b 34.2 35.2 45.0 46.3 -y 0.001 -x 0.001 -t dem -o srtm.dem -outdir $SCRATCHDIR/preGeodmod/
@@ -23,12 +24,12 @@ EXAMPLE = """example:
 """
 
 def create_parser():
-    parser = argparse.ArgumentParser(description='Get horizontal and vertical displacements based on ascending and descending data',
+    parser = argparse.ArgumentParser(description='Prepare data for Geodmod software',
                                      formatter_class=argparse.RawTextHelpFormatter,
                                      epilog=EXAMPLE)
 
     parser.add_argument('file', nargs=1, help='ascending and descending timeseries files\n')
-    parser.add_argument('dset', nargs=1, help='date12 of timeseries to be converted')
+    #parser.add_argument('dset', nargs=1, help='date12 of timeseries to be converted')
     parser.add_argument('-b', '--bbox', dest='SNWE', type=float, nargs=4, metavar=('S', 'N', 'W', 'E'),
                         help='Bounding box of area to be geocoded.\n' +
                         'Include the uppler left corner of the first pixel' +
@@ -37,10 +38,10 @@ def create_parser():
                         help='output pixel size in degree in latitude.')
     parser.add_argument('-x', '--lonstep', dest='lonStep', type=float,
                         help='output pixel size in degree in longitude.')
-    parser.add_argument('-t','--type',dest='Type',type=str,
-                        help='The type of data with choice [unw, cor, dem]')
-    parser.add_argument('-o', '--output', dest='outfile', nargs=1,
-                        help='output file name')
+    #parser.add_argument('-o', '--output', dest='outfile', nargs=1,
+    #                    help='output file name')
+    parser.add_argument('-startDate','--startDate',dest='StartDate',nargs=?,help='date1 of timeseires to be converted')
+    parser.add_argument('-endDate','--endDate',dest='EndDate',nargs=?,help='date2 of timeseries to be converted')
     parser.add_argument('-outdir','--outdir',dest='outdir',nargs=1,
                         help='output directory')
     return parser
@@ -49,6 +50,28 @@ def create_parser():
 def cmd_line_parse(iargs=None):
     parser = create_parser()
     inps = parser.parse_args(args=iargs)
+    
+    # default startDate and endDate
+    atr = readfile.read_attribute(inps.file)
+    if not inps.StartDate:
+        inps.StartDate=atr['START_DATE']
+    if not inps.EndDate:
+        inps.EndDate=atr['END_DATE']
+    #dateList = None;
+    #if k in ['timeseries']:
+    #    dateList = timeseries(inps.file).get_date_list()
+    #if not inps.StartDate:
+    #    if k in ['timeseries']:
+    #       inps.StartDate=dateList[0]
+    #    else:
+    #        inps.StartDate=atr['START_DATE']
+    #   #datevector = ptime.date_list2vector(dateList)[1]
+    #if not inps.EndDate:
+    #    if k in ['timeseries']:
+    #        inps.EndDate=dateList[-1]
+    #    else:
+    #        inps.EndDate=atr['END_DATE']
+
     return inps
     
 def format_args(arr, dst=""):
@@ -99,25 +122,43 @@ def write_rsc_file(inps,in_file,out_file):
 def processdata(inps):
     #use geocode.py and save_roipac.py to process data"
     atr_asc = inps.file[0]
-    os.chdir(os.path.dirname(atr_asc))
+    # find data path
+    #os.chdir(os.path.dirname(atr_asc))
     
     if os.path.exists("".join(inps.outdir))=='False':
         os.mkdir("".join(inps.outdir))
+   
+    # process cor and dem dataset
+    corname='temporalCoherence'
+    cor_args = [corname, '-b',inps.SNWE, '-y',inps.latStep, '-x',inps.lonStep, '--outdir',"".join(inps.outdir)]
+    print("geocode.py", cor_args)
+    args_str = format_args(cor_args)
+    mintpy.geocode.main(args_str.split())
+    demname='geometryRadar'
+    dem_args = [demname, '-b',inps.SNWE, '-y',inps.latStep, '-x',inps.lonStep, '--outdir',"".join(inps.outdir)]
+    args_str = format_args(dem_args)
+    mintpy.geocode.main(args_str.split())
+   
+    #unw file
     asc_args = [atr_asc, '-b',inps.SNWE, '-y',inps.latStep, '-x',inps.lonStep, '--outdir',"".join(inps.outdir)]
     print("geocode.py", asc_args)
     args_str = format_args(asc_args)
     mintpy.geocode.main(args_str.split())
-
+        
+    #save dataset of unw cor and dem
     os.chdir("".join(inps.outdir))
     filename, extension = get_path_com(atr_asc)[1:3]
-    if filename=='ifgramStack':
-        asct_args = ['geo_'+filename+extension, 'coherence-'+inps.dset[0], '-o', inps.outfile]
-    elif filename=='temporalCoherence':     
-        asct_args = ['geo_'+filename+extension, '-o', inps.outfile]    
-    elif filename=='geometryRadar':
-        asct_args = ['geo_'+filename+extension, 'height', '-o', inps.outfile]        
-    else:
-        asct_args = ['geo_'+filename+extension, inps.dset[0], '-o', inps.outfile]
+    asct_args = ['geo_'+filename+extension, inps.StartDate,'_',inps.EndDate]
+    print("save_roipac.py", asct_args)
+    asct_str = format_args(asct_args)
+    mintpy.save_roipac.main(asct_str.split())     
+
+    asct_args = ['geo_temporalCoherence.h5', '-o', inps.StartDate,'_',inps.EndDate,'.cor']    
+    print("save_roipac.py", asct_args)
+    asct_str = format_args(asct_args)
+    mintpy.save_roipac.main(asct_str.split())
+    
+    asct_args = ['geo_geometryRadar.h5', 'height', '-o', 'srtm.dem']
     print("save_roipac.py", asct_args)
     asct_str = format_args(asct_args)
     mintpy.save_roipac.main(asct_str.split())
@@ -126,28 +167,17 @@ def processdata(inps):
 def main(iargs=None):
     inps = cmd_line_parse(iargs)
     
-   
-    if inps.Type=='unw':
-        #  geocode timeseries**.h5 file and get the deformation field of two time periods
-        processdata(inps)
-
-    elif inps.Type=='cor':
-        #  geocode ifgramStack.h5 file and get the coherence of two time periods
-        processdata(inps)
-    else:
-        #  geocode geometryRadar.h5 file and get the dem
-        
-        processdata(inps)
-        outfile = format_args([format_args(inps.outfile) + '.rsc'])
-        #rscdir = os.path.dirname(os.path.abspath(outfile))
-        #print(rscdir)        
-        #os.chdir(rscdir)
-        write_rsc_file(inps,outfile,format_args([format_args(inps.outfile) +'.rsc1']))
-        os.remove(outfile)
-        print('rename *.rsc1 to *.rsc')
-        os.rename(format_args([format_args(inps.outfile) +'.rsc1']),outfile)
-        
-        
+    #  geocode timeseries**.h5 file and get the deformation field of two time periods
+    #  and geocode ifgramStack.h5 file and get the coherence of two time periods
+    #  and geocode geometryRadar.h5 file and get the dem    
+    processdata(inps)
+    
+    # rename *.rsc1 to *.rsc
+    outfile = format_args(['srtm.dem' + '.rsc'])
+    write_rsc_file(inps,outfile,format_args(['srtm.dem' +'.rsc1']))
+    os.remove(outfile)
+    print('rename *.rsc1 to *.rsc')
+    os.rename(format_args(['srtm.dem' +'.rsc1']),outfile)
 
 ######################################################################################
 if __name__ == '__main__':
