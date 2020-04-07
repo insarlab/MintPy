dd5e266056978f745dec4955586b27dd9b4ac5c8
Zhang Yunjun <yunjunzgeo@gmail.com>
2020-04-06 22:04:12 -0700

    Update cluster.py

--- mintpy/objects/cluster.py
+++ mintpy/objects/cluster.py
@@ -1,21 +1,32 @@
-from dask_jobqueue import LSFCluster, PBSCluster, SLURMCluster
+try:
+    from dask_jobqueue import LSFCluster, PBSCluster, SLURMCluster
+except ImportError:
+    raise ImportError('Cannot import dask_jobqueue!')
 
 
 def get_cluster(cluster_type, **kwargs):
+
+    # check input cluster type
+    cluster_type = cluster_type.lower()
+    cluster_list = ['lsf','pbs','slurm']
+    if cluster_type not in cluster_list:
+        msg = "Cluster type '{}' not supported".format(cluster_type)
+        msg += '\nsupported cluster types: {}'.format(cluster_list)
+        raise ValueError()
+
     print("Using cluster type: {}".format(cluster_type))
     print("Using config name: {}".format(kwargs['config_name']))
 
-    cluster_type = cluster_type.lower()
+    # check walltime format for each cluster type
     kwargs['walltime'] = format_walltime_for_cluster(cluster_type, kwargs["walltime"])
 
+    # initiate cluster object
     if cluster_type == 'lsf':
         cluster = LSFCluster(**kwargs)
     elif cluster_type == 'pbs':
         cluster = PBSCluster(**kwargs)
     elif cluster_type == 'slurm':
         cluster = SLURMCluster(**kwargs)
-    else:
-        raise ValueError("Cluster type '{}' not supported".format(cluster_type))
 
     return cluster
 
