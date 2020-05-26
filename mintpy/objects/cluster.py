5e5b8fccaaf369a8734caddb70042194eb295472
Joshua Zahner <joshuazahner@gmail.com>
2020-05-26 13:26:56 -0500

    Created submit_workers function for submitting dask workers to the client scheduler

--- mintpy/objects/cluster.py
+++ mintpy/objects/cluster.py
@@ -5,8 +5,12 @@ from dask.distributed import LocalCluster, Client
 
 class DaskCluster:
 
-    def __init__(self, cluster_type, write_job_script=True, **kwargs):
+    def __init__(self, cluster_type, num_workers, write_job_script=True, **kwargs):
         """Generic dask cluster wrapper"""
+
+        self.cluster = None
+        self.client = None
+        self.num_workers = 0
 
         # Properly format cluster type for consistency
         cluster_type = cluster_type.lower()
@@ -47,6 +51,8 @@ class DaskCluster:
             self.cluster = jobqueue.PBSCluster(**kwargs)
         elif cluster_type == 'slurm':
             self.cluster = jobqueue.SLURMCluster(**kwargs)
+
+        self.cluster.scale(num_workers)
 
         if write_job_script:
             self.write_job_script()
@@ -94,3 +100,57 @@ class DaskCluster:
                 raise ValueError("input walltime ({}) not in HH:MM:SS or HH:MM format.".format(walltime))
 
         return walltime
+
+    def split_box2sub_boxes(self, box, num_split, dimension='x'):
+        """Further divides the box size into `num_split` different sub_boxes.
+        Note that this is different from `split2boxes()`, whic splits based on chunk_size (memory-based).
+
+        :param box: [x0, y0, x1, y1]: list[int] of size 4
+        :param num_split: int, the number of sub_boxes to split a box into
+        :param dimension: str = 'y' or 'x', the dimension along which to split the boxes
+        """
+        x0, y0, x1, y1 = box
+        length, width = y1 - y0, x1 - x0
+
+        sub_boxes = []
+        if dimension == 'y':
+            for i in range(num_split):
+                start = (i * length) // num_split + y0
+                end = ((i + 1) * length) // num_split + y0
+                sub_boxes.append([x0, start, x1, end])
+
+        else:
+            for i in range(num_split):
+                start = (i * width) // num_split + x0
+                end = ((i + 1) * width) // num_split + x0
+                sub_boxes.append([start, y0, end, y1])
+
+        return sub_boxes
+
+    def submit_workers(self, func, func_data):
+
+        # This line needs to be in a function or in a `if __name__ == "__main__":` block. If it is in no function
+        # or "main" block, each worker will try to create its own client (which is bad) when loading the module
+        print('Initiating dask client')
+        self.client = Client(self.cluster)
+
+        # split the primary box into sub boxes for each worker
+        box = func_data['box']
+        sub_boxes = self.split_box2sub_boxes(box, num_split=self.num_workers, dimension='x')
+        print('Split patch into {} sub boxes in x direction for workers to process'.format(len(sub_boxes)))
+
+        # submit jobs for each worker
+        start_time_sub = time.time()
+        futures = []
+        for i, sub_box in enumerate(sub_boxes):
+            print('Submit job to workers for sub box {}: {}'.format(i, sub_box))
+            func_data['box'] = sub_box
+
+            # David: I haven't played with fussing with `retries`, however sometimes a future fails
+            # on a worker for an unknown reason. retrying will save the whole process from failing.
+            # TODO:  I don't know what to do if a future fails > 3 times. I don't think an error is
+            # thrown in that case, therefore I don't know how to recognize when this happens.
+            future = self.client.submit(func, **func_data, retries=3)
+            futures.append(future)
+
+        return futures, start_time_sub
