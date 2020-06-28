51e38635ac8efa1d54077928a8d9fec22e0f65e3
Zhang Yunjun <yunjunzgeo@gmail.com>
2020-06-28 15:25:08 -0700

    utils1.get_lookup_file(): revert work_dir arg to fix bug (#389)
    * utils1.get_lookup_file(): revert work_dir arg to fix bug
    
    revert the previouly introduced work_dir argument to fix the bug it introduced; and replace it by adding more directory candicates to achive the same functionality.

--- mintpy/objects/coord.py
+++ mintpy/objects/coord.py
@@ -45,9 +45,7 @@ class coordinate:
         """
         self.src_metadata = metadata
         if lookup_file is None:
-            # search the default lookup file based on the directory of input file
-            work_dir = os.path.dirname(metadata['FILE_PATH'])
-            lookup_file = get_lookup_file(lookup_file, work_dir=work_dir, print_msg=False)
+            lookup_file = get_lookup_file(lookup_file, print_msg=False)
         if isinstance(lookup_file, str):
             lookup_file = [lookup_file, lookup_file]
         self.lookup_file = lookup_file
