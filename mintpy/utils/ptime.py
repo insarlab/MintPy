insarlab/MintPy

commit 8195e97d2f1dab56b3aa9c052bdc1c3cdc3ac1a3

bugfix in ptime.yyyymmdd2years()
fix a bug in yyyymmdd2years().

remove obsolete date_index()

move closest_weather_product_time() to tropo_pyaps.py

mintpy/utils/ptime.py
@@ -118,31 +118,36 @@ def decimal_year2datetime(years):
 
 
 def yyyymmdd2years(dates):
-    """Convert date(s) string into float number in the unit of year"""
+    """Convert date(s) string into float number in the unit of year
+
+    Parameters: dates - (list of) str, date in YYYYMMDD format
+    Returns:    years - (list of) float, years including the date and time info
+    """
 
     if isinstance(dates, str):
-        date_list = [dates]
+        dates = [dates]
     else:
-        date_list = list(dates)
+        dates = list(dates)
 
-    date_format = get_date_str_format(date_list[0])
+    date_format = get_date_str_format(dates[0])
 
-    y_list = []
+    years = []
     for date_str in dates:
         d = dt.strptime(date_str, date_format)
         y = (d.year + (d.timetuple().tm_yday - 1) / 365.25 + 
              d.hour / (365.25 * 24) + 
              d.minute / (365.25 * 24 * 60) + 
              d.second / (365.25 * 24 * 60 * 60))
-        y_list.append(y)
+        years.append(y)
 
     if isinstance(dates, str):
-        y_list = y_list[0]
+        years = years[0]
 
-    return y_list
+    return years
 
 
 def yymmdd2yyyymmdd(date):
+    """Convert date str from YYMMDD to YYYYMMDD format"""
     if date[0] == '9':
         date = '19'+date
     else:
@@ -151,6 +156,7 @@ def yymmdd2yyyymmdd(date):
 
 
 def yy2yyyy(year):
+    """Convert year str from YY to YYYY format"""
     if year[0] == '9':
         year = '19'+year
     else:
@@ -159,7 +165,7 @@ def yy2yyyy(year):
 
 
 def yyyymmdd(dates):
-    """Convert date strings in yymmdd format, if exists, to yyyymmdd format"""
+    """Convert date str from (YY)YYMMDD to YYYYMMDD format"""
     if isinstance(dates, str):
         if len(dates) == 6:
             datesOut = yymmdd2yyyymmdd(dates)
@@ -179,7 +185,7 @@ def yyyymmdd(dates):
 
 
 def yymmdd(dates):
-    """Convert date strings in yyyymmdd format, if exists, to yymmdd format"""
+    """Convert date str in (YY)YYMMDD to YYMMDD format"""
     if isinstance(dates, str):
         if len(dates) == 8:
             datesOut = dates[2:8]
@@ -262,14 +268,6 @@ def read_date_list(date_list_in, date_list_all=None):
 
 
 ################################################################
-def date_index(date_list):
-    date_inds = {}
-    for ni in range(len(date_list)):
-        date_inds[date_list[ni]] = ni
-    return date_inds
-
-
-################################################################
 def date_list2tbase(date_list):
     """Get temporal Baseline in days with respect to the 1st date
     Parameters: date_list - list of string, date in YYYYMMDD or YYMMDD format
@@ -318,29 +316,6 @@ def date_list2vector(date_list):
 
     return dates, datevector
 
-
-################################################################
-def closest_weather_product_time(sar_acquisition_time, grib_source='ECMWF'):
-    """Find closest available time of weather product from SAR acquisition time
-    Inputs:
-        sar_acquisition_time - string, SAR data acquisition time in seconds
-        grib_source - string, Grib Source of weather reanalysis product
-    Output:
-        grib_hr - string, time of closest available weather product
-    Example:
-        '06' = closest_weather_product_time(atr['CENTER_LINE_UTC'])
-        '12' = closest_weather_product_time(atr['CENTER_LINE_UTC'], 'NARR')
-    """
-    # Get hour/min of SAR acquisition time
-    sar_time = float(sar_acquisition_time)
-
-    # Find closest time in available weather products
-    grib_hr_list = [0, 6, 12, 18]
-    grib_hr = int(min(grib_hr_list, key=lambda x: abs(x-sar_time/3600.)))
-
-    # Adjust time output format
-    grib_hr = "%02d" % grib_hr
-    return grib_hr
 
 
 ###########################Simple progress bar######################

mintpy/tropo_pyaps.py
@@ -164,7 +164,7 @@ def check_inputs(inps):
     # hour
     if not inps.hour:
         if 'CENTER_LINE_UTC' in atr.keys():
-            inps.hour = ptime.closest_weather_product_time(atr['CENTER_LINE_UTC'], inps.trop_model)
+            inps.hour = closest_weather_product_time(atr['CENTER_LINE_UTC'], inps.trop_model)
         else:
             parser.print_usage()
             raise Exception('no input for hour')
@@ -250,6 +250,29 @@ def check_inputs(inps):
 
 
 ###############################################################
+def closest_weather_product_time(sar_acquisition_time, grib_source='ECMWF'):
+    """Find closest available time of weather product from SAR acquisition time
+    Inputs:
+        sar_acquisition_time - string, SAR data acquisition time in seconds
+        grib_source - string, Grib Source of weather reanalysis product
+    Output:
+        grib_hr - string, time of closest available weather product
+    Example:
+        '06' = closest_weather_product_time(atr['CENTER_LINE_UTC'])
+        '12' = closest_weather_product_time(atr['CENTER_LINE_UTC'], 'NARR')
+    """
+    # Get hour/min of SAR acquisition time
+    sar_time = float(sar_acquisition_time)
+
+    # Find closest time in available weather products
+    grib_hr_list = [0, 6, 12, 18]
+    grib_hr = int(min(grib_hr_list, key=lambda x: abs(x-sar_time/3600.)))
+
+    # Adjust time output format
+    grib_hr = "%02d" % grib_hr
+    return grib_hr
+
+
 def standardize_trop_model(tropModel, standardWeatherModelNames):
     tropModel = tropModel.replace('-', '').upper()
     if tropModel in standardWeatherModelNames.keys():

commit 31ab350362dcd331ca5d44f001f50ef7f18fb144

bugfix: ptime.yyyymmdd2years() returns list of str when given str (#413)
fix a bug introduced in #410 

Co-authored-by: Zhang Yunjun <yunjunzgeo@gmail.com>

mintpy/utils/ptime.py
@@ -124,15 +124,16 @@ def yyyymmdd2years(dates):
     Returns:    years - (list of) float, years including the date and time info
     """
 
+    # make a copy in list of input arg
     if isinstance(dates, str):
-        dates = [dates]
+        date_list = [dates]
     else:
-        dates = list(dates)
+        date_list = list(dates)
 
-    date_format = get_date_str_format(dates[0])
+    date_format = get_date_str_format(date_list[0])
 
     years = []
-    for date_str in dates:
+    for date_str in date_list:
         d = dt.strptime(date_str, date_format)
         y = (d.year + (d.timetuple().tm_yday - 1) / 365.25 + 
              d.hour / (365.25 * 24) + 