271bbaef6c3a6e9541b92d622462cb1b83b4b927
Robert Zinke <rzinke@sar.jpl.nasa.gov>
2020-11-16 21:15:54 -0800

    Updated mask.py to work with coherence files and others

--- mintpy/mask.py
+++ mintpy/mask.py
@@ -125,16 +125,15 @@ def mask_file(fname, mask_file, out_file, inps=None):
     maxDigit = max([len(i) for i in dsNames])
     dsDict = {}
     for dsName in dsNames:
-        if dsName not in ['coherence']:
-            print('masking {d:<{w}} from {f} ...'.format(d=dsName, w=maxDigit, f=fname))
-            data = readfile.read(fname, datasetName=dsName, print_msg=False)[0]
-            
-            # keep timeseries data as 3D matrix when there is only one acquisition
-            # because readfile.read() will squeeze it to 2D
-            if atr['FILE_TYPE'] == 'timeseries' and len(data.shape) == 2:
-                data = np.reshape(data, (1, data.shape[0], data.shape[1]))
+        print('masking {d:<{w}} from {f} ...'.format(d=dsName, w=maxDigit, f=fname))
+        data = readfile.read(fname, datasetName=dsName, print_msg=False)[0]
 
-            data = mask_matrix(data, mask, fill_value=inps.fill_value)
+        # keep timeseries data as 3D matrix when there is only one acquisition
+        # because readfile.read() will squeeze it to 2D
+        if atr['FILE_TYPE'] == 'timeseries' and len(data.shape) == 2:
+            data = np.reshape(data, (1, data.shape[0], data.shape[1]))
+
+        data = mask_matrix(data, mask, fill_value=inps.fill_value)
         dsDict[dsName] = data
 
     # default output filename
