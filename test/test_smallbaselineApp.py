d575134f794702b0f28426d89683cb8be3b95aa1
Zhang Yunjun <yunjunz@users.noreply.github.com>
2020-12-13 12:16:27 -0800

    circle CI: add SanFranSenDT42 v0.1 for testing
    + circle ci:
       - install gdal to be able to run prep_aria.py
       - add SanFranSenDT42 for testing for aria
    
    + test:
       - configs/SanFranSenDT42: add mintpy.load.x/ystep for testing
       - test_sbApp: switch SanFranSenDT42 from v1 to v0 for smaller size and faster download / uncompression

--- test/test_smallbaselineApp.py
+++ test/test_smallbaselineApp.py
@@ -27,7 +27,7 @@ URL_LIST = [
     'https://zenodo.org/record/3952953/files/FernandinaSenDT128.tar.xz',
     'https://zenodo.org/record/3952950/files/WellsEnvD2T399.tar.xz',
     'https://zenodo.org/record/3952917/files/KujuAlosAT422F650.tar.xz',
-    'https://zenodo.org/record/4265413/files/SanFranSenDT42.tar.xz',
+    'https://zenodo.org/record/4320005/files/SanFranSenDT42.tar.xz',
     'https://zenodo.org/record/4318134/files/WCapeSenAT29.tar.xz',
 ]
 
