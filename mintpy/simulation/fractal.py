c2cb60b837a2f153330af8cdabc99117e99ef584
scott <scott.stanie@gmail.com>
2021-01-07 13:02:43 -0600

    set k1 lower bound to 4*res in fractal.py

--- mintpy/simulation/fractal.py
+++ mintpy/simulation/fractal.py
@@ -96,7 +96,7 @@ def fractal_surface_atmos(shape=(128, 128), resolution=60., p0=1., freq0=1e-3,
     beta /= 2.
 
     kmax = np.max(k)
-    k1 = regime[0] * kmax
+    k1 = max(regime[0] * kmax, 4 * resolution)
     k2 = regime[1] * kmax
 
     regime1 = k <= k1
@@ -108,7 +108,7 @@ def fractal_surface_atmos(shape=(128, 128), resolution=60., p0=1., freq0=1e-3,
     fraction3 = np.power(k[regime3], beta[2])
 
     fraction = np.zeros(k.shape, np.float32)
-    fraction[regime1] = np.maximum(fraction1, 1)
+    fraction[regime1] = fraction1
     fraction[regime2] = fraction2 / np.min(fraction2) * np.max(fraction[regime1])
     fraction[regime3] = fraction3 / np.min(fraction3) * np.max(fraction[regime2])
 
