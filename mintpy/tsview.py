770c017bab774fb93b9f2a739d892683307ed2c6
Zhang Yunjun <yunjunz@users.noreply.github.com>
2021-01-14 12:04:26 -0800

    add solid_earth_tides.py
    + add solid_earth_tides.py to support solid Earth tides calculation and correction for time-series file.
    
    + speedup the calculation by >30 times via the step_size argument, by feeding the fortran code the coarse grid for calculation and text file writing, then resize the output into the same shape as the input size. This is due to the fact that the SET varies very slowly in space. Comparison with and without step_size shows a difference in vertical SET component up to 5e-8 m, which is negligible.
    
    + support input ts-file in radar coord, via:
       - prepare_los_geometry() to prepare the incidence angle and heading angle from geometry file in geo/radar coord into geo-coordinates, ready to be used by mintpy.objects.solid module
       - mintpy.geocode and mintpy.objects.resample module to geocode the geometry and radar-code the calculated tide

--- mintpy/tsview.py
+++ mintpy/tsview.py
@@ -245,8 +245,8 @@ def read_init_info(inps):
 
     # do not plot native reference point if it's out of the coverage due to subset
     if (inps.ref_yx and 'Y_FIRST' in atr.keys()
-        and inps.ref_yx == (int(atr['REF_Y']), int(atr['REF_X'])) 
-        and not (inps.pix_box[0] <= inps.ref_yx[1] < inps.pix_box[2] 
+        and inps.ref_yx == (int(atr['REF_Y']), int(atr['REF_X']))
+        and not (inps.pix_box[0] <= inps.ref_yx[1] < inps.pix_box[2]
                  and inps.pix_box[1] <= inps.ref_yx[0] < inps.pix_box[3])):
         inps.disp_ref_pixel = False
         print('the native REF_Y/X is out of subset box, thus do not display')
