01db97d052dc89acd30dc25b0c34ba96ae5c2f85
Zhang Yunjun <yunjunzgeo@gmail.com>
2021-03-19 13:25:20 -0700

    plot_sbApp.sh: add mising time-series files
    + utils.ptime.yyyymmdd(): bugfix for date str with "THHMM" suffix, such as the data for UAVSAR
    
    + sh/plot_sbApp.sh: - fix the bug of previously missing timeseries.h5 - apply the same generic find and plot syntax to geo_timeseries_* files

--- mintpy/sh/plot_smallbaselineApp.sh
+++ mintpy/sh/plot_smallbaselineApp.sh
@@ -2,8 +2,9 @@
 ###############################################################
 # Plot Results from Routine Workflow with smallbaselineApp.py
 # Author: Zhang Yunjun, 2017-07-23
-# Latest update: 2021-02-25
+# Latest update: 2021-03-08
 ###############################################################
+# Update the date above to enable auto copyover/overwrite
 
 
 ## Change to 0 if you do not want to re-plot loaded dataset again
@@ -45,24 +46,24 @@ view='view.py --nodisplay --dpi 150 --update '
 opt=' --dem '$dem_file' --mask '$mask_file' -u cm '
 #opt=' --dem '$dem_file' --mask '$mask_file' -u cm --vlim -2 2'
 if [ $plot_key_files -eq 1 ]; then
-    file=velocity.h5;              test -f $file && $view $file $opt               >> $log_file
-    file=temporalCoherence.h5;     test -f $file && $view $file -c gray --vlim 0 1 >> $log_file
-    file=maskTempCoh.h5;           test -f $file && $view $file -c gray --vlim 0 1 >> $log_file
-    file=inputs/geometryRadar.h5;  test -f $file && $view $file                    >> $log_file
-    file=inputs/geometryGeo.h5;    test -f $file && $view $file                    >> $log_file
+    file='velocity.h5';              test -f $file && $view $file $opt               >> $log_file
+    file='temporalCoherence.h5';     test -f $file && $view $file -c gray --vlim 0 1 >> $log_file
+    file='maskTempCoh.h5';           test -f $file && $view $file -c gray --vlim 0 1 >> $log_file
+    file='inputs/geometryRadar.h5';  test -f $file && $view $file                    >> $log_file
+    file='inputs/geometryGeo.h5';    test -f $file && $view $file                    >> $log_file
 fi
 
 
 ## Loaded Dataset
 if [ $plot_loaded_data -eq 1 ]; then
-    file=inputs/ifgramStack.h5
+    file='inputs/ifgramStack.h5'
     test -f $file && h5ls $file/unwrapPhase      && $view $file unwrapPhase- --zero-mask --wrap -c cmy >> $log_file
     test -f $file && h5ls $file/unwrapPhase      && $view $file unwrapPhase- --zero-mask               >> $log_file
     test -f $file && h5ls $file/coherence        && $view $file coherence-         --mask no -v 0 1    >> $log_file
     test -f $file && h5ls $file/connectComponent && $view $file connectComponent-  --mask no           >> $log_file
 
     # phase-unwrapping error correction
-    for dset in unwrapPhase_bridging unwrapPhase_phaseClosure unwrapPhase_bridging_phaseClosure; do
+    for dset in 'unwrapPhase_bridging' 'unwrapPhase_phaseClosure' 'unwrapPhase_bridging_phaseClosure'; do
         test -f $file && h5ls $file/$dset        && $view $file $dset-             --zero-mask        >> $log_file
     done
 fi
@@ -70,36 +71,34 @@ fi
 
 ## Auxliary Files from loaded dataset
 if [ $plot_loaded_data_aux -eq 1 ]; then
-    file=avgPhaseVelocity.h5;   test -f $file && $view $file                      >> $log_file
-    file=avgSpatialCoh.h5;      test -f $file && $view $file -c gray --vlim 0 1   >> $log_file
-    file=maskConnComp.h5;       test -f $file && $view $file -c gray --vlim 0 1   >> $log_file
+    file='avgPhaseVelocity.h5';   test -f $file && $view $file                      >> $log_file
+    file='avgSpatialCoh.h5';      test -f $file && $view $file -c gray --vlim 0 1   >> $log_file
+    file='maskConnComp.h5';       test -f $file && $view $file -c gray --vlim 0 1   >> $log_file
 fi
 
 
 ## Time-series files
 opt='--mask '$mask_file' --noaxis -u cm --wrap --wrap-range -10 10 '
 if [ $plot_timeseries -eq 1 ]; then
-    find . -name 'timeseries_*.h5' -exec $view {} $opt >> $log_file \;
+    file='timeseries.h5'; test -f $file && $view $file $opt >> $log_file
+    find . -name 'timeseries_*.h5' -exec   $view {}    $opt >> $log_file \;
 fi
 
 
 ## Geo coordinates for UNAVCO Time-series InSAR Archive Product
 if [ $plot_geocoded_data -eq 1 ]; then
-    file=./geo/geo_maskTempCoh.h5;                  test -f $file && $view $file -c gray  >> $log_file
-    file=./geo/geo_temporalCoherence.h5;            test -f $file && $view $file -c gray  >> $log_file
-    file=./geo/geo_velocity.h5;                     test -f $file && $view $file velocity >> $log_file
-    file=./geo/geo_timeseries_ECMWF_demErr_ramp.h5; test -f $file && $view $file --noaxis >> $log_file
-    file=./geo/geo_timeseries_ECMWF_demErr.h5;      test -f $file && $view $file --noaxis >> $log_file
-    file=./geo/geo_timeseries_demErr_ramp.h5;       test -f $file && $view $file --noaxis >> $log_file
-    file=./geo/geo_timeseries_demErr.h5;            test -f $file && $view $file --noaxis >> $log_file
+    file='./geo/geo_maskTempCoh.h5';          test -f $file && $view $file -c gray  >> $log_file
+    file='./geo/geo_temporalCoherence.h5';    test -f $file && $view $file -c gray  >> $log_file
+    file='./geo/geo_velocity.h5';             test -f $file && $view $file velocity >> $log_file
+    find . -name './geo/geo_timeseries_*.h5' -exec             $view {}    $opt     >> $log_file \;
 fi
 
 
 if [ $plot_the_rest -eq 1 ]; then
-    for tropo in ERA5 ECMWF MERRA NARR; do
-        file=velocity${tropo}.h5;   test -f $file && $view $file --mask no >> $log_file
+    for tropo in 'ERA5' 'ERAI' 'ECMWF' 'MERRA' 'NARR'; do
+        file='velocity'${tropo}'.h5';  test -f $file && $view $file --mask no >> $log_file
     done
-    file=numInvIfgram.h5;           test -f $file && $view $file --mask no >> $log_file
+    file='numInvIfgram.h5';            test -f $file && $view $file --mask no >> $log_file
 fi
 
 
