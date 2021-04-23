269f472345ecb1c0f7f81e9d931c441debf75372
Zhang Yunjun <yunjunzgeo@gmail.com>
2021-04-23 13:19:30 -0700

    save_kite: add comments on velocity as input file
    + add more comments on using velocity file as input
    
    + support geocoded ifgramStack file as input
    
    + grab date1/2 all the time

--- mintpy/save_kite.py
+++ mintpy/save_kite.py
@@ -10,7 +10,7 @@ import sys
 import argparse
 import datetime as dt
 import numpy as np
-from mintpy.utils import readfile
+from mintpy.utils import ptime, readfile
 
 try:
     from kite.scene import Scene, SceneConfig
@@ -23,19 +23,20 @@ r2d = 180. / np.pi
 
 
 EXAMPLE = """example:
-  #for Velocity [use step to export coseismic displacement]:
-  save_kite.py geo/geo_velocity.h5 -g geo/geo_geometry.h5
-  save_kite.py geo/geo_velocity.h5 -g geo/geo_geometry.h5 -m geo/geo_maskTempCoh.h5
-  save_kite.py geo/geo_velocity.h5 -d step20210104 -g geo/geo_geometry.h5 -m geo/geo_maskTempCoh.h5
+  ## displacement [event-type inversion]
+  # option 1: use velocity file with step estimation from timeseries2velocity.py for co-seismic displacement
   save_kite.py geo/geo_velocity.h5 -d step20210104 -g geo/geo_geometry.h5 -m geo/geo_maskTempCoh.h5 -o dsc
-  
-  #for Timeseries:
-  save_kite.py geo/geo_timeseries_ERA5_ramp_demErr.h5 -d 20101120_20110220 -g geo/geo_geometry.h5
-  save_kite.py geo/geo_timeseries_ERA5_ramp_demErr.h5 -d 20101120_20110220 -g geo/geo_geometry.h5 -m geo/geo_maskTempCoh.h5
+
+  # option 2: use time-series / ifgramStack file with date1_date2 for the transient displacement:
   save_kite.py geo/geo_timeseries_ERA5_ramp_demErr.h5 -d 20101120_20110220 -g geo/geo_geometry.h5 -m geo/geo_maskTempCoh.h5 -o dsc
-  
-  # IMPORT TO kite
-  spool outfile_name              % /do quadtree,covariance/aps and then File>Save Scene and it is ready for GROND or BEAT
+  save_kite.py geo/geo_ifgramStack.h5     -d unwrapPhase-20101120_20110220 -g geo/geo_geometry.h5 -m geo/geo_maskTempCoh.h5 -o dsc
+
+  ## velocity [interseismic or tensile dislocation inversion]
+  # https://pyrocko.org/beat/docs/current/examples/Rectangular_tensile.html
+  save_kite.py geo/geo_velocity.h5 -d velocity -g geo/geo_geometry.h5 -m geo/geo_maskTempCoh.h5 -o dsc
+
+  ## import to kite
+  spool outfile_name    % /do quadtree,covariance/aps and then File>Save Scene and it is ready for GROND or BEAT
 """
 
 KITE_URL = 'https://github.com/pyrocko/kite'
@@ -46,14 +47,17 @@ def create_parser():
                                      formatter_class=argparse.RawTextHelpFormatter,
                                      epilog=EXAMPLE)
 
-    parser.add_argument('file', help='file to be converted, in geo coordinate.')
-    parser.add_argument('-d', '--dset', '--dataset', dest='dset',
-                        help='velocity step, date of timeseries, or date12 of unw. interferograms to be converted')
-    parser.add_argument('-g', '--geom', dest='geom',
-                        help='insar geometry; incidence, azimuth, height')
-    parser.add_argument('-m', '--mask', dest='mask',
-                        help='mask file, use mask.py to create on')
-    parser.add_argument('-o', '--output', dest='outfile',
+    parser.add_argument('file', type=str, help='file to be converted, in geo coordinate.')
+    parser.add_argument('-d', '--dset', '--dataset', dest='dset', type=str, required=True,
+                        help='dataset of interest to be converted.\n'+
+                             'e.g.: velocity / stepYYYYMMDD for velocity HDF5 file,\n'+
+                             '      date12 in YYYYMMDD_YYYYMMDD for time-series HDF5 file,\n'+
+                             '      date12 in unwrapPhase-YYYYMMDD_YYYYMMDD for ifgramStack HDF5 file.')
+    parser.add_argument('-g', '--geom', dest='geom_file', type=str,
+                        help='geometry file for incidence /azimuth angle and height.')
+    parser.add_argument('-m', '--mask', dest='mask_file', type=str,
+                        help='mask file, or run mask.py to mask the input file beforehand.')
+    parser.add_argument('-o', '--output', dest='outfile', type=str,
                         help='output filename')
     return parser
 
@@ -140,43 +144,53 @@ def main(iargs=None):
     inps = cmd_line_parse(iargs)
 
     print('\n-------------------READ INPUTS -------------------')
-    # check reference date
     print('Read metadata from file: {}'.format(inps.file))
     attr = readfile.read_attribute(inps.file)
-    if attr['FILE_TYPE'] == 'timeseries' and inps.dset:
-        inps.ref_date, inps.dset = inps.dset.split('_')
-    else:
-        inps.ref_date, inps.dset = None, None
-
-    # read data
-    print('Read data from file: {}'.format(inps.file))
-    dis, attr = readfile.read(inps.file, datasetName=inps.dset)
-    if attr['FILE_TYPE'] == 'timeseries' and inps.ref_date:
-        dis -= readfile.read(inps.file, datasetName=inps.ref_date)[0]
-
-    #Mask data
-    if inps.mask is not None:
-        mask = readfile.read(inps.mask)[0]
-        print('Masking data')
-        dis[mask==0] = np.nan
-
-    if inps.ref_date is not None:
-        print('\nFirst  InSAR date: {}'.format(inps.ref_date))
-        print('Second InSAR date: {}'.format(inps.dset))
 
     # output filename
     if not inps.outfile:
         inps.outfile = attr['PROJECT_NAME']
 
+    # date1/2
+    if attr['FILE_TYPE'] in ['timeseries', 'HDFEOS']:
+        date1, date2 = inps.dset.split('_')
+        inps.dset = date2
+
+    elif attrs['FILE_TYPE'] == 'ifgramStack':
+        date1, date2 = inps.dset.split('-')[1].split('_')
+
+    else:
+        # velocity and *.unw files
+        date1, date2 = ptime.yyyymmdd(attrs['DATE12'].split('-'))
+        if inps.dset.startswith('step'):
+            date1 = inps.dset.split('step')[-1]
+            date2 = date1
+    print('First  InSAR date: {}'.format(date1))
+    print('Second InSAR date: {}'.format(date2))
+
+    # read data
+    print('Read {} from file: {}'.format(inps.dset, inps.file))
+    dis, attr = readfile.read(inps.file, datasetName=inps.dset)
+
+    if attr['FILE_TYPE'] == 'timeseries':
+        print('Read {} from file: {}'.format(date1, inps.file))
+        dis -= readfile.read(inps.file, datasetName=date1)[0]
+
+    # mask data
+    if inps.mask_file is not None:
+        mask = readfile.read(inps.mask_file)[0]
+        print('Set data to NaN for pixels with zero value in file: {}'.format(inps.mask_file))
+        dis[mask==0] = np.nan
+
     # read geometry incidence / azimuth angle
-    print('\nread incidence / azimuth angle from file: {}'.format(inps.geom))
-    inc_angle = readfile.read(inps.geom, datasetName='incidenceAngle')[0]
-    az_angle = readfile.read(inps.geom, datasetName='azimuthAngle')[0]
+    print('\nread incidence / azimuth angle from file: {}'.format(inps.geom_file))
+    inc_angle = readfile.read(inps.geom_file, datasetName='incidenceAngle')[0]
+    az_angle = readfile.read(inps.geom_file, datasetName='azimuthAngle')[0]
     print('Mean satellite incidence angle; {0:.2f}°'.format(np.nanmean(inc_angle)))
     print('Mean satellite heading angle: {0:.2f}°'.format(90 - np.nanmean(az_angle)))
 
     # create kite container
-    scene = mintpy2kite(dis, attr, inps.ref_date, inps.dset, inc_angle, az_angle, out_file=inps.out_file)
+    scene = mintpy2kite(dis, attr, date1, date2, inc_angle, az_angle, out_file=inps.out_file)
 
     return
 
