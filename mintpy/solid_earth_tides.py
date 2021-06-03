c7ed1dd16c47e575f9762bcafc9714c426959c85
Zhang Yunjun <yunjunzgeo@gmail.com>
2021-06-03 16:29:01 -0700

    Update solid_earth_tides.py

--- mintpy/solid_earth_tides.py
+++ mintpy/solid_earth_tides.py
@@ -286,6 +286,7 @@ def calc_solid_earth_tides_timeseries(ts_file, geom_file, set_file, date_wise_ac
     prog_bar.close()
 
     # radar-coding if input in radar-coordinates
+    # use ts_file to avoid potential missing CENTER_LINE_UTC attributes in geom_file from alosStack
     atr = readfile.read_attribute(ts_file)
     if 'Y_FIRST' not in atr.keys():
         print('radar-coding the LOS tides time-series ...')
