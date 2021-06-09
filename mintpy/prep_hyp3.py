3c9f801a74f7579fc3aa31274642dea5356e7199
forrestfwilliams <forrestfwilliams@icloud.com>
2021-06-09 14:29:47 +1200

    prep_hyp3: update to be compatible with current hyp3 products
    - removes hard coding of EARTH_RADIUS and HEIGHT
    - enables use of HyP3 DEM and incidence angle products
    - reorganizes main() and add_hyp3_metadata() functions
    - updates help information

--- mintpy/prep_hyp3.py
+++ mintpy/prep_hyp3.py
@@ -15,7 +15,6 @@ from mintpy.utils import readfile, writefile, utils as ut
 
 
 SPEED_OF_LIGHT = 299792458  # m/s
-EARTH_RADIUS = 6371e3       # Earth radius in meters
 
 
 EXAMPLE = """example:
@@ -24,8 +23,8 @@ EXAMPLE = """example:
   prep_hyp3.py  interferograms/*/*unw_phase_clip.tif
   prep_hyp3.py  interferograms/*/*corr_clip.tif
   prep_hyp3.py  interferograms/*/*clip.tif
-  prep_hyp3.py  dem.tif
-  prep_hyp3.py  dem_clip.tif
+  prep_hyp3.py  interferograms/*/*dem.tif
+  prep_hyp3.py  interferograms/*/*inc_map.tif
 """
 
 DESCRIPTION = """
@@ -34,8 +33,9 @@ DESCRIPTION = """
   2) S1AA_20161223T070700_20170116T070658_VVP024_INT80_G_ueF_74C2_corr.tif
   3) S1AA_20161223T070700_20170116T070658_VVP024_INT80_G_ueF_74C2.txt
 
-  A DEM is also needed e.g.:
-  1) dem.tif
+  A DEM filename is needed and a incidence angle filename is recommended  e.g.:
+  1) S1AA_20161223T070700_20170116T070658_VVP024_INT80_G_ueF_74C2_dem.tif
+  2) S1AA_20161223T070700_20170116T070658_VVP024_INT80_G_ueF_74C2_inc_map.tif
 
   This script will read these files, read the geospatial metadata from GDAL,
   find the corresponding HyP3 metadata file (for interferograms and coherence),
@@ -49,8 +49,9 @@ DESCRIPTION = """
           S1AA_20161223T070700_20170116T070658_VVP024_INT80_G_ueF_74C2_unw_phase.tif
           S1AA_20161223T070700_20170116T070658_VVP024_INT80_G_ueF_74C2_corr.tif
           S1AA_20161223T070700_20170116T070658_VVP024_INT80_G_ueF_74C2.txt
-      For the geometry file 1 file is needed:
-          dem.tif (a DEM with any name)
+      For the geometry file 2 file are recommended:
+          S1AA_20161223T070700_20170116T070658_VVP024_INT80_G_ueF_74C2_dem.tif (a required DEM)
+          S1AA_20161223T070700_20170116T070658_VVP024_INT80_G_ueF_74C2_inc_map.tif (an optional incidence angle)
 
   After running prep_hyp3.py:
       For each interferogram:
@@ -59,9 +60,11 @@ DESCRIPTION = """
           S1AA_20161223T070700_20170116T070658_VVP024_INT80_G_ueF_74C2_corr.tif
           S1AA_20161223T070700_20170116T070658_VVP024_INT80_G_ueF_74C2_corr.tif.rsc
           S1AA_20161223T070700_20170116T070658_VVP024_INT80_G_ueF_74C2.txt
-      For the input DEM geometry file:
-          dem.tif
-          dem.tif.rsc
+      For the input geometry files:
+          S1AA_20161223T070700_20170116T070658_VVP024_INT80_G_ueF_74C2_dem.tif
+          S1AA_20161223T070700_20170116T070658_VVP024_INT80_G_ueF_74C2_dem.tif.rsc
+          S1AA_20161223T070700_20170116T070658_VVP024_INT80_G_ueF_74C2_inc_map.tif
+          S1AA_20161223T070700_20170116T070658_VVP024_INT80_G_ueF_74C2_inc_map.tif.rsc
 
   Notes:
     HyP3 currently only supports generation of Sentinel-1 interferograms, so
@@ -94,56 +97,50 @@ def cmd_line_parse(iargs=None):
 def add_hyp3_metadata(fname,meta,is_ifg=True):
     '''Read/extract attribute data from HyP3 metadata file and add to metadata dictionary
     Inputs:
-        *unw_phase.tif or *corr.tif file name, *dem.tif e.g.
+        *unw_phase.tif, *corr.tif file name, *dem.tif, *inc_map.cfg e.g.
             S1AA_20161223T070700_20170116T070658_VVP024_INT80_G_ueF_74C2_unw_phase.tif
+            S1AA_20161223T070700_20170116T070658_VVP024_INT80_G_ueF_74C2_unw_phase_clip.tif
             S1AA_20161223T070700_20170116T070658_VVP024_INT80_G_ueF_74C2_corr.tif
-            dem.tif
+            S1AA_20161223T070700_20170116T070658_VVP024_INT80_G_ueF_74C2_corr_clip.tif
+            S1AA_20161223T070700_20170116T070658_VVP024_INT80_G_ueF_74C2_dem.tif
+            S1AA_20161223T070700_20170116T070658_VVP024_INT80_G_ueF_74C2_dem_clip.tif
         Metadata dictionary (meta)
     Output:
         Metadata dictionary (meta)
     '''
 
+    # determine interferogram pair info and hyp3 metadata file name
+    sat, date1_string, date2_string, pol, res, soft, proc, ids, *_ = os.path.basename(fname).split('_')
+    job_id = '_'.join([sat, date1_string, date2_string, pol, res, soft, proc, ids])
+    directory = os.path.dirname(fname)
+    meta_file = f'{os.path.join(directory,job_id)}.txt'
+
+    # open and read hyp3 metadata
+    hyp3_meta = {}
+    with open(meta_file, 'r') as f:
+        for line in f:
+            key, value = line.strip().split(': ')
+            hyp3_meta[key] = value
+
+    # add universal hyp3 metadata
+    meta['PROCESSOR'] = 'hyp3'
+    meta['HEADING'] = hyp3_meta['Heading']
+    meta['ALOOKS'] = hyp3_meta['Azimuth looks']
+    meta['RLOOKS'] = hyp3_meta['Range looks']
+    meta['P_BASELINE_TOP_HDR'] = hyp3_meta['Baseline']
+    meta['P_BASELINE_BOTTOM_HDR'] = hyp3_meta['Baseline']
+    meta['EARTH_RADIUS'] = hyp3_meta['Earth radius at nadir']
+    meta['HEIGHT'] = hyp3_meta['Spacecraft height']
+
+    # add metadata that is only relevant to interferogram files
     if is_ifg:
-        # determine interferogram pair info and hyp3 metadata file name
-        sat, date1_string, date2_string, pol, res, soft, proc, ids, *_ = os.path.basename(fname).split('_')
-
-        job_id = '_'.join([sat, date1_string, date2_string, pol, res, soft, proc, ids])
-        directory = os.path.dirname(fname)
-        meta_file = f'{os.path.join(directory,job_id)}.txt'
-
         date1 = datetime.strptime(date1_string,'%Y%m%dT%H%M%S')
         date2 = datetime.strptime(date2_string,'%Y%m%dT%H%M%S')
         date_avg = date1 + (date2 - date1) / 2
         date_avg_seconds = (date_avg - date_avg.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds()
 
-        # open and read hyp3 metadata
-        hyp3_meta = {}
-        with open(meta_file, 'r') as f:
-            for line in f:
-                key, value = line.strip().split(': ')
-                hyp3_meta[key] = value
-
-        # add relevant data to meta object
         meta['CENTER_LINE_UTC'] = date_avg_seconds
         meta['DATE12'] = f'{date1.strftime("%y%m%d")}-{date2.strftime("%y%m%d")}'
-        meta['HEADING'] = hyp3_meta['Heading']
-        meta['ALOOKS'] = hyp3_meta['Azimuth looks']
-        meta['RLOOKS'] = hyp3_meta['Range looks']
-        meta['P_BASELINE_TOP_HDR'] = hyp3_meta['Baseline']
-        meta['P_BASELINE_BOTTOM_HDR'] = hyp3_meta['Baseline']
-
-    else:
-        meta_file = os.path.splitext(fname)[0]+'.txt'
-
-        # open and read hyp3 metadata
-        hyp3_meta = {}
-        with open(meta_file, 'r') as f:
-            for line in f:
-                key, value = line.strip().split(': ')
-                hyp3_meta[key] = value
-
-        # add relevant data to meta object
-        meta['HEADING'] = hyp3_meta['Heading']
 
     return(meta)
 
@@ -159,13 +156,9 @@ def add_sentinel1_metadata(meta):
 
     # note: HyP3 currently only supports Sentinel-1 data, so Sentinel-1
     #       configuration is hard-coded.
-
-    meta['PROCESSOR'] = 'hyp3'
     meta['PLATFORM'] = 'Sen'
     meta['ANTENNA_SIDE'] = -1
     meta['WAVELENGTH'] = SPEED_OF_LIGHT / sensor.SEN['carrier_frequency']
-    meta['HEIGHT'] = 693000.0 # nominal altitude of Sentinel1 orbit
-    meta['EARTH_RADIUS'] = EARTH_RADIUS
 
     # add LAT/LON_REF1/2/3/4 based on whether satellite ascending or descending
     meta['ORBIT_DIRECTION'] = 'ASCENDING' if float(meta['HEADING']) > -90 else 'DESCENDING'
@@ -207,12 +200,11 @@ def main(iargs=None):
     for fname in inps.file:
         meta = readfile.read_gdal_vrt(fname)
 
-        if any([x in fname for x in ['unw_phase','corr','inc_map']]):
-            meta = add_hyp3_metadata(fname, meta, is_ifg=True)
-        else:
-            meta = add_hyp3_metadata(fname, meta, is_ifg=False)
+        is_ifg = any([x in fname for x in ['unw_phase','corr']])
 
+        meta = add_hyp3_metadata(fname, meta, is_ifg=is_ifg)
         meta = add_sentinel1_metadata(meta)
+
         rsc_file = fname+'.rsc'
         writefile.write_roipac_rsc(meta, out_file=rsc_file)
 
