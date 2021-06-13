2a30010249231148296289245304b597140f9ccf
Bryan Marfito <bjmarfito@yahoo.com>
2021-06-13 19:33:38 +0800

    Update tropo_pyaps3.py

--- mintpy/tropo_pyaps3.py
+++ mintpy/tropo_pyaps3.py
@@ -657,11 +657,6 @@ def calc_delay_timeseries(inps):
     geom_obj.open()
     inps.inc = geom_obj.read(datasetName='incidenceAngle')
     inps.dem = geom_obj.read(datasetName='height')
-
-    #Inc angle of HyP3 is in radians
-    #Converts degree to radians
-    if 'hyp3' in geom_obj.metadata['PROCESSOR']:
-        inps.inc *=180 / np.pi
         
     # for testing
     if inps.custom_height:
