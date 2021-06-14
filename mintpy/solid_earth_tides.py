3988fd02efa904b9225457be01e1e9e532c5aff2
Bryan Marfito <bjmarfito@yahoo.com>
2021-06-14 08:25:51 +0800

    Update solid_earth_tides.py

--- mintpy/solid_earth_tides.py
+++ mintpy/solid_earth_tides.py
@@ -135,7 +135,26 @@ def prepare_los_geometry(geom_file):
     else:
         print('use the HEADING attribute as the mean heading angle')
         head_angle = np.ones(inc_angle.shape, dtype=np.float32) * float(atr['HEADING'])
-        
+    
+    # 'Y_FIRST' not in 'degree'
+    # e.g. meters for UTM projection from ASF HyP3
+    if not atr['Y_UNIT'].lower().startswith('deg'):
+        length, width = int(atr['LENGTH']), int(atr['WIDTH'])
+        lat0 = float(atr['Y_FIRST'])
+        lon0 = float(atr['X_FIRST'])
+        lat_step = float(atr['Y_STEP'])
+        lon_step = float(atr['X_STEP'])
+        lat1 = lat0 + lat_step * (length - 1)
+        lon1 = lon0 + lon_step * (width - 1)
+        lat0, lon0 = ut.to_latlon(atr['OG_FILE_PATH'], lon0, lat0)
+        lat1, lon1 = ut.to_latlon(atr['OG_FILE_PATH'], lon1, lat1)
+        lat_step = (lat1 -lat0) / (length - 1)
+        lon_step = (lon1 -lon0) / (width - 1)
+        atr['Y_FIRST'] = lat0
+        atr['X_FIRST'] = lon0
+        atr['Y_STEP'] = lat_step
+        atr['X_STEP'] = lon_step
+
     # geocode inc/az angle data if in radar-coord
     if 'Y_FIRST' not in atr.keys():
         print('-'*50)
@@ -251,6 +270,10 @@ def calc_solid_earth_tides_timeseries(ts_file, geom_file, set_file, date_wise_ac
 
     # prepare LOS geometry: geocoding if in radar-coordinates
     inc_angle, head_angle, atr_geo = prepare_los_geometry(geom_file)
+    inc_angle[inc_angle==0] = np.nan
+    head_angle[head_angle==0] = np.nan
+
+    
     # get LOS unit vector
     with warnings.catch_warnings():
         warnings.simplefilter("ignore", category=RuntimeWarning)
