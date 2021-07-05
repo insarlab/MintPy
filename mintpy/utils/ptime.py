prep_fringe: fix sign issue & add --sub-x/y --ps-mask options (#594)
+ prep_fringe:
   - add --sub-x/y option to load AOI from fringe to mintpy
   - add --ps-mask option to load PS mask from fringe to mintpy
   - fix sign issue, as confirmed by Yujie.

+ subset: change the default prefix of the output file name from 'subset_' to 'sub_'

+ utils.ptime: add utc2solar_time() to convert UTC time to the local solar time.

+ docs/conda.txt: comment out `pyhdf` because it's only used for MERRA, which is currently not supported, thus not needed.

mintpy/utils/ptime.py
@@ -10,7 +10,7 @@ import os
 import sys
 import re
 import time
-from datetime import datetime as dt, timedelta
+import datetime as dt
 import numpy as np
 from mintpy.objects.progress import progressBar
 
@@ -62,7 +62,7 @@ def round_seconds(datetime_obj):
     """
     datetime_obj_out = datetime_obj
     if datetime_obj_out.microsecond >= 5e5:
-        datetime_obj_out += timedelta(seconds=1)
+        datetime_obj_out += dt.timedelta(seconds=1)
     return datetime_obj_out.replace(microsecond=0)
 
 
@@ -74,7 +74,7 @@ def yyyymmdd2season(date_str):
     """
     # get day of the year
     date_str = yyyymmdd(date_str)
-    yday = dt.strptime(date_str, "%Y%m%d").timetuple().tm_yday
+    yday = dt.datetime.strptime(date_str, "%Y%m%d").timetuple().tm_yday
 
     # determine the season
     season = None
@@ -94,9 +94,9 @@ def datenum2datetime(datenum):
     Parameters: datenum : Date in datenum format, i.e. 731763.5
     Returns:    datetime: Date in datetime.datetime format, datetime.datetime(2003, 7, 1, 12, 0)
     """
-    return dt.fromordinal(int(datenum)) \
-           + timedelta(days=datenum % 1) \
-           - timedelta(days=366)
+    return dt.datetime.fromordinal(int(datenum)) \
+           + dt.timedelta(days=datenum % 1) \
+           - dt.timedelta(days=366)
 
 
 def decimal_year2datetime(years):
@@ -110,7 +110,7 @@ def decimal_year2datetime(years):
         yday = np.floor((x - year) * 365.25).astype(int) + 1
         x2 = '{:d}-{:d}'.format(year, yday)
         try:
-            xt = dt.strptime(x2, "%Y-%j")
+            xt = dt.datetime.strptime(x2, "%Y-%j")
         except:
             raise ValueError('wrong format: ',x)
         return xt
@@ -145,7 +145,7 @@ def yyyymmdd2years(dates):
 
     years = []
     for date_str in date_list:
-        d = dt.strptime(date_str, date_format)
+        d = dt.datetime.strptime(date_str, date_format)
         y = (d.year + (d.timetuple().tm_yday - 1) / 365.25 +
              d.hour / (365.25 * 24) +
              d.minute / (365.25 * 24 * 60) +
@@ -290,7 +290,7 @@ def date_list2tbase(date_list):
     # date str to dt object
     date_list = yyyymmdd(date_list)
     date_format = get_date_str_format(str(date_list))
-    dates = [dt.strptime(i, date_format) for i in date_list]
+    dates = [dt.datetime.strptime(i, date_format) for i in date_list]
 
     # dt object to time difference in days
     tbase = []
@@ -315,7 +315,7 @@ def date_list2vector(date_list):
     """
     date_list = yyyymmdd(date_list)
     date_format = get_date_str_format(str(date_list))
-    dates = [dt.strptime(i, date_format) for i in date_list]
+    dates = [dt.datetime.strptime(i, date_format) for i in date_list]
 
     # date in year - float format
     datevector = []
@@ -341,3 +341,35 @@ def get_date_range(dmin, dmax):
     date_objs = np.arange(t1, t2, dtype='datetime64[D]').astype('M8[D]').astype('O')
     date_list = [obj.strftime('%Y%m%d') for obj in date_objs] + [dmax]
     return date_list
+
+
+def utc2solar_time(utc_time, longitude):
+    """Convert UTC time to solar local time.
+    Solar time: https://en.wikipedia.org/wiki/Solar_time
+    Link: https://stackoverflow.com/questions/13314626
+
+    Parameters: utc_time   - datetime.datetime object for the UTC time
+                longitude  - float, longitude of the observer in degrees
+    Returns:    solar_time - datetime.datetime object for the local solar time
+    Example:    utc_time = dt.datetime(2015, 2, 9, 3, 18, 48)
+                solar_time = ptime.utc2solar_time(utc_time, 130.7)
+    """
+    from math import pi, cos, sin
+    # use 366 for leap years
+    if utc_time.year % 4 == 0 and utc_time.year % 100 != 0 and utc_time.year % 400 != 0:
+        year_len = 366
+    else:
+        year_len = 365
+
+    gamma = 2 * pi / year_len * (utc_time.timetuple().tm_yday - 1 + float(utc_time.hour - 12) / 24)
+    eqtime = 229.18 * (0.000075 + 0.001868 * cos(gamma) - 0.032077 * sin(gamma) \
+             - 0.014615 * cos(2 * gamma) - 0.040849 * sin(2 * gamma))
+    #decl = 0.006918 - 0.399912 * cos(gamma) + 0.070257 * sin(gamma) \
+    #       - 0.006758 * cos(2 * gamma) + 0.000907 * sin(2 * gamma) \
+    #       - 0.002697 * cos(3 * gamma) + 0.00148 * sin(3 * gamma)
+    time_offset = eqtime + 4 * longitude
+    tst = utc_time.hour * 60 + utc_time.minute + utc_time.second / 60 + time_offset
+    solar_time = dt.datetime.combine(utc_time.date(), dt.time(0)) + dt.timedelta(minutes=tst)
+
+    return solar_time
+