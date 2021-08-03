c411984c5ac4a6bab1f387411c6937b84df7668b
Zhang Yunjun <yunjunzgeo@gmail.com>
2021-08-03 15:49:06 -0700

    Update load_data.py

--- mintpy/load_data.py
+++ mintpy/load_data.py
@@ -641,14 +641,14 @@ def prepare_metadata(iDict):
             from mintpy import prep_cosicorr as prep_module
 
         # run prep_{processor} module
-        for key in [i for i in iDict.keys() if (i.startswith('mintpy.load.') and i.endswith('File')) and (i != 'mintpy.load.metaFile')]:
+        for key in [i for i in iDict.keys() if (i.startswith('mintpy.load.') and i.endswith('File') and i != 'mintpy.load.metaFile')]:
             if len(glob.glob(str(iDict[key]))) > 0:
                 # print command line
                 script_name = '{}.py'.format(os.path.basename(prep_module.__name__).split('.')[-1])
                 iargs = [iDict[key]]
                 if processor == 'gamma' and iDict['PLATFORM']:
                     iargs += ['--sensor', iDict['PLATFORM'].lower()]
-                if processor == 'cosicorr':
+                elif processor == 'cosicorr':
                     iargs += ['--metadata', iDict['mintpy.load.metaFile']]
                 print(script_name, ' '.join(iargs))
                 # run
