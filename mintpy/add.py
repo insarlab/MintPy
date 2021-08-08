965a53f31ec9a9caea5e2e0315448373e972cf0b
Zhang Yunjun <yunjunzgeo@gmail.com>
2021-08-07 22:11:58 -0700

    add.py: handle TS in the same way as diff.py
    + diff.py: _check_reference() -> check_reference() with more comments
    
    + add.py:
       - check num of files == 2 for TS file only
       - support two TS file with different reference point, different reference date, different date list; to be consistent with diff.py.

--- mintpy/add.py
+++ mintpy/add.py
@@ -10,14 +10,17 @@ import os
 import sys
 import argparse
 import numpy as np
+from mintpy.objects import timeseries
 from mintpy.utils import readfile, writefile
+from mintpy.diff import check_reference
 
 
 ################################################################################
 EXAMPLE = """example:
-  add.py mask_1.h5 mask_2.h5 mask_3.h5 -o mask_all.h5
-  add.py 081008_100220.unw 100220_110417.unw -o 081008_110417.unw
-  add.py timeseries_ERA5.h5 inputs/ERA5.h5 -o timeseries.h5
+  add.py mask_1.h5 mask_2.h5 mask_3.h5        -o mask_all.h5
+  add.py 081008_100220.unw  100220_110417.unw -o 081008_110417.unw
+  add.py timeseries_ERA5.h5 inputs/ERA5.h5    -o timeseries.h5
+  add.py timeseriesRg.h5    inputs/TECsub.h5  -o timeseriesRg_TECsub.h5 --force
 """
 
 
@@ -29,6 +32,8 @@ def create_parser():
 
     parser.add_argument('file', nargs='+', help='files (2 or more) to be added')
     parser.add_argument('-o', '--output', dest='outfile', help='output file name')
+    parser.add_argument('--force', action='store_true',
+                        help='Enforce the adding for the shared dates only for time-series files')
     return parser
 
 
@@ -38,6 +43,12 @@ def cmd_line_parse(iargs=None):
     if len(inps.file) < 2:
         parser.print_usage()
         sys.exit('ERROR: At least 2 input files needed!')
+
+    # only two input files are supported for time-series type
+    atr = readfile.read_attribute(inps.file[0])
+    if atr['FILE_TYPE'] == 'timeseries' and len(inps.file) != 2:
+        raise ValueError('Only TWO files are supported for time-series, input has {}'.format(len(inps.file)))
+
     return inps
 
 
@@ -52,7 +63,7 @@ def add_matrix(data1, data2):
     return data
 
 
-def add_file(fnames, out_file=None):
+def add_file(fnames, out_file=None, force=False):
     """Generate sum of all input files
     Parameters: fnames : list of str, path/name of input files to be added
                 out_file : str, optional, path/name of output file
@@ -67,26 +78,76 @@ def add_file(fnames, out_file=None):
             out_file += '_plus_' + os.path.splitext(os.path.basename(fnames[i]))[0]
         out_file += ext
 
-    dsDict = {}
-    dsNames = readfile.get_dataset_list(fnames[0])
-    for dsName in dsNames:
-        # ignore dsName if input file has single dataset
-        if len(dsNames) == 1:
-            dsName2read = None
-        else:
-            dsName2read = dsName
+    atr1 = readfile.read_attribute(fnames[0])
+    atr2 = readfile.read_attribute(fnames[1])
 
-        print('adding {} ...'.format(dsName))
-        data = readfile.read(fnames[0], datasetName=dsName2read)[0]
-        for i in range(1, len(fnames)):
-            data2 = readfile.read(fnames[i], datasetName=dsName2read)[0]
-            data = add_matrix(data, data2)
-        dsDict[dsName] = data
+    if atr1['FILE_TYPE'] == 'timeseries':
+        file1, file2 = fnames[0], fnames[1]
 
-    # output
-    atr = readfile.read_attribute(fnames[0])
-    print('use metadata from the 1st file: {}'.format(fnames[0]))
-    writefile.write(dsDict, out_file=out_file, metadata=atr, ref_file=fnames[0])
+        # check dates shared by two timeseries files
+        dateList1 = timeseries(file1).get_date_list()
+        dateList2 = timeseries(file2).get_date_list()
+        dateListShared = [i for i in dateList1 if i in dateList2]
+        dateShared = np.ones((len(dateList1)), dtype=np.bool_)
+        if dateListShared != dateList1:
+            print('WARNING: {} does not contain all dates in {}'.format(file2, file1))
+            if force:
+                dateListEx = list(set(dateList1) - set(dateListShared))
+                print('Continue and enforce the differencing for their shared dates only.')
+                print('\twith following dates are ignored for differencing:\n{}'.format(dateListEx))
+                dateShared[np.array([dateList1.index(i) for i in dateListEx])] = 0
+            else:
+                raise Exception('To enforce the differencing anyway, use --force option.')
+
+        # check reference point
+        ref_date, ref_y, ref_x = check_reference(atr1, atr2)
+
+        # read data2 (consider different reference_date/pixel)
+        print('read from file: {}'.format(file2))
+        data2 = readfile.read(file2, datasetName=dateListShared)[0]
+
+        if ref_y and ref_x:
+            print('* referencing data from {} to y/x: {}/{}'.format(os.path.basename(file2), ref_y, ref_x))
+            ref_box = (ref_x, ref_y, ref_x + 1, ref_y + 1)
+            ref_val = readfile.read(file2, datasetName=dateListShared, box=ref_box)[0]
+            data2 -= np.tile(ref_val.reshape(-1, 1, 1), (1, data2.shape[1], data2.shape[2]))
+
+        if ref_date:
+            print('* referencing data from {} to date: {}'.format(os.path.basename(file2), ref_date))
+            ref_ind = dateListShared.index(ref_date)
+            data2 -= np.tile(data2[ref_ind, :, :], (data2.shape[0], 1, 1))
+
+        # read data1
+        print('read from file: {}'.format(file1))
+        data = readfile.read(file1)[0]
+
+        # apply adding
+        mask = data == 0.
+        data[dateShared] += data2
+        data[mask] = 0.               # Do not change zero phase value
+        del data2
+
+        # write file
+        writefile.write(data, out_file=out_file, metadata=atr1, ref_file=file1)
+
+    else:
+        dsDict = {}
+        dsNames = readfile.get_dataset_list(fnames[0])
+        for dsName in dsNames:
+            # ignore dsName if input file has single dataset
+            dsName2read = None if len(dsNames) == 1 else dsName
+
+            print('adding {} ...'.format(dsName))
+            data = readfile.read(fnames[0], datasetName=dsName2read)[0]
+            for i in range(1, len(fnames)):
+                data2 = readfile.read(fnames[i], datasetName=dsName2read)[0]
+                data = add_matrix(data, data2)
+            dsDict[dsName] = data
+
+        # output
+        print('use metadata from the 1st file: {}'.format(fnames[0]))
+        writefile.write(dsDict, out_file=out_file, metadata=atr1, ref_file=fnames[0])
+
     return out_file
 
 
@@ -95,7 +156,7 @@ def main(iargs=None):
     inps = cmd_line_parse(iargs)
     print('input files to be added: ({})\n{}'.format(len(inps.file), inps.file))
 
-    inps.outfile = add_file(inps.file, inps.outfile)
+    inps.outfile = add_file(inps.file, inps.outfile, force=inps.force)
 
     print('Done.')
     return inps.outfile
