fc6c66e1919fa2dc45ec92f05b28a2a0e7dbdda9
Zhang Yunjun <yunjunzgeo@gmail.com>
2021-08-31 23:07:25 -0700

    __init__: rm unused module imports

--- mintpy/__init__.py
+++ mintpy/__init__.py
@@ -1,13 +1,11 @@
-from __future__ import print_function
-import sys
-import os
-
 # get version info
 from mintpy.version import release_version, logo
 __version__ = release_version
 __logo__ = logo
 
 ## check environment variable
+#import os
+#import sys
 #mintpy_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #sys.path.insert(1, mintpy_path)
 #sys.path.insert(1, os.path.join(mintpy_path, 'defaults'))
