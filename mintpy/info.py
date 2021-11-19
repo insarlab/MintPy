insarlab/MintPy

commit bdb46692f2507ebbbfda2c9c47a6dbb9104f7745

codacy style cleanup (#674)

mintpy/info.py
@@ -14,10 +14,10 @@ import numpy as np
 
 from mintpy.utils import readfile, ptime
 from mintpy.objects import (
-    giantIfgramStack, 
-    giantTimeseries, 
-    ifgramStack, 
-    timeseries, 
+    giantIfgramStack,
+    giantTimeseries,
+    ifgramStack,
+    timeseries,
     HDFEOS,
 )
 
@@ -226,11 +226,8 @@ def print_slice_list(fname, disp_num=False, print_msg=False):
     """Print slice info of file"""
     slice_list = readfile.get_slice_list(fname)
     if print_msg:
-        for i in range(len(slice_list)):
-            if disp_num:
-                print('{}\t{}'.format(slice_list[i], i))
-            else:
-                print(slice_list[i])
+        for i, slice_name in range(len(slice_list)):
+            print(f'{slice_name}\t{i}' if disp_num else slice_name)
     return slice_list
 
 

mintpy/tropo_pyaps3.py
@@ -677,7 +677,7 @@ def calc_delay_timeseries(inps):
 
             # check dataset size in space / time
             date_list = [str(re.findall('\d{8}', os.path.basename(i))[0]) for i in grib_files]
-            if (get_dataset_size(tropo_file) != get_dataset_size(geom_file) 
+            if (get_dataset_size(tropo_file) != get_dataset_size(geom_file)
                     or any(i not in timeseries(tropo_file).get_date_list() for i in date_list)):
                 flag = 'run'
                 print('2) output file does NOT have the same len/wid as the geometry file {} or does NOT contain all dates'.format(geom_file))

docker/Dockerfile
@@ -5,7 +5,7 @@ FROM debian:stretch
 LABEL \
     "Description"="Container for open source time series InSAR processing with Mintpy" \
     "Github Source"="https://github.com/insarlab/MintPy/" \
-    "Installation"="https://github.com/insarlab/MintPy/blob/master/docs/installation.md" \
+    "Installation"="https://github.com/insarlab/MintPy/blob/main/docs/installation.md" \
     "Dockerfile Author"="Andre Theron" \
     "Email"="andretheronsa@gmail.com"
 
@@ -22,7 +22,7 @@ RUN apt-get update && \
     && rm -rf /var/lib/apt/lists/*
 
 # Change default shell from dash to bash permanently
-RUN ln -sf bash /bin/sh
+SHELL ["/bin/bash", "-c"]
 
 # Prepare build time environment for conda and mintpy
 ARG MINTPY_HOME=/home/python/MintPy
@@ -34,7 +34,7 @@ ARG WORK_DIR=/home/work/
 ENV MINTPY_HOME=${MINTPY_HOME}
 ENV PYTHON3DIR=${PYTHON3DIR}
 ENV PYAPS_HOME=${PYAPS_HOME}
-ENV PATH=${MINTPY_HOME}/mintpy:${MINTPY_HOME}/sh:${PYTHON3DIR}/bin:${PATH}
+ENV PATH=${MINTPY_HOME}/mintpy:${PYTHON3DIR}/bin:${PATH}
 ENV PYTHONPATH=${PYTHONPATH}:${MINTPY_HOME}:${PYAPS_HOME}
 ENV PROJ_LIB=${PYTHON3DIR}/share/proj
 
@@ -45,7 +45,7 @@ WORKDIR /home/python
 RUN git clone https://github.com/insarlab/MintPy.git
 
 # Pull and config PyAPS
-RUN git clone https://github.com/yunjunz/pyaps3.git PyAPS
+RUN git clone https://github.com/yunjunz/PyAPS.git
 
 # Install miniconda
 RUN wget https://repo.continuum.io/miniconda/Miniconda3-4.5.4-Linux-x86_64.sh && \
@@ -59,10 +59,6 @@ RUN ${PYTHON3DIR}/bin/conda config --add channels conda-forge && \
 
 # Install pykml
 RUN ${PYTHON3DIR}/bin/pip install git+https://github.com/yunjunz/pykml.git
-
-# Install DASK - not tested yet
-#RUN mkdir -p /home/.config/dask && \
-#    cp ${MINTPY_HOME}/mintpy/defaults/dask_mintpy.yaml /home/.config/dask/dask_mintpy.yaml
 
 # Set working directory ENV - map a host data volume to this using "docker build . -t mintpy && docker run mintpy -v /host/path/to/data:/home/work/"
 RUN mkdir -p ${WORK_DIR}

mintpy/add_attribute.py
@@ -60,11 +60,8 @@ def update_file_attribute(fname, atr_new):
             print('All updated (removed) attributes already exists (do not exists) and have the same value, skip update.')
         else:
             for key, value in iter(atr_new.items()):
-                if value == 'None':
-                    try:
-                        atr.pop(key)
-                    except:
-                        pass
+                if value == 'None' and key in atr.keys():
+                    atr.pop(key)
                 else:
                     atr[key] = value
 

mintpy/asc_desc2horz_vert.py
@@ -240,8 +240,8 @@ def asc_desc_files2horz_vert(fname1, fname2, dsname=None, azimuth=-90):
 
         # get box2read for the current file
         coord = ut.coordinate(atr)
-        [x0, x1] = coord.lalo2yx([west,  east],  coord_type='lon')
-        [y0, y1] = coord.lalo2yx([north, south], coord_type='lat')
+        x0 = coord.lalo2yx(west, coord_type='lon')
+        y0 = coord.lalo2yx(north, coord_type='lat')
         box = (x0, y0, x0 + width, y0 + length)
 
         # read
@@ -288,7 +288,6 @@ def write_to_one_file(outfile, dH, dV, atr, dLOS_list, atr_list, ref_file=None):
     """Write all datasets into one HDF5 file"""
 
     print('write all datasets into {}'.format(outfile))
-    length, width = dH.shape
     dsDict = {}
     for i in range(len(atr_list)):
         # auto dataset name

mintpy/dem_error.py
@@ -276,14 +276,6 @@ def read_geometry(ts_file, geom_file=None, box=None):
     ts_obj = timeseries(ts_file)
     ts_obj.open(print_msg=False)
 
-    # size
-    if box:
-        num_row = box[3] - box[1]
-        num_col = box[2] - box[0]
-    else:
-        num_row = ts_obj.length
-        num_col = ts_obj.width
-
     # 0/2/3D geometry
     if geom_file:
         geom_obj = geometry(geom_file)

mintpy/image_stitch.py
@@ -98,8 +98,8 @@ def manual_offset_estimate(mat1, mat2):
 
     # Calculate the Offset - Difference
     # offset=V2[y00:y11,x00:x11]-V2[y0:y1,x0:x1]
-    offset = (np.nansum(V2[y00:y11, x00:x11]) / np.sum(np.isfinite(V2[y00:y11, x00:x11])) 
-              - np.nansum(V1[y0:y1, x0:x1]) / np.sum(np.isfinite(V1[y0:y1, x0:x1])))
+    offset = (  np.nansum(mat2[y00:y11, x00:x11]) / np.sum(np.isfinite(mat2[y00:y11, x00:x11])) 
+              - np.nansum(mat1[y0:y1, x0:x1]) / np.sum(np.isfinite(mat1[y0:y1, x0:x1])))
 
     return offset
 
@@ -228,7 +228,7 @@ def plot_stitch(mat11, mat22, mat, mat_diff, out_fig=None, disp_fig=False):
     plt.tight_layout()
 
     # output
-    plt.savefig(out_fig, bbox_inches='tight', transparent=True, dpi=150)
+    fig.savefig(out_fig, bbox_inches='tight', transparent=True, dpi=150)
     print('save figure to file: {}'.format(out_fig))
 
     if disp_fig:

mintpy/lookup_geo2radar.py
@@ -32,7 +32,7 @@ EXAMPLE = '''examples:
 '''
 
 def write_h5(datasetDict, out_file, metadata=None, ref_file=None, compression=None):
-    
+
     if os.path.isfile(out_file):
         print('delete exsited file: {}'.format(out_file))
         os.remove(out_file)
@@ -46,7 +46,7 @@ def write_h5(datasetDict, out_file, metadata=None, ref_file=None, compression=No
             f.attrs[key] = str(value)
             #print(key + ': ' +  value)
     print('finished writing to {}'.format(out_file))
-        
+
     return out_file  
 
 
@@ -59,8 +59,7 @@ def get_dataNames(FILE):
 
 
 def parallel_process(array, function, n_jobs=16, use_kwargs=False, front_num=1):
-    """
-        A parallel version of the map function with a progress bar. 
+    """A parallel version of the map function with a progress bar. 
 
         Args:
             array (array-like): An array to iterate over.
@@ -97,7 +96,7 @@ def parallel_process(array, function, n_jobs=16, use_kwargs=False, front_num=1):
             del f
             #pass
     out = []
-    #Get the results from the futures. 
+    #Get the results from the futures.
     for i, future in tqdm(enumerate(futures)):
         del i
         try:
@@ -106,6 +105,7 @@ def parallel_process(array, function, n_jobs=16, use_kwargs=False, front_num=1):
             out.append(e)
     return front + out
 
+
 def split_range(N, M):
     #list0 = np.arange(0,N)
     dx = round(N/M)
@@ -113,40 +113,39 @@ def split_range(N, M):
     for i in range(M):
         a0 = i*dx
         b0 = (i+1)*dx
-        
-        if b0 > N: 
+
+        if b0 > N:
             b0 = N
-            
+
         l0 = np.arange(a0,b0)
         list00.append(l0)
-        
+
     return list00
-    
+
 
 def split_box(data,row_sample,col_sample):
     data_split= []
     row,col = data.shape
     list_row = split_range(row, row_sample)
     list_col = split_range(col, col_sample)
-    
+
     for i in range(row_sample):
         for j in range(col_sample):
             y0 = min(list_row[i])
             y1 = max(list_row[i])
-            
             x0 = min(list_col[j])
             x1 = max(list_col[j])
-            
+
             data0 = data[y0:y1+1,x0:x1+1]
             data_split.append(data0)
-            
+
     return data_split
-            
+
+
 def function(data0):
     points, zz1, zz2, grid_x0, grid_y0 = data0
     grid_lat0 = griddata(points, zz1, (grid_x0, grid_y0), method='nearest')
     grid_lon0 = griddata(points, zz2, (grid_x0, grid_y0), method='nearest')
-
     return grid_lat0, grid_lon0
 
 
@@ -168,7 +167,7 @@ def cmd_line_parse(iargs=None):
 
 ################################################################################        
 def main(iargs=None):
-    
+
     inps = cmd_line_parse(iargs) 
     geom = inps.geometryGeo
     rangeCoord = readfile.read(geom,datasetName = 'rangeCoord')[0]
@@ -177,13 +176,13 @@ def main(iargs=None):
     azimuthCoord = azimuthCoord.astype(np.float64)
     #CPX_lt =complex(rangeCoord + '+' + azimuthCoord+'j')
     #CPX_lt = rangeCoord  + 1j *azimuthCoord
-    
+
     meta_geo = readfile.read_attribute(geom)
     post_Lat = meta_geo['Y_STEP']
     post_Lon = meta_geo['X_STEP']
     Corner_LAT = meta_geo['Y_FIRST']
     Corner_LON = meta_geo['X_FIRST']
-            
+
     if inps.write:
         meta = readfile.read_attribute(inps.write)
     elif inps.reference:
@@ -191,102 +190,79 @@ def main(iargs=None):
     else:
         print('write_file or the reference_file should be provided at least one.')
         sys.exit(1)
-    
+
     WIDTH_geo  = int(meta_geo['WIDTH'])
     LENGTH_geo  = int(meta_geo['LENGTH'])
-    
+
     x = np.arange(0,WIDTH_geo)
     y = np.arange(0,LENGTH_geo)
     xv, yv = np.meshgrid(x, y)
-    
+
     LAT = float(Corner_LAT) + yv*float(post_Lat)
     LON = float(Corner_LON) + xv*float(post_Lon)
     LAT = LAT.flatten()
     LON = LON.flatten() 
-        
+
     WIDTH  = int(meta['WIDTH'])
     LENGTH  = int(meta['LENGTH'])
-    
-    
+
     xx0 = rangeCoord.flatten()
     yy0 = azimuthCoord.flatten()
-    
+
     zz01 = LAT.flatten()
     zz02 = LON.flatten()
-    
+
     xx = xx0[xx0!=0]
     yy = yy0[xx0!=0]
     zz1 = zz01[xx0!=0] #lat 
     zz2 = zz02[xx0!=0] # lon
-    
+
     #points = (xx,yy)
     #points = np.zeros((len(xx),2))
     #points[:,0] = xx
     #points[:,1] = yy
-    
+
     x = np.arange(0,WIDTH)
     y = np.arange(0,LENGTH)
     grid_x, grid_y = np.meshgrid(x, y)
-    
+
     row_sample = 10
     col_sample = 10
-    
+
     list_row = split_range(LENGTH, row_sample)
     list_col = split_range(WIDTH, col_sample)
-    
-    split_grid_y= split_box(grid_y,row_sample,col_sample)
+
+    split_grid_y = split_box(grid_y,row_sample,col_sample)
     split_grid_x = split_box(grid_x,row_sample,col_sample)
-    
+
     data_parallel = []
-    for i in range(len(split_grid_y)):
-        #print(str(i))
-        ax = split_grid_x[i]
-        ay = split_grid_y[i]
-        
+    for i, (ay, ax) in enumerate(zip(split_grid_y, split_grid_x)):
         # extend the search area by 5 pixels
         max_ax = max(ax.flatten()) + 5 
         min_ax = min(ax.flatten()) - 5
-        
         max_ay = max(ay.flatten()) + 5
         min_ay = min(ay.flatten()) - 5
-        
+
         f0 = np.where((min_ax < xx) & (xx < max_ax) & (min_ay < yy) & (yy < max_ay))
         xx0 = xx[f0]
         yy0 = yy[f0]
         zz10 = zz1[f0]
         zz20 = zz2[f0]
-        
+
         points0 = np.zeros((len(xx0),2))
         points0[:,0] = xx0
         points0[:,1] = yy0
-        
         #print(split_grid_x[i].shape)
-        
-        data0 = (points0, zz10, zz20, split_grid_x[i],split_grid_y[i])
+
+        data0 = (points0, zz10, zz20, ax, ay)
         data_parallel.append(data0)
-    
+
     #grid_lat_all = []
     #grid_lon_all = []
-    
-    #print(grid_x.shape)
-    #prog_bar = ptime.progressBar(maxValue=100)
-    #for i in range(len(split_grid_y)):
-    #    #grid_x0 = split_grid_x[i]
-    #    #grid_y0 = split_grid_y[i]
-    #    #print(grid_x0.shape)
-    #    points0, zz10,zz20,grid_x0, grid_y0 = data_parallel[i]
-        
-    #    grid_lat0 = griddata(points0, zz10, (grid_x0, grid_y0), method='linear')
-    #    grid_lat_all.append(grid_lat0)
-        
-    #    grid_lon0 = griddata(points0, zz20, (grid_x0, grid_y0), method='linear')
-    #    grid_lon_all.append(grid_lon0)
-    #    prog_bar.update(i+1, every=1, suffix='{}/{} boxes'.format(i+1, 100))
-    #prog_bar.close() 
-    
-    grid_lat = np.zeros((LENGTH,WIDTH),dtype = np.float32)
-    grid_lon = np.zeros((LENGTH,WIDTH),dtype = np.float32)
-    
+
+    grid_lat = np.zeros((LENGTH,WIDTH), dtype=np.float32)
+    grid_lon = np.zeros((LENGTH,WIDTH), dtype=np.float32)
+
     proNumb = inps.parallelNumb
     future = np.zeros((len(data_parallel),))
     future = list(future)
@@ -297,31 +273,28 @@ def main(iargs=None):
             k0 = i*col_sample + j
             kk = future[k0]
             y0 = min(list_row[i])
-            y1 = max(list_row[i])  
+            y1 = max(list_row[i])
             x0 = min(list_col[j])
             x1 = max(list_col[j])
             #print(kk)
             try:
                 lat0 = kk[0]
                 lon0 = kk[1]
-        
                 grid_lat[y0:y1+1,x0:x1+1] = lat0
                 grid_lon[y0:y1+1,x0:x1+1] = lon0
             except Exception as e:
                 del e
-            
+
     #grid_lat = griddata(points, zz1, (grid_x, grid_y), method='nearest')
     #grid_lon = griddata(points, zz2, (grid_x, grid_y), method='nearest')
 
-    
     dataNames = get_dataNames(inps.write)
     datasetDict = dict()
     meta = readfile.read_attribute(inps.write)
-    
     for k0 in dataNames:
         datasetDict[k0] = readfile.read(inps.write,datasetName = k0)[0]
-     
-    DEM  = readfile.read(inps.write,datasetName = 'height')[0]
+
+    DEM = readfile.read(inps.write,datasetName = 'height')[0]
     grid_lat[DEM==0] = 0
     grid_lon[DEM==0] = 0
     grid_lat[grid_lat==0] = 'nan'
@@ -329,9 +302,8 @@ def main(iargs=None):
     datasetDict['latitude'] = grid_lat
     datasetDict['longitude'] = grid_lon
     write_h5(datasetDict, inps.write, metadata=meta, ref_file=None, compression=None)
-    
     print('done.')
-    
+
     return
 
 

commit 674a8adf8fd9f285e376fa3629386fe513687478

bugfix for info.py --slice
+ info.py: fix bug introduced in #674. Thank you @falkamelung for reporting it.

+ tropo_pyaps3.snwe2str(): simplify syntax

+ README/badge: use native color for circle CI test result

mintpy/info.py
@@ -226,7 +226,7 @@ def print_slice_list(fname, disp_num=False, print_msg=False):
     """Print slice info of file"""
     slice_list = readfile.get_slice_list(fname)
     if print_msg:
-        for i, slice_name in range(len(slice_list)):
+        for i, slice_name in enumerate(slice_list):
             print(f'{slice_name}\t{i}' if disp_num else slice_name)
     return slice_list
 

mintpy/tropo_pyaps3.py
@@ -411,29 +411,14 @@ def snwe2str(snwe):
     """Get area extent in string"""
     if not snwe:
         return None
-
-    area = ''
     s, n, w, e = snwe
 
-    if s < 0:
-        area += '_S{}'.format(abs(s))
-    else:
-        area += '_N{}'.format(abs(s))
+    area = ''
+    area += '_S{}'.format(abs(s)) if s < 0 else '_N{}'.format(abs(s))
+    area += '_S{}'.format(abs(n)) if n < 0 else '_N{}'.format(abs(n))
+    area += '_W{}'.format(abs(w)) if w < 0 else '_E{}'.format(abs(w))
+    area += '_W{}'.format(abs(e)) if e < 0 else '_E{}'.format(abs(e))
 
-    if n < 0:
-        area += '_S{}'.format(abs(n))
-    else:
-        area += '_N{}'.format(abs(n))
-
-    if w < 0:
-        area += '_W{}'.format(abs(w))
-    else:
-        area += '_E{}'.format(abs(w))
-
-    if e < 0:
-        area += '_W{}'.format(abs(e))
-    else:
-        area += '_E{}'.format(abs(e))
     return area
 
 