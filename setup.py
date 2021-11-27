162d9fbbae0156ffde310bff1492bf1cc5397b59
Zhang Yunjun <yunjunzgeo@gmail.com>
2021-11-26 19:09:38 -0800

    Revert "set matplotlib<3.5 temporarily due to upstream repo bug (#704)"
    This reverts commit d76edb928ba40e32ef8a7935bffb1644b613f099.

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ def do_setup():
             "h5py",
             "joblib",
             "lxml",
-            "matplotlib<3.5",
+            "matplotlib",
             "numpy",
             "pyaps3",
             "pykml>=0.2",
