6e2ab32e028add3652ddba698a69a0a14eabe4ac
yjzhenglamarmota <zyjatpku@gmail.com>
2022-02-28 16:51:11 -0800

    Updated Code to reflect comments
    Updated the code following Heresh and Yunjun's comments.
    Changed code name

--- mintpy/closure_phase_bias.py
+++ mintpy/closure_phase_bias.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
-# Estimate fading signal in level-n time-series
+############################################################
+# Program is part of MintPy                                #
+# Copyright (c) 2013, Zhang Yunjun, Heresh Fattahi         #
+# Author: Yujie Zheng, Feb 2022                            #
+############################################################
+# Compute average con-nl closure phase and output mask identifying areas suseptible to closure phase errors.
 
 import os, sys
 import h5py
@@ -10,11 +15,20 @@ from mintpy import ifgram_inversion as ifginv
 import argparse
 import matplotlib.pyplot as plt
 
+################################################################################
+REFERENCE = """reference:
+  Zheng, Y., et al., (2022) On Closure Phase and Systematic Bias in Multilooked SAR Interferometry, IEEE TGRS, under review (minor revision)
+"""
+EXAMPLE = """example:
+  closure_phase_bias.py -i inputs/ifgramStack.h5 --nl 20 
+"""
+
 def create_parser():
-    parser = argparse.ArgumentParser(description = 'Create an indication map for closure phase bias')
-    # input dataset
+    parser = argparse.ArgumentParser(description = 'Create an indication map for closure phase bias.')
     parser.add_argument('-i','--ifgramstack',type = str, dest = 'ifgram_stack',help = 'interferogram stack file that contains the unwrapped phases')
     parser.add_argument('--nl', dest = 'nl', type = int, default = 20, help = 'connection level that we are correcting to (or consider as no bias)')
+    parser.add_argument('--theta',dest = 'theta', type = float, help = 'Threashold for phase (unit: radian), default to be 3 sigma')
+    parser.add_argument('--epi',dest = 'episilon', type = float, default = 0.3, help = 'Threashold for amplitude, default 0.3')
     parser.add_argument('--maxMemory', dest = 'max_memory', type = float, default = 8, help = 'max memory to use in GB')
     parser.add_argument('-o', dest = 'outdir', type = str, default = '.', help = 'output file directory')
     return parser
@@ -24,22 +38,30 @@ def cmd_line_parse(iargs=None):
     inps = parser.parse_args(args = iargs)
     return inps
 
-# Obtain sum of consecutive sequential closure phase of connection n
-def cum_seq_closurePhase(inps,n,box): # we call triplets 2-level closure phase.
+# Obtain sum of consecutive complex sequential closure phase of connection n
+def cum_seq_closurePhase(SLC_list, date12_list_all, ifgram_stack, ref_phase, n, box): 
+    """
+    Input parameters:
+        SLC_list : list of SLC dates
+        date12_list_all: date12 of all the interferograms stored in the ifgramstack file
+        ifgram_stack: stack file 
+        refphase : reference phase
+        n        : connection level of the closure phase
+        box      : bounding box for the patch 
+    """
     cp_idx = []
-    SLC_list = inps.SLC_list
-    for i in range(inps.NSLC-n):
+    NSLC = len(SLC_list)
+    for i in range(NSLC-n):
         ifgram = []
         flag = True
         for j in range(n):
-            ifgram.append('{}_{}'.format(inps.SLC_list[i+j],inps.SLC_list[i+j+1]))
+            ifgram.append('{}_{}'.format(SLC_list[i+j],SLC_list[i+j+1]))
         ifgram.append('{}_{}'.format(SLC_list[i],SLC_list[i+n]))
         for ifgram_name in ifgram:
-            if ifgram_name not in inps.date12_list_all:
-                # print('Interferogram not found: ', ifgram_name )
-                flag = False
+            if ifgram_name not in date12_list_all:
+                flag = False # if missing an interferogram, we won't make the corresponding closure phase
         if flag:
-            cp_idx.append([inps.date12_list_all.index(ifgram[j]) for j in range(n+1)])
+            cp_idx.append([date12_list_all.index(ifgram[j]) for j in range(n+1)])
 
     cp_idx = np.array(cp_idx, np.int16)
     cp_idx = np.unique(cp_idx, axis = 0)
@@ -50,19 +72,19 @@ def cum_seq_closurePhase(inps,n,box): # we call triplets 2-level closure phase.
     
     if num_cp <1:
         print('No closure phase measurements found, abort')
-        sys.exit()
+        raise Exception("No triplets found!")
         
     box_width  = box[2] - box[0]
     box_length = box[3] - box[1]
-    phase = readfile.read(inps.ifgram_stack, box=box,print_msg=False)[0]
+    phase = readfile.read(ifgram_stack, box=box,print_msg=False)[0]
     cum_cp = np.zeros((box_length, box_width), np.complex64)
     for i in range(num_cp):
         cp0_w = np.zeros ((box_length, box_width), np.float32)
         for j in range(n):
                     idx = cp_idx[i,j]
-                    cp0_w = cp0_w + phase[idx,:,:] - inps.ref_phase[idx]
+                    cp0_w = cp0_w + phase[idx,:,:] - ref_phase[idx]
         idx = cp_idx[i,n]
-        cp0_w = cp0_w - (phase[idx,:,:]-inps.ref_phase[idx])
+        cp0_w = cp0_w - (phase[idx,:,:]-ref_phase[idx])
         cum_cp = cum_cp + (np.exp(1j*cp0_w))
         
     # cum_cp = np.angle(cum_cp)
@@ -77,7 +99,6 @@ def main(iargs = None):
     date12_list_all = stack_obj.get_date12_list(dropIfgram=False)
     print('scene length, width', length, width)
     ref_phase = stack_obj.get_reference_phase(unwDatasetName = 'unwrapPhase')
-    inps.ref_phase = ref_phase
     inps.length = length
     inps.width = width
     # retrieve the list of SLC dates from ifgramStack.h5
@@ -94,10 +115,7 @@ def main(iargs = None):
     print('number of SLC found : ', len(SLC_list))
     print('first SLC: ', SLC_list[0])
     print('last  SLC: ', SLC_list[-1])
-    inps.SLC_list = SLC_list
-    inps.NSLC = len(SLC_list)
-    inps.date12_list = date12_list
-    inps.date12_list_all = date12_list_all
+   
 
     # split igram_file into blocks to save memory
     box_list, num_box = ifginv.split2boxes(inps.ifgram_stack,inps.max_memory)
@@ -112,20 +130,24 @@ def main(iargs = None):
                 print('box width:  {}'.format(box_width))
                 print('box length: {}'.format(box_length))
 
-            closurephase[box[1]:box[3],box[0]:box[2]], numcp = cum_seq_closurePhase(inps,inps.nl,box)
+            closurephase[box[1]:box[3],box[0]:box[2]], numcp = cum_seq_closurePhase(SLC_list, date12_list_all, inps.ifgram_stack, ref_phase,inps.nl,box)
 
     
     
-    # What is a good threadshod?
+    # What is a good thredshold?
     # Assume that it's pure noise so that the phase is uniform distributed from -pi to pi.
     # The standard deviation of phase in each loop is pi/sqrt(3) (technically should be smaller because when forming loops there should be a reduction in phase variance)
     # The standard deviation of phase in cumulative wrapped closure phase is pi/sqrt(3)/sqrt(numcp) -- again another simplification assuming no correlation.
-    # We use 2\delta as threashold -- 95.4% confidence
-
-    threashold_cp = np.pi/np.sqrt(3)/np.sqrt(numcp)*3 # 3/sigma, 99.7% confidence
-    mask = np.ones([length,width],np.float32)
-    mask[np.abs(np.angle(closurephase))>threashold_cp] = 0 # this masks areas with potential bias
-    mask[np.abs(np.abs(closurephase)/numcp < 0.3)] = 1 # this unmasks areas with low correlation (where it's hard to know wheter there is bias either)
+    # We use 3\delta as threshold -- 99.7% confidence
+    
+    if inps.theta:
+        threshold_cp = inps.theta
+    else:
+        threshold_cp = np.pi/np.sqrt(3)/np.sqrt(numcp)*3 # 3/sigma, 99.7% confidence
+    
+    mask = np.ones([length,width],np.float32)  
+    mask[np.abs(np.angle(closurephase))>threshold_cp] = 0 # this masks areas with potential bias
+    mask[np.abs(np.abs(closurephase)/numcp < inps.episilon)] = 1 # this unmasks areas with low correlation (where it's hard to know wheter there is bias either)
     
     # save mask
     meta = dict(stack_obj.metadata)
