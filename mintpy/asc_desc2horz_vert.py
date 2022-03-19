insarlab/MintPy

commit 70d66d1bcfbd3b6115d54cbdc2bce674e7df267f

initial NO_DATA_VALUE support
+ add a new standard metadata: NO_DATA_VALUE

+ readfile.py:
   - read_attribute(): grab NO_DATA_VALUE with 'none' as default
   - add get_no_data_value() to get the value of the input file
   - add SPECIAL_STR2NUM() to facilitate no_data_value handling
   - read_isce_xml() / read_gdal_vrt(): grab NoDataValue if exists.

+ view.py: mask out pixels with no_data_value for single subplot

+ tests/config/*.txt: speedup testing by decrease plot.maxMemory to 2GB to use more parallel processes for plotting

mintpy/utils/readfile.py
@@ -27,6 +27,7 @@ from mintpy.objects import (
     timeseries,
     HDFEOS
 )
+from mintpy.utils import utils0 as ut
 
 
 SPEED_OF_LIGHT = 299792458.0   # meter/second
@@ -47,6 +48,7 @@ standardMetadataKeys = {
     'LENGTH'             : ['length', 'FILE_LENGTH', 'lines', 'azimuth_lines', 'nlines', 'az_samp',
                             'interferogram_azimuth_lines'],
     'ORBIT_DIRECTION'    : ['passDirection'],
+    'NO_DATA_VALUE'      : ['NoDataValue'],
     'PLATFORM'           : ['spacecraftName', 'sensor'],
     'POLARIZATION'       : ['polarization'],
     'PRF'                : ['prf'],
@@ -165,6 +167,16 @@ ENVI_BAND_INTERLEAVE = {
 ENVI_BYTE_ORDER = {
     '0': 'little-endian',
     '1': 'big-endian',
+}
+
+
+SPECIAL_STR2NUM = {
+    'yes'   : True,
+    'true'  : True,
+    'no'    : False,
+    'false' : False,
+    'none'  : None,
+    'nan'   : np.nan,
 }
 
 
@@ -794,6 +806,20 @@ def get_hdf5_compression(fname):
     return compression
 
 
+def get_no_data_value(fname):
+    """Grab the NO_DATA_VALUE of the input file."""
+    val = read_attribute(fname).get('NO_DATA_VALUE', None)
+    val = str(val).lower()
+
+    if ut.is_number(val):
+        val = float(val)
+    elif val in SPECIAL_STR2NUM.keys():
+        val = SPECIAL_STR2NUM[val]
+    else:
+        raise ValueError(f'Un-recognized no-data-value type: {val}')
+    return val
+
+
 #########################################################################
 def read_attribute(fname, datasetName=None, metafile_ext=None):
     """Read attributes of input file into a dictionary
@@ -1124,6 +1150,10 @@ def read_attribute(fname, datasetName=None, metafile_ext=None):
         else:
             atr['UNIT'] = '1'
 
+    # NO_DATA_VALUE
+    no_data_value = atr.get('NO_DATA_VALUE', None)
+    atr['NO_DATA_VALUE'] = str(no_data_value).lower()
+
     # FILE_PATH
     if 'FILE_PATH' in atr.keys() and 'OG_FILE_PATH' not in atr.keys():
         # need to check original source file to successfully subset legacy-sensor products
@@ -1407,6 +1437,11 @@ def read_isce_xml(fname):
         # data_type
         xmlDict['data_type'] = DATA_TYPE_ENVI2NUMPY[xmlDict['data_type']]
 
+        # NoDataValue
+        meta = root.find("./PAMRasterBand/NoDataValue")
+        if meta is not None:
+            xmlDict['NoDataValue'] = meta.text
+
     # standardize metadata keys
     xmlDict = standardize_metadata(xmlDict)
 
@@ -1491,6 +1526,9 @@ def read_gdal_vrt(fname):
         # constrain longitude within (-180, 180]
         if atr['X_FIRST'] > 180.:
             atr['X_FIRST'] -= 360.
+
+    # no data value
+    atr['NoDataValue'] = ds.GetRasterBand(1).GetNoDataValue()
 
     atr = standardize_metadata(atr)
 

mintpy/view.py
@@ -1620,7 +1620,7 @@ class viewer():
                                          print_msg=False)[0]
                 data[data != 0.] -= ref_data
 
-            # masking
+            # masking - input options
             if self.zero_mask:
                 vprint('masking pixels with zero value')
                 data = np.ma.masked_where(data == 0., data)
@@ -1629,6 +1629,13 @@ class viewer():
                 data = np.ma.masked_where(self.msk == 0., data)
             else:
                 self.msk = np.ones(data.shape, dtype=np.bool_)
+
+            # masking - NO_DATA_VALUE
+            no_data_val = readfile.get_no_data_value(self.file)
+            if no_data_val is not None and not np.isnan(no_data_val):
+                vprint(f'masking pixels with NO_DATA_VALUE of {no_data_val}')
+                data = np.ma.masked_where(data == no_data_val, data)
+
             # update/save mask info
             if np.ma.is_masked(data):
                 self.msk *= ~data.mask

commit bbe68634a7f4c672cfe207d0bb9e8d7c3e908836

readfile.read_snap_dim(): bugfix on WAVELENGTH unit
+ utils.readfile.read_snap_dim(): bugfix on WAVELENGTH unit

+ asc_desc2horz_vert: pass angles in deg instead of rad

+ utils.plot.auto_adjust_xaxis_date(): fix a bug introduced in https://github.com/insarlab/MintPy/pull/734

+ local_oscilator_drift.get_relative_range_distance(): typo fix

+ docs/README: update aux links in Yunjun et al. (2019)

mintpy/asc_desc2horz_vert.py
@@ -156,9 +156,9 @@ def get_design_matrix(los_inc_angle, los_az_angle, horz_az_angle=-90):
     This could be easily modified to support multiple view geometry
         (e.g. two adjcent tracks from asc & desc) to resolve 3D
 
-    Parameters: los_inc_angle - 1D np.ndarray in size of (num_file), LOS incidence angle in radians.
-                los_az_angle  - 1D np.ndarray in size of (num_file), LOS azimuth   angle in radians.
-                horz_az_angle - float, azimuth angle for the horizontal direction of interest in degrees.
+    Parameters: los_inc_angle - 1D np.ndarray in size of (num_file), LOS incidence angle in degree.
+                los_az_angle  - 1D np.ndarray in size of (num_file), LOS azimuth   angle in degree.
+                horz_az_angle - float, azimuth angle for the horizontal direction of interest in degree.
                                 Measured from the north with anti-clockwise direction as positive.
     Returns:    A             - 2D matrix in size of (num_file, 2)
 
@@ -166,8 +166,8 @@ def get_design_matrix(los_inc_angle, los_az_angle, horz_az_angle=-90):
     num_file = los_inc_angle.shape[0]
     A = np.zeros((num_file, 2))
     for i in range(num_file):
-        A[i, 0] = np.sin(los_inc_angle[i]) * np.cos(los_az_angle[i] - horz_az_angle)
-        A[i, 1] = np.cos(los_inc_angle[i])
+        A[i, 0] = np.sin(np.deg2rad(los_inc_angle[i])) * np.cos(np.deg2rad(los_az_angle[i] - horz_az_angle))
+        A[i, 1] = np.cos(np.deg2rad(los_inc_angle[i]))
 
     return A
 
@@ -175,9 +175,9 @@ def get_design_matrix(los_inc_angle, los_az_angle, horz_az_angle=-90):
 def asc_desc2horz_vert(dlos, los_inc_angle, los_az_angle, horz_az_angle=-90):
     """Decompose asc / desc LOS data into horz / vert data.
     Parameters: dlos          - 2D np.ndarray in size of (num_file, num_pixel), LOS displacement in meters.
-                los_inc_angle - 1/2D np.ndarray in size of (num_file), num_pixel), LOS incidence angle in radians.
-                los_az_angle  - 1/2D np.ndarray in size of (num_file), num_pixel), LOS azimuth   angle in radians.
-                horz_az_angle - float, horizontal azimuth angle of interest in radians.
+                los_inc_angle - 1/2D np.ndarray in size of (num_file), num_pixel), LOS incidence angle in degree.
+                los_az_angle  - 1/2D np.ndarray in size of (num_file), num_pixel), LOS azimuth   angle in degree.
+                horz_az_angle - float, horizontal azimuth angle of interest in degree.
     Returns:    dhorz         - 1D np.ndarray in size of (num_pixel), horizontal displacement in meters.
                 dvert         - 1D np.ndarray in size of (num_pixel), vertical   displacement in meters.
     """
@@ -265,13 +265,11 @@ def run_asc_desc2horz_vert(inps):
             print('calculate the constant LOS incidence / azimuth angles from metadata as:')
             print(f'LOS incidence angle: {los_inc_angle[i]:.1f} deg')
             print(f'LOS azimuth   angle: {los_az_angle[i]:.1f} deg')
-        los_inc_angle *= np.pi / 180.
-        los_az_angle *= np.pi / 180.
 
 
     ## 3. decompose LOS displacements into horizontal / Vertical displacements
     print('---------------------')
-    dhorz, dvert = asc_desc2horz_vert(dlos, los_inc_angle, los_az_angle, np.deg2rad(inps.horz_az_angle))
+    dhorz, dvert = asc_desc2horz_vert(dlos, los_inc_angle, los_az_angle, inps.horz_az_angle)
     dhorz = np.reshape(dhorz, (length, width))
     dvert = np.reshape(dvert, (length, width))
     dlos = np.reshape(dlos, (num_file, length, width))

mintpy/local_oscilator_drift.py
@@ -38,8 +38,7 @@ def create_parser():
                                      formatter_class=argparse.RawTextHelpFormatter,
                                      epilog='{}\n{}\n{}'.format(REFERENCE, TEMPLATE, EXAMPLE))
 
-    parser.add_argument(
-        dest='file', help='timeseries / interferograms file, i.e. timeseries.h5')
+    parser.add_argument(dest='file', help='timeseries / interferograms file, i.e. timeseries.h5')
     parser.add_argument(dest='range_dist_file',
                         help='Slant range distance file, i.e. inputs/geometryRadar.h5, inputs/geometryGeo.h5\n' +
                         'or use range_distance.py to generate it.')
@@ -55,11 +54,11 @@ def cmd_line_parse(iargs=None):
 
 
 #########################################################################################
-def get_relative_range_distance(metadata):
-    length, width = int(metadata['LENGTH']), int(metadata['WIDTH'])
-    range_dist_1d = float(metadata['RANGE_PIXEL_SIZE']) * np.linspace(0, width-1, width)
+def get_relative_range_distance(meta):
+    length, width = int(meta['LENGTH']), int(meta['WIDTH'])
+    range_dist_1d = float(meta['RANGE_PIXEL_SIZE']) * np.linspace(0, width-1, width)
     range_dist = np.tile(range_dist_1d, (length, 1))
-    range_dist -= range_dist[int(atr['REF_Y']), int(atr['REF_X'])]
+    range_dist -= range_dist[int(meta['REF_Y']), int(meta['REF_X'])]
     return range_dist
 
 
@@ -88,6 +87,7 @@ def correct_local_oscilator_drift(fname, rg_dist_file=None, out_file=None):
         print('read range distance from file: %s' % (rg_dist_file))
         rg_dist = readfile.read(rg_dist_file, datasetName='slantRangeDistance', print_msg=False)[0]
         rg_dist -= rg_dist[int(atr['REF_Y']), int(atr['REF_X'])]
+
     ramp_rate = np.array(rg_dist * 3.87e-7, np.float32)
 
     # Correct LOD Ramp for Input fname

mintpy/objects/gps.py
@@ -122,7 +122,7 @@ def get_gps_los_obs(meta, obs_type, site_names, start_date, end_date, gps_comp='
                 from mintpy.utils import readfile, utils as ut
                 meta = readfile.read_attribute('geo/geo_velocity.h5')
                 SNWE = ut.four_corners(meta)
-                site_names = gps.search_gps(SNWE, start_date, end_date)
+                site_names = gps.search_gps(SNWE, start_date='20150101', end_date='20190619')
                 vel = gps.get_gps_los_obs(meta, 'velocity',     site_names, start_date='20150101', end_date='20190619')
                 dis = gps.get_gps_los_obs(meta, 'displacement', site_names, start_date='20150101', end_date='20190619')
     """

mintpy/utils/plot.py
@@ -436,45 +436,45 @@ def auto_adjust_colormap_lut_and_disp_limit(data, num_multilook=1, max_discrete_
 def auto_adjust_xaxis_date(ax, datevector, fontsize=12, every_year=None, buffer_year=0.2,
                            every_month=None):
     """Adjust X axis
-    Input:
-        ax          - matplotlib figure axes object
-        datevector  - list of float, date in years
-                         i.e. [2007.013698630137, 2007.521917808219, 2007.6463470319634]
-                      OR list of datetime.datetime objects
-        every_year  - int, number of years per major locator
-        buffer_year - float in years, None for keep the original xlim range.
-    Output:
-        ax  - matplotlib figure axes object
-        dss - datetime.datetime object, xmin
-        dee - datetime.datetime object, xmax
+    Parameters: ax          - matplotlib figure axes object
+                datevector  - list of float, date in years
+                              i.e. [2007.013698630137, 2007.521917808219, 2007.6463470319634]
+                              OR list of datetime.datetime objects
+                every_year  - int, number of years per major locator
+                buffer_year - float in years, None for keep the original xlim range.
+    Returns:    ax          - matplotlib figure axes object
+                xmin/max    - datetime.datetime object, X-axis min/max value
     """
-    # convert datetime.datetime format into date in years
+    # convert datetime.datetime format into date in years in float
     if isinstance(datevector[0], dt.datetime):
-        datevector = [i.year + (i.timetuple().tm_yday-1)/365.25 for i in datevector]
+        datevector = [i.year + (i.timetuple().tm_yday - 1) / 365.25 for i in datevector]
 
-    # Min/Max
+    # xmin/max
     if buffer_year is not None:
-        ts = datevector[0]  - buffer_year;        ys=int(ts);  ms=int((ts - ys) * 12.0)
-        te = datevector[-1] + buffer_year + 0.1;  ye=int(te);  me=int((te - ye) * 12.0)
-        if ms > 12:   ys = ys + 1;   ms = 1
-        if me > 12:   ye = ye + 1;   me = 1
-        if ms < 1:    ys = ys - 1;   ms = 12
-        if me < 1:    ye = ye - 1;   me = 12
-        dss = dt.datetime(ys, ms, 1, 0, 0)
-        dee = dt.datetime(ye, me, 1, 0, 0)
+        # refine with buffer_year
+        t0 = datevector[0]  - buffer_year;        
+        t1 = datevector[-1] + buffer_year + 0.1;  
+        y0 = int(t0);  m0 = int((t0 - y0) * 12.0)
+        y1 = int(t1);  m1 = int((t1 - y1) * 12.0)
+        if m0 > 12:   y0 += 1;   m0 = 1
+        if m1 > 12:   y1 += 1;   m1 = 1
+        if m0 < 1 :   y0 -= 1;   m0 = 12
+        if m1 < 1 :   y1 -= 1;   m1 = 12
+        xmin = dt.datetime(y0, m0, 1, 0, 0)
+        xmax = dt.datetime(y1, m1, 1, 0, 0)
     else:
-        (dss, dee) = ax.get_xlim()
-    ax.set_xlim(dss, dee)
+        (xmin, xmax) = mdates.num2date(ax.get_xlim())
+    ax.set_xlim(xmin, xmax)
 
     # auto param
     if not every_year:
-        every_year = max(1, np.rint((dee - dss).days / 365.25 / 5).astype(int))
+        every_year = max(1, np.rint((xmax - xmin).days / 365.25 / 5).astype(int))
 
     if not every_month:
-        if   every_year <= 3:  every_month = 1
-        elif every_year <= 6:  every_month = 3
-        elif every_year <= 12: every_month = 6
-        else:                  every_month = None
+        if   every_year <= 3 :  every_month = 1
+        elif every_year <= 6 :  every_month = 3
+        elif every_year <= 12:  every_month = 6
+        else:                   every_month = None
 
     # Label/Tick format
     ax.fmt_xdata = mdates.DateFormatter('%Y-%m-%d %H:%M:%S')
@@ -486,7 +486,7 @@ def auto_adjust_xaxis_date(ax, datevector, fontsize=12, every_year=None, buffer_
     # Label font size
     ax.tick_params(labelsize=fontsize)
     # fig2.autofmt_xdate()     #adjust x overlap by rorating, may enble again
-    return ax, dss, dee
+    return ax, xmin, xmax
 
 
 def auto_adjust_yaxis(ax, dataList, fontsize=12, ymin=None, ymax=None):

mintpy/utils/readfile.py
@@ -1687,7 +1687,8 @@ def read_snap_dim(fname):
     dim_dict['PLATFORM'] = sensor.standardize_sensor_name(dim_dict['PLATFORM'])
 
     # wavelength
-    dim_dict['WAVELENGTH'] = SPEED_OF_LIGHT / float(dim_dict['radar_frequency'])
+    # radar_frequency is in the unit of MHz
+    dim_dict['WAVELENGTH'] = SPEED_OF_LIGHT / (float(dim_dict['radar_frequency']) * 1e6)
 
     # x/y_first/step_unit 
     transform = root.find("Geoposition/IMAGE_TO_MODEL_TRANSFORM").text.split(',')

mintpy/utils/utils0.py
@@ -477,15 +477,15 @@ def heading2azimuth_angle(head_angle):
 
 
 def enu2los(e, n, u, inc_angle, head_angle=None, az_angle=None):
-    """
+    """Project east/north/up motion into the line-of-sight direction defined by incidence/azimuth angle.
     Parameters: e          - np.ndarray or float, displacement in east-west direction, east as positive
                 n          - np.ndarray or float, displacement in north-south direction, north as positive
                 u          - np.ndarray or float, displacement in vertical direction, up as positive
-                inc_angle  - np.ndarray or float, local incidence angle from vertical
+                inc_angle  - np.ndarray or float, local incidence angle from vertical, in the unit of degrees
                 head_angle - np.ndarray or float, azimuth angle of the SAR platform along track direction
-                             measured from the north with clockwise direction as positive in the unit of degrees
+                             measured from the north with clockwise direction as positive, in the unit of degrees
                 az_angle   - np.ndarray or float, azimuth angle of the LOS vector from the ground to the SAR platform
-                             measured from the north with anti-clockwise direction as positive in the unit of degrees
+                             measured from the north with anti-clockwise direction as positive, in the unit of degrees
                              head_angle = 90 - az_angle
     Returns:    v_los      - np.ndarray or float, displacement in line-of-sight direction, moving toward satellite as positive
 
@@ -501,14 +501,12 @@ def enu2los(e, n, u, inc_angle, head_angle=None, az_angle=None):
         # head_angle -> az_angle
         if head_angle is not None:
             az_angle = heading2azimuth_angle(head_angle)
-    if az_angle is None:
-        raise ValueError('az_angle can not be None!')
+        else:
+            raise ValueError('az_angle can not be None!')
 
-    inc_angle *= np.pi/180.
-    az_angle *= np.pi/180.
-    v_los = (  e * np.sin(inc_angle) * np.sin(az_angle) * -1
-             + n * np.sin(inc_angle) * np.cos(az_angle)
-             + u * np.cos(inc_angle))
+    v_los = (  e * np.sin(np.deg2rad(inc_angle)) * np.sin(np.deg2rad(az_angle)) * -1
+             + n * np.sin(np.deg2rad(inc_angle)) * np.cos(np.deg2rad(az_angle))
+             + u * np.cos(np.deg2rad(inc_angle)))
 
     return v_los
 

tests/test_asc_desc2horz_vert.py
@@ -34,7 +34,7 @@ def cmd_line_parse(iargs=None):
 
 ################################################################################
 def main(iargs=None):
-    
+
     inps = cmd_line_parse(iargs)
     print('-'*50)
     print(os.path.abspath(__file__))
@@ -66,12 +66,7 @@ def main(iargs=None):
 
     # asc / desc LOS --> horz / vert [estimation]
     dlos = np.vstack((dlos0.reshape(1, -1), dlos1.reshape(1, -1)))
-    estH, estV = asc_desc2horz_vert(
-        dlos,
-        los_inc_angle=np.deg2rad(los_inc_angle),
-        los_az_angle=np.deg2rad(los_az_angle),
-        horz_az_angle=np.deg2rad(horz_az_angle),
-    )
+    estH, estV = asc_desc2horz_vert(dlos, los_inc_angle, los_az_angle, horz_az_angle)
     estH = estH.reshape(length, width)
     estV = estV.reshape(length, width)
 