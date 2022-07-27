0a8882153072debaf9da3e76b92d7c5bfdb6cbcf
Zhang Yunjun <yunjunzgeo@gmail.com>
2022-07-27 15:16:28 -0700

    codacy

--- mintpy/spatial_average.py
+++ mintpy/spatial_average.py
@@ -46,20 +46,20 @@ def main(iargs=None):
     print('\n*************** Spatial Average ******************')
     mean_list, date_list = ut.spatial_average(
         inps.file,
-         datasetName=inps.datasetName,
-         maskFile=inps.mask_file,
-         saveList=True,
+        datasetName=inps.datasetName,
+        maskFile=inps.mask_file,
+        saveList=True,
     )
 
     atr = readfile.read_attribute(inps.file)
     if inps.disp_fig and atr['FILE_TYPE'] == 'timeseries':
-        dates, datevector = ptime.date_list2vector(date_list)
+        dates = ptime.date_list2vector(date_list)[0]
         # plot
         fig = plt.figure()
         ax = fig.add_subplot(111)
         ax.plot(dates, mean_list, '-o')#, lw=2, ms=16, alpha=0.7) #, mfc='crimson')
         ax.set_title('Spatial Average', fontsize=12)
-        ax = pp.auto_adjust_xaxis_date(ax, datevector)[0]
+        ax = pp.auto_adjust_xaxis_date(ax, dates)[0]
         ax.set_xlabel('Time [years]', fontsize=12)
         ax.set_ylabel('Mean', fontsize=12)
         plt.show()
