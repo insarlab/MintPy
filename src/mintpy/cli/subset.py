4da897faa378010978461be516a01f82328902b3
Zhang Yunjun <yunjunz@outlook.com>
2023-08-23 16:05:17 +0800

    subset: support hdf5 <--> binary
    + support hdf5 input --> binary output
    
    + support binary input --> hdf5 output

--- src/mintpy/cli/subset.py
+++ src/mintpy/cli/subset.py
@@ -21,6 +21,7 @@ EXAMPLE = """example:
   subset.py inputs/ifgramStack.h5 -y 400  1500 -x 200   600
   subset.py geo_velocity.h5       -l 30.5 30.8 -L 130.3 130.9
   subset.py 030405_090801.unw     -t SinabungT495F50AlosA.template
+  subset.py demLat*.dem.wgs84 --lat 32.5 33.0 --lon 130.2 130.6 -o srtm1.h5
 
   # subset to the same coverage as the reference file
   subset.py geo_incidence.h5 -r subset_geo_velocity.h5
