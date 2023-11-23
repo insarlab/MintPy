4419a463e41a4d69a0e56dc26a64092bf03c03b4
Zhang Yunjun <yunjunz@outlook.com>
2023-11-23 15:09:39 +0800

    fix_typos.sh: use github.com instead of raw.githubusercontent.com for spelling.dat url
    + scripts/fix_typos.sh: use the github.com url for the spelling.dat file, as downloading raw.githubusercontent.com files in China does not work well
    
    + scripts/fix_typos.sh: check gdal_dict.txt file instead of the ./fix_typos dir, so that one could run the script without deleting the ./fix_typos directory
    
    + scripts/fix_typos.sh: add more words to the AUTHORIZED_LIST

--- scripts/fix_typos.sh
+++ scripts/fix_typos.sh
@@ -45,35 +45,39 @@ esac
 GDAL_ROOT=$SCRIPT_DIR/..
 cd "$GDAL_ROOT"
 
-if ! test -d fix_typos; then
+if [ ! -f fix_typos/gdal_dict.txt ]; then
     # Get our fork of codespell that adds --words-white-list and full filename support for -S option
-    mkdir fix_typos
-    (cd fix_typos
-     git clone https://github.com/rouault/codespell
-     (cd codespell && git checkout gdal_improvements)
-     # Aggregate base dictionary + QGIS one + Debian Lintian one
-     curl https://raw.githubusercontent.com/qgis/QGIS/master/scripts/spell_check/spelling.dat | sed "s/:/->/" | sed "s/:%//" | grep -v "colour->" | grep -v "colours->" > qgis.txt
-     curl https://salsa.debian.org/lintian/lintian/-/raw/master/data/spelling/corrections | grep "||" | grep -v "#" | sed "s/||/->/" > debian.txt
-     cat codespell/data/dictionary.txt qgis.txt debian.txt | awk 'NF' > gdal_dict.txt
-     echo "difered->deferred" >> gdal_dict.txt
-     echo "differed->deferred" >> gdal_dict.txt
-     grep -v 404 < gdal_dict.txt > gdal_dict.txt.tmp
-     mv gdal_dict.txt.tmp gdal_dict.txt
+    mkdir -p fix_typos
+    (
+        cd fix_typos
+        if [ ! -d codespell ]; then
+          git clone https://github.com/rouault/codespell
+        fi
+        (cd codespell && git checkout gdal_improvements)
+        # Aggregate base dictionary + QGIS one + Debian Lintian one
+        curl https://github.com/qgis/QGIS/blob/master/scripts/spell_check/spelling.dat | sed "s/:/->/" | sed "s/:%//" | grep -v "colour->" | grep -v "colours->" > qgis.txt
+        curl https://salsa.debian.org/lintian/lintian/-/raw/master/data/spelling/corrections | grep "||" | grep -v "#" | sed "s/||/->/" > debian.txt
+        cat codespell/data/dictionary.txt qgis.txt debian.txt | awk 'NF' > gdal_dict.txt
+        echo "difered->deferred" >> gdal_dict.txt
+        echo "differed->deferred" >> gdal_dict.txt
+        grep -v 404 < gdal_dict.txt > gdal_dict.txt.tmp
+        mv gdal_dict.txt.tmp gdal_dict.txt
     )
 fi
 
 EXCLUDED_FILES="*/.svn*,*/.git/*,configure,config.log,config.status,config.guess,config.sub,*/autom4te.cache/*,*.ai,*.svg"
+if [ -z ${AUTHORIZED_LIST+x} ]; then export AUTHORIZED_LIST=""; fi
 AUTHORIZED_LIST="$AUTHORIZED_LIST,te" # gdalwarp switch
 AUTHORIZED_LIST="$AUTHORIZED_LIST,LaTeX,BibTeX"
 AUTHORIZED_LIST="$AUTHORIZED_LIST,ALOS,Alos"
 AUTHORIZED_LIST="$AUTHORIZED_LIST,lon,Lon,LON"
 # New Mintpy ones
 AUTHORIZED_LIST="$AUTHORIZED_LIST,alos,ALOS,alosStack"
-AUTHORIZED_LIST="$AUTHORIZED_LIST,NED"
+AUTHORIZED_LIST="$AUTHORIZED_LIST,NED,LOD,Lod,lod"
 AUTHORIZED_LIST="$AUTHORIZED_LIST,waterMask,watermask"
 AUTHORIZED_LIST="$AUTHORIZED_LIST,smallbaselineApp"
 AUTHORIZED_LIST="$AUTHORIZED_LIST,Nealy" # Author in reference
 
-python fix_typos/codespell/codespell.py -w -i 3 -q 2 -S "$EXCLUDED_FILES,./autotest/*,./build*/*" \
+python fix_typos/codespell/codespell.py -w -i 3 -q 2 -S "$EXCLUDED_FILES,./autotest/*,./build*/*,./fix_typos/*" \
     --words-white-list="$AUTHORIZED_LIST" \
     -D ./fix_typos/gdal_dict.txt .
