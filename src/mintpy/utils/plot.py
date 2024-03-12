1445b22f98cbc32d0877e39b337c7170360c3691
Zhang Yunjun <yunjunz@outlook.com>
2024-03-13 00:56:23 +0800

    subset: revert changes to allow Y or X input

--- src/mintpy/utils/plot.py
+++ src/mintpy/utils/plot.py
@@ -46,6 +46,7 @@ MPL_COLORS = [
     '#bcbd22',
     '#17becf',
 ]
+# ensur UTM coordinate plot axes do not use scientific notation
 plt.rcParams["axes.formatter.limits"] = (-1e10, 1e10)
 
 
@@ -1128,7 +1129,8 @@ def plot_gps(ax, SNWE, inps, metadata=dict(), print_msg=True):
     start_date = inps.gps_start_date if inps.gps_start_date else metadata.get('START_DATE', None)
     end_date = inps.gps_end_date if inps.gps_end_date else metadata.get('END_DATE', None)
 
-    if 'UTM_ZONE' in metadata:
+    # pre-query: convert UTM to lat/lon for query
+    if 'UTM_ZONE' in metadata.keys():
         south, west = ut0.utm2latlon(atr, SNWE[2], SNWE[0])
         north, east = ut0.utm2latlon(atr, SNWE[3], SNWE[1])
         SNWE = (south, north, west, east)
@@ -1140,7 +1142,8 @@ def plot_gps(ax, SNWE, inps, metadata=dict(), print_msg=True):
         print('  continue without GNSS plots.')
         return ax
 
-    if 'UTM_ZONE' in metadata:
+    # post-query: convert lat/lon to UTM for plotting
+    if 'UTM_ZONE' in metadata.keys():
         site_lats, site_lons = ut0.latlon2utm(site_lats, site_lons)
 
     # mask out stations not coincident with InSAR data
--- src/mintpy/utils/utils.py
+++ src/mintpy/utils/utils.py
@@ -328,7 +328,7 @@ def transect_yx(z, atr, start_yx, end_yx, interpolation='nearest'):
         y_step = float(atr['Y_STEP'])
         x_step = float(atr['X_STEP'])
         if not atr.get('UTM_ZONE', None):   # WGS84 lat/lon
-            [lat0, lat1], _ = coordinate(atr).yx2lalo([y0, y1], [x0, x1])
+            [lat0, lat1] = coordinate(atr).yx2lalo([y0, y1], [x0, x1])[0]
             lat_c = (lat0 + lat1) / 2.
             y_step *= np.pi/180.0 * earth_radius
             x_step *= np.pi/180.0 * earth_radius * np.cos(lat_c * np.pi/180)
