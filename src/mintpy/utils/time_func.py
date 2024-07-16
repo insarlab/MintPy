d19aeda42857717fe0852540fb500c2480ae54d2
Liuqi2002519 <liuqi2002519@163.com>
2024-07-16 21:38:27 +0800

    Update time_func.py

--- src/mintpy/utils/time_func.py
+++ src/mintpy/utils/time_func.py
@@ -8,6 +8,7 @@
 #   from mintpy.utils import time_func
 
 
+import math
 import numpy as np
 from scipy import linalg
 
@@ -305,7 +306,7 @@ def get_design_matrix4polynomial_func(yr_diff, degree):
     """
     A = np.zeros([len(yr_diff), degree + 1], dtype=np.float32)
     for i in range(degree+1):
-        A[:,i] = (yr_diff**i) / np.math.factorial(i)
+        A[:,i] = (yr_diff**i) / math.factorial(i)
 
     return A
 
