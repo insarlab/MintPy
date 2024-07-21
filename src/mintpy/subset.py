477aff7dca31460201e8e7b7dd7db06378a86a59
Falk Amelung <famelung@earth.miami.edu>
2024-07-22 00:51:29 +0200

    subset.py: support subseeting of file in radar coordinates using --lat --lon

--- src/mintpy/subset.py
+++ src/mintpy/subset.py
@@ -183,27 +183,51 @@ def subset_input_dict2box(subset_dict, meta_dict):
 
     # Use subset_lat/lon input if existed,  priority: lat/lon > y/x > len/wid
     coord = ut.coordinate(meta_dict)
-    if subset_dict.get('subset_lat', None):
-        sub_y = coord.lalo2yx(subset_dict['subset_lat'], None)[0]
-    elif subset_dict['subset_y']:
-        sub_y = subset_dict['subset_y']
+
+    if 'Y_FIRST' in meta_dict.keys():
+        geocoded =True
     else:
-        sub_y = [0, length]
+        geocoded =False
 
-    if subset_dict.get('subset_lon', None):
-        sub_x = coord.lalo2yx(None, subset_dict['subset_lon'])[1]
-    elif subset_dict['subset_x']:
-        sub_x = subset_dict['subset_x']
+    if geocoded:
+        if subset_dict.get('subset_lat', None):
+            sub_y = coord.lalo2yx(subset_dict['subset_lat'], None)[0]
+        else:
+            sub_y = [0, length]
+
+        if subset_dict.get('subset_lon', None):
+            sub_x = coord.lalo2yx(None, subset_dict['subset_lon'])[1]
+        else:
+            sub_x = [0, width]
+    
+        # Get subset box in y/x
+        sub_x = sorted(sub_x)
+        sub_y = sorted(sub_y)
+        pix_box = (sub_x[0], sub_y[0], sub_x[1], sub_y[1])
+
+        # Get subset box in lat/lon from subset box in y/x
+        geo_box = coord.box_pixel2geo(pix_box)
     else:
-        sub_x = [0, width]
+        if subset_dict.get('subset_lat', None):
+            geo_box = (subset_dict['subset_lon'][0], subset_dict['subset_lat'][1], subset_dict['subset_lon'][1], subset_dict['subset_lat'][0])
+            pix_box = coord.bbox_geo2radar(geo_box, buf=0)
+        else:
+            if subset_dict['subset_y']:
+                sub_y = subset_dict['subset_y']
+            else:
+                sub_y = [0, length]
+        
+            if subset_dict['subset_x']:
+                sub_x = subset_dict['subset_x']
+            else:
+                sub_x = [0, width]
+                 # Get subset box in y/x
+            sub_x = sorted(sub_x)
+            sub_y = sorted(sub_y)
+            pix_box = (sub_x[0], sub_y[0], sub_x[1], sub_y[1])
 
-    # Get subset box in y/x
-    sub_x = sorted(sub_x)
-    sub_y = sorted(sub_y)
-    pix_box = (sub_x[0], sub_y[0], sub_x[1], sub_y[1])
-
-    # Get subset box in lat/lon from subset box in y/x
-    geo_box = coord.box_pixel2geo(pix_box)
+            # Get subset box in lat/lon from subset box in y/x
+            geo_box = coord.box_pixel2geo(pix_box)
 
     return pix_box, geo_box
 
