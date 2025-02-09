3b37a9680fb3bee7311e78e347a063802304da13
Ritwika Dasgupta <ritwika21296@gmail.com>
2025-02-10 02:56:48 +0900

    sync snap11

--- src/mintpy/utils/readfile.py
+++ src/mintpy/utils/readfile.py
@@ -1897,7 +1897,7 @@ def read_snap_dim(fname):
     bases = ds.find("MDElem[@name='Baselines']").findall("MDElem")[0].findall("MDElem")
 
     # date12
-    dates = [x.get('name').split(':')[1].strip() for x in bases]
+    dates = [x.get('name').split('_')[1].strip() for x in bases]
     [date1, date2] = sorted(dt.datetime.strptime(x, '%d%b%Y').strftime('%Y%m%d') for x in dates)
     dim_dict['DATE12'] = f'{date1[2:]}-{date2[2:]}'
 
