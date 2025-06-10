insarlab/MintPy

commit 8198ddacc7c8aabd466bae8c67021ed4e433cd6e

`subset.py`: support `--lat/lon` in radar coordinates (#1238)
+ `coord.geo2radar()`: use smaller search range (x/y_factor) for more accurate conversion. Tests based on Sentinel-1 miaplpy dataset show this change brings the conversion accuracy to the sub-pixel level.

+ `subset`: support subsetting radar-coded files using lat/lon
   - revert the change in `subset_input_dict2box()` to remain its simplicity as described in the comment.
   - `read_aux_subset2inps()`: add the code body to convert bbox in lat/lon to bbox in y/x for radar-coded files
   - `read_aux_subset2inps()`: set `buf=0` while calling `bbox_geo2radar()` for a more precise conversion, the extra buffer zone seems not needed, as it already produces a slightly bigger bbox during the geo2radar conversion, even with buf=0.

+ `cli/subset`: add usage example for radar-coded files

---------

Co-authored-by: Zhang Yunjun <yunjunz@outlook.com>

src/mintpy/objects/coord.py
@@ -347,8 +347,8 @@ class coordinate:
             rg_step_deg = 180./np.pi * rg_step / (self.earth_radius * np.cos(lat_c * np.pi/180.))
 
             az, rg = np.zeros(lat.shape), np.zeros(lat.shape)
-            x_factor = 10
-            y_factor = 10
+            x_factor = 4
+            y_factor = 4
 
             # search the overlap area of buffer in x/y direction and use the cross center
             if lat.size == 1:
@@ -409,8 +409,8 @@ class coordinate:
                 x_factor = 2 * np.ceil(abs(lut.lon_step) / rg_step)
                 y_factor = 2 * np.ceil(abs(lut.lat_step) / az_step)
             except:
-                x_factor = 10
-                y_factor = 10
+                x_factor = 4
+                y_factor = 4
 
             if 'SUBSET_XMIN' in self.src_metadata.keys():
                 rg += int(self.src_metadata['SUBSET_XMIN'])
@@ -490,16 +490,18 @@ class coordinate:
         return geo_box
 
 
-    def bbox_geo2radar(self, geo_box, print_msg=False):
+    def bbox_geo2radar(self, geo_box, buf=None, print_msg=False):
         """Calculate bounding box in x/y for file in radar coord, based on input geo box.
         Parameters: geo_box - tuple of 4 float, indicating the UL/LR lon/lat
+                    buf     - int, buffer length in number of pixels
         Returns:    pix_box - tuple of 4 int, indicating the UL/LR x/y of the bounding box in radar coord
                               for the corresponding lat/lon coverage.
         """
         lat = np.array([geo_box[3], geo_box[3], geo_box[1], geo_box[1]])
         lon = np.array([geo_box[0], geo_box[2], geo_box[0], geo_box[2]])
         y, x, y_res, x_res = self.geo2radar(lat, lon, print_msg=print_msg)
-        buf = 2 * np.max(np.abs([x_res, y_res]))
+        if buf is None:
+            buf = 2 * np.max(np.abs([x_res, y_res]))
         pix_box = (np.min(x) - buf, np.min(y) - buf,
                    np.max(x) + buf, np.max(y) + buf)
         return pix_box

src/mintpy/cli/subset.py
@@ -33,8 +33,11 @@ EXAMPLE = """example:
   subset.py geo_velocity.h5 -l 32.2 33.5  --outfill-nan
   subset.py Mask.h5 -x 500 3500 --outfill 0
 
-  # "tight" subset for geocoded lookup table larger than data file
-  subset.py geomap_4rlks.trans --tight
+  # crop radar-coded file using lat/lon
+  # Recommend subsetting all files in one command to ensure consistent output file size
+  # as the geo2radar coordinate conversion may not be strictly accurate
+  subset.py velocity.h5 geometryRadar.h5 --lookup geometryRadar.h5 --lat 25.894 25.896 --lon -80.124 -80.122
+  subset.py slcStack.h5 geometryRadar.h5 --lookup geometryRadar.h5 --lat 25.894 25.896 --lon -80.124 -80.122
 """
 
 def create_parser(subparsers=None):

src/mintpy/subset.py
@@ -418,6 +418,8 @@ def subset_file(fname, subset_dict_input, out_file=None):
     return out_file
 
 
+################################ CLI utility ################################
+
 def read_aux_subset2inps(inps):
     # Convert All Inputs into subset_y/x/lat/lon
     # Input Priority: subset_y/x/lat/lon > reference > template > tight
@@ -465,5 +467,30 @@ def read_aux_subset2inps(inps):
 
         # Update subset_y/x/lat/lon
         inps = subset_box2inps(inps, pix_box, geo_box)
+
+    # scenario: subset radar-coded file using lat/lon
+    atr = readfile.read_attribute(inps.file[0])
+    if ('Y_FIRST' not in atr.keys()
+            and (inps.subset_lat is not None and inps.subset_lon is not None)
+            and (inps.subset_y is None and inps.subset_x is None)
+            and inps.lookup_file is not None):
+        print('convert bounding box in lat/lon to y/x')
+        print(f'input bounding box in lat/lon: {geo_box}')
+        if not os.path.isfile(inps.lookup_file):
+            raise FileNotFoundError(f'lookup file {inps.lookup_file} NOT found!')
+
+        # convert geo_box to pix_box
+        coord = ut.coordinate(atr, lookup_file=inps.lookup_file)
+        geo_box = (inps.subset_lon[0], inps.subset_lat[1],
+                   inps.subset_lon[1], inps.subset_lat[0])    # (W, N, E, S)
+        pix_box = coord.bbox_geo2radar(geo_box, buf=0)
+        pix_box = coord.check_box_within_data_coverage(pix_box)
+        print(f'box to read for datasets in y/x: {pix_box}')
+
+        # update inps
+        inps.subset_x = [pix_box[0], pix_box[2]]
+        inps.subset_y = [pix_box[1], pix_box[3]]
+        inps.subset_lat = None
+        inps.subset_lon = None
 
     return inps

commit d5d7f4b95335417f44a22b5674823e4b1ee0aab8

bugfixes in cood / tropo_pyaps3
+ objects.coord.coordinate._get_lookup_row_col(): if the given y/x_factor is too small, resulting in no overlap area, then iterate with larger y/x_factor values. This fixes the bug introduced in #1238, which reduced the default y/x_factor from 10 to 4.

+ tropo_pyaps3: fix a bug introduced by #1364

+ prep_aria: remove the print out msg for a more informative print out msg, which was duplicated.

src/mintpy/objects/coord.py
@@ -194,14 +194,23 @@ class coordinate:
         To support point outside of value pool/matrix, could use np.polyfit to fit a line
         for y and x value buffer and return the intersection point row/col
         """
-        ymin = y - y_factor;  ymax = y + y_factor
-        xmin = x - x_factor;  xmax = x + x_factor
-        if not geo_coord:
-            ymin = max(ymin, 0.5)
-            xmin = max(xmin, 0.5)
-        mask_y = np.multiply(self.lut_y >= ymin, self.lut_y <= ymax)
-        mask_x = np.multiply(self.lut_x >= xmin, self.lut_x <= xmax)
-        mask_yx = np.multiply(mask_y, mask_x)
+        # iterate multiple times if the given y/x_factor is too small, resulting in no overlap
+        num_iter = 1
+        mask_yx = np.zeros(self.lut_y.shape, dtype=np.bool_)
+        while np.sum(mask_yx) <= 0 and num_iter<=3:
+            # search overlap
+            ymin = y - y_factor;  ymax = y + y_factor
+            xmin = x - x_factor;  xmax = x + x_factor
+            if not geo_coord:
+                ymin = max(ymin, 0.5)
+                xmin = max(xmin, 0.5)
+            mask_y = np.multiply(self.lut_y >= ymin, self.lut_y <= ymax)
+            mask_x = np.multiply(self.lut_x >= xmin, self.lut_x <= xmax)
+            mask_yx = np.multiply(mask_y, mask_x)
+            # prepare for the next iteration
+            y_factor *= 2
+            x_factor *= 2
+            num_iter += 1
 
         # for debugging only
         if debug_mode:

src/mintpy/prep_aria.py
@@ -370,7 +370,6 @@ def write_ifgram_stack(outfile, stackFiles, box=None, xstep=1, ystep=1, mli_meth
             f["dropIfgram"][ii] = True
 
             # loop through stacks
-            print(stackFiles.keys())
             for dsName in stackFiles.keys():
                 dsStack = gdal.Open(stackFiles[dsName], gdal.GA_ReadOnly)
                 bnd = dsStack.GetRasterBand(bndIdx)
@@ -378,8 +377,6 @@ def write_ifgram_stack(outfile, stackFiles, box=None, xstep=1, ystep=1, mli_meth
                 if xstep * ystep > 1:
                     mli_method_spec = mli_method if dsName not in \
                         ['connCompStack'] else 'nearest'
-                    print(f'apply {xstep} x {ystep} multilooking/downsampling via '
-                          f'{mli_method_spec} to: {dsName}')
                     data = multilook_data(data, ystep, xstep, method=mli_method_spec)
                 data[data == noDataValues[dsName]] = 0  #assign pixel with no-data to 0
 

src/mintpy/tropo_pyaps3.py
@@ -421,7 +421,7 @@ def check_pyaps_account_config(tropo_model):
 
     # for ERA5: ~/.cdsapirc
     if tropo_model == 'ERA5' and os.path.isfile(rc_file):
-        fc = np.loadtxt('.cdsapirc', dtype=str)
+        fc = np.loadtxt(rc_file, dtype=str)
         if not (fc[0,0] == 'url:' and fc[0,1] == 'https://cds.climate.copernicus.eu/api'):
             raise ValueError('CDS account is NOT setup to the latest format! Check https://github.com/insarlab/PyAPS/.')
 