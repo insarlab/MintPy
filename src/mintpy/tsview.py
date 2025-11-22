4b5f8a7261923768d9fd5c0b15f7e314d7b2cb48
Zhang Yunjun <yunjunz@outlook.com>
2025-11-22 14:09:25 +0800

    Update src/mintpy/tsview.py
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- src/mintpy/tsview.py
+++ src/mintpy/tsview.py
@@ -276,10 +276,7 @@ def is_native_reference_point(ref_yx, atr, max_err=2):
     x0, x1 = ref_x - max_err, ref_x + max_err
     y0, y1 = ref_y - max_err, ref_y + max_err
 
-    if (x0 <= ref_yx[1] < x1 and y0 <= ref_yx[0] < y1):
-        return True
-    else:
-        return False
+    return x0 <= ref_yx[1] < x1 and y0 <= ref_yx[0] < y1
 
 
 def read_exclude_date(input_ex_date, dateListAll):
