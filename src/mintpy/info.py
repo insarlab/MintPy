insarlab/MintPy

commit 122c36c30adfbc41fd2dcfd35e46c5acaa3d7961

`subset`: support UTM coord while subsetting in latitude/longitude (#1464)
* Fix UTM coordinate conversion for subset latitude and longitude

* support lat/lon -> y/x for both WGS84 and UTM files: For files in WGS84, subset works using --lat or --lon alone, or using --lat/lon together; while for files in UTM, subset works ONLY when using --lat/lon together.

---------

Co-authored-by: Zhang Yunjun <yunjunz@outlook.com>

src/mintpy/subset.py
@@ -161,12 +161,13 @@ def subset_input_dict2box(subset_dict, meta_dict):
                               subset_lat : list of 2 float, subset in lat direction, default=None
                               subset_lon : list of 2 float, subset in lon direction, default=None
                 meta_dict   - dict, including the following items:
-                              'WIDTH'      : int
-                              'LENGTH': int
-                              'X_FIRST'    : float, optional
-                              'Y_FIRST'    : float, optional
-                              'X_STEP'     : float, optional
-                              'Y_STEP'     : float, optional
+                              'WIDTH'    : int
+                              'LENGTH'   : int
+                              'X_FIRST'  : float, optional
+                              'Y_FIRST'  : float, optional
+                              'X_STEP'   : float, optional
+                              'Y_STEP'   : float, optional
+                              'UTM_ZONE' : str, optional
     Returns:    pix_box     - 4-tuple of int, in pixel unit of 1, in (x0, y0, x1, y1)
                 geo_box     - 4-tuple of float, in lat/lon unit (degree)
                               None if file is in radar coordinate.
@@ -184,14 +185,30 @@ def subset_input_dict2box(subset_dict, meta_dict):
     # Use subset_lat/lon input if existed,  priority: lat/lon > y/x > len/wid
     coord = ut.coordinate(meta_dict)
     if subset_dict.get('subset_lat', None):
-        sub_y = coord.lalo2yx(subset_dict['subset_lat'], None)[0]
+        if 'UTM_ZONE' in meta_dict:
+            # lat/lon --> UTM --> y/x conversion
+            if subset_dict.get('subset_lon', None):
+                sub_y = coord.lalo2yx(subset_dict['subset_lat'], subset_dict['subset_lon'])[0]
+            else:
+                raise TypeError('Both --lat/lon are required for WGS84-UTM coordinates conversion.')
+        else:
+            # lat/lon --> y/x conversion
+            sub_y = coord.lalo2yx(subset_dict['subset_lat'], None)[0]
     elif subset_dict['subset_y']:
         sub_y = subset_dict['subset_y']
     else:
         sub_y = [0, length]
 
     if subset_dict.get('subset_lon', None):
-        sub_x = coord.lalo2yx(None, subset_dict['subset_lon'])[1]
+        if 'UTM_ZONE' in meta_dict:
+            # lat/lon --> UTM --> y/x conversion
+            if subset_dict.get('subset_lat', None):
+                sub_x = coord.lalo2yx(subset_dict['subset_lat'], subset_dict['subset_lon'])[1]
+            else:
+                raise TypeError('Both --lat/lon are required for WGS84-UTM coordinates conversion.')
+        else:
+            # lat/lon --> y/x conversion
+            sub_x = coord.lalo2yx(None, subset_dict['subset_lon'])[0]
     elif subset_dict['subset_x']:
         sub_x = subset_dict['subset_x']
     else:

src/mintpy/cli/subset.py
@@ -57,7 +57,7 @@ def create_parser(subparsers=None):
     parser.add_argument('-l', '--lat', '--sub-lat', '--subset-lat', dest='subset_lat',
                         type=float, nargs=2, help='subset range in latitude')
     parser.add_argument('-L', '--lon', '--sub-lon', '--subset-lon', dest='subset_lon',
-                        type=float, nargs=2, help='subset range in column\n\n')
+                        type=float, nargs=2, help='subset range in longitude\n\n')
 
     parser.add_argument('-t', '--template', dest='template_file',
                         help='template file with subset setting.  i.e. \n'

commit f02ee9755eaff73fdc8f8402e3b19e04299010a2

bugfix in subset.subset_input_dict2box() (#1484)
+ subset: Fix a bug introduced in #1464, while subsetting a file in WGS84 coordinate using --lon option.

+ info: print out the SNWE info for geocoded files.

src/mintpy/info.py
@@ -174,6 +174,11 @@ def print_aux_info(fname):
         print('file type: '+atr['FILE_TYPE'])
         if 'Y_FIRST' in atr.keys():
             print('coordinates : GEO')
+            n = float(atr['Y_FIRST'])
+            w = float(atr['X_FIRST'])
+            s = n + float(atr['Y_STEP']) * int(atr['LENGTH'])
+            e = w + float(atr['X_STEP']) * int(atr['WIDTH'])
+            print(f'SNWE: {s}, {n}, {w}, {e}.')
         else:
             print('coordinates : RADAR')
         if k in ['timeseries']:

src/mintpy/subset.py
@@ -208,7 +208,7 @@ def subset_input_dict2box(subset_dict, meta_dict):
                 raise TypeError('Both --lat/lon are required for WGS84-UTM coordinates conversion.')
         else:
             # lat/lon --> y/x conversion
-            sub_x = coord.lalo2yx(None, subset_dict['subset_lon'])[0]
+            sub_x = coord.lalo2yx(None, subset_dict['subset_lon'])[1]
     elif subset_dict['subset_x']:
         sub_x = subset_dict['subset_x']
     else: